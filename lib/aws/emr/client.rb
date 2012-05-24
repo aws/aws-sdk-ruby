32eedf401788f607972de8c594da93c5e13b2f3f
Neville Kadwa <neville.kadwa@infogroup.com>
2012-05-24 15:15:59 -0400

    preliminary implmenetation of all emr methods

--- lib/aws/emr/client.rb
+++ lib/aws/emr/client.rb
@@ -9,13 +9,13 @@ module AWS
       # @private
       CACHEABLE_REQUESTS = Set[]
 
-      #define_client_method :add_instance_groups, 'AddInstanceGroups'
-      #define_client_method :add_job_flow_steps, 'AddJobFlowSteps'
+      define_client_method :add_instance_groups, 'AddInstanceGroups'
+      define_client_method :add_job_flow_steps, 'AddJobFlowSteps'
       define_client_method :describe_job_flows, 'DescribeJobFlows'
-      #define_client_method :modify_instance_groups, 'ModifyInstanceGroups'
-      #define_client_method :run_job_flow, 'RunJobFlow'
-      #define_client_method :set_termination_protection, 'SetTerminationProtection'
-      #define_client_method :terminate_job_flows, 'TerminateJobFlows'
+      define_client_method :modify_instance_groups, 'ModifyInstanceGroups'
+      define_client_method :run_job_flow, 'RunJobFlow'
+      define_client_method :set_termination_protection, 'SetTerminationProtection'
+      define_client_method :terminate_job_flows, 'TerminateJobFlows'
 
     end
 
