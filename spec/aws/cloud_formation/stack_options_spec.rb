9b25c67d02f341492eca306d984efa3f42622372
Daichi Arai <daichi2k@gmail.com>
2012-06-26 15:09:51 +0900

    fix to coding style

--- spec/aws/cloud_formation/stack_options_spec.rb
+++ spec/aws/cloud_formation/stack_options_spec.rb
@@ -12,44 +12,75 @@ module AWS
       end
 
       describe '#apply_template' do
+
         let(:stack_options) { @dummy_class }
 
-        context 'opts[:template] is String not url' do
+        context 'opts[:template] as a not url string' do
+
           let(:opts) { {:template => 'template' } }
+
           it 'has opts[:template_body]' do
+
             stack_options.send(:apply_template, opts)
+
             opts.should have_key(:template_body)
+
             opts.should_not have_key(:template_url)
+
           end
 
           context 'include "http"' do
+
             let(:opts) { {:template => '"Protocol": "http"' } }
+
             it 'has opts[:template_body]' do
+
               stack_options.send(:apply_template, opts)
+
               opts.should have_key(:template_body)
+
               opts.should_not have_key(:template_url)
+
             end
+
           end
+
         end
 
-        context 'opts[:template] is String as a http url' do
+        context 'opts[:template] as a http url string' do
+
           let(:opts) { { :template => 'http://s3.amazon.com/bucket_name/key' } }
+
           it 'has opts[:template_url]' do
+
             stack_options.send(:apply_template, opts)
+
             opts.should_not have_key(:template_body)
+
             opts.should have_key(:template_url)
+
           end
+
         end
 
-        context 'opts[:template] is String as a https url' do
+        context 'opts[:template] as a https url string' do
+
           let(:opts) { { :template => 'https://s3.amazon.com/bucket_name/key' } }
+
           it 'has opts[:template_url]' do
+
             stack_options.send(:apply_template, opts)
+
             opts.should_not have_key(:template_body)
+
             opts.should have_key(:template_url)
+
           end
+
         end
+
       end
+
     end
   end
 end
