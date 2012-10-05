3954c1c472242ed7f063de62342b9f889a2e7c2e
Trevor Rowe <trevorrowe@gmail.com>
2012-10-04 23:33:53 -0700

    Added basic ElastiCache classes.

---
+++ lib/aws/elasticache.rb
@@ -0,0 +1,48 @@
+# Copyright 2011-2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+require 'aws/core'
+require 'aws/elasticache/config'
+
+module AWS
+
+  # Provides an expressive, object-oriented interface to Amazon ElastiCache.
+  #
+  # == Credentials
+  #
+  # You can setup default credentials for all AWS services via
+  # AWS.config:
+  #
+  #   AWS.config(
+  #     :access_key_id => 'YOUR_ACCESS_KEY_ID',
+  #     :secret_access_key => 'YOUR_SECRET_ACCESS_KEY')
+  #
+  # Or you can set them directly on the ElastiCache interface:
+  #
+  #   ec = AWS::ElastiCache.new(
+  #     :access_key_id => 'YOUR_ACCESS_KEY_ID',
+  #     :secret_access_key => 'YOUR_SECRET_ACCESS_KEY')
+  #
+  class ElastiCache
+
+    AWS.register_autoloads(self) do
+      autoload :Client,                        'client'
+      autoload :Errors,                        'errors'
+      autoload :Request,                       'request'
+    end
+
+    include Core::ServiceInterface
+
+  end
+
+end
