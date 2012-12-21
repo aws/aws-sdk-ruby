f9df283bc75bf63a57312b926a4d94422b528d99
Trevor Rowe <trevrowe@amazon.com>
2012-12-21 10:41:36 -0800

    Updated data pipeline documentation to show the correct return value hash keys.

--- lib/aws/data_pipeline/client.rb
+++ lib/aws/data_pipeline/client.rb
@@ -48,13 +48,12 @@ module AWS
       #     exists with the same name and unique identifier, a new pipeline
       #     will not be created. Instead, you'll receive the pipeline
       #     identifier from the previous attempt. The uniqueness of the name
-      #     and unique identifier combination is scoped to the AWS account or
-      #     IAM user credentials.
+      #     and unique identifier combination is scoped to the AWS account.
       #   * +:description+ - (String) The description of the new pipeline.
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +pipelineId+ - (String)
+      #   * +:pipeline_id+ - (String)
 
       # @!method delete_pipeline(options = {})
       # Calls the DeletePipeline API operation.
@@ -71,10 +70,7 @@ module AWS
       #   * +:object_ids+ - *required* - (Array<String>) Identifiers of the
       #     pipeline objects that contain the definitions to be described. You
       #     can pass as many as 25 identifiers in a single call to
-      #     DescribeObjects.
-      #   * +:evaluate_expressions+ - (Boolean) Indicates whether any
-      #     expressions in the object should be evaluated when the object
-      #     descriptions are returned.
+      #     DescribeObjects
       #   * +:marker+ - (String) The starting point for the results to be
       #     returned. The first time you call DescribeObjects, this value
       #     should be empty. As long as the action returns HasMoreResults as
@@ -83,15 +79,15 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +pipelineObjects+ - (Array<Hash>)
-      #     * +id+ - (String)
-      #     * +name+ - (String)
-      #     * +fields+ - (Array<Hash>)
-      #       * +key+ - (String)
-      #       * +stringValue+ - (String)
-      #       * +refValue+ - (String)
-      #   * +marker+ - (String)
-      #   * +hasMoreResults+ - (Boolean)
+      #   * +:pipeline_objects+ - (Array<Hash>)
+      #     * +:id+ - (String)
+      #     * +:name+ - (String)
+      #     * +:fields+ - (Array<Hash>)
+      #       * +:key+ - (String)
+      #       * +:string_value+ - (String)
+      #       * +:ref_value+ - (String)
+      #   * +:marker+ - (String)
+      #   * +:has_more_results+ - (Boolean)
 
       # @!method describe_pipelines(options = {})
       # Calls the DescribePipelines API operation.
@@ -103,47 +99,42 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +pipelineDescriptionList+ - (Array<Hash>)
-      #     * +pipelineId+ - (String)
-      #     * +name+ - (String)
-      #     * +fields+ - (Array<Hash>)
-      #       * +key+ - (String)
-      #       * +stringValue+ - (String)
-      #       * +refValue+ - (String)
-      #     * +description+ - (String)
+      #   * +:pipeline_description_list+ - (Array<Hash>)
+      #     * +:pipeline_id+ - (String)
+      #     * +:name+ - (String)
+      #     * +:fields+ - (Array<Hash>)
+      #       * +:key+ - (String)
+      #       * +:string_value+ - (String)
+      #       * +:ref_value+ - (String)
+      #     * +:description+ - (String)
 
       # @!method evaluate_expression(options = {})
       # Calls the EvaluateExpression API operation.
       # @param [Hash] options
-      #   * +:pipeline_id+ - *required* - (String) The identifier of the
-      #     pipeline.
-      #   * +:object_id+ - *required* - (String) The identifier of the object.
-      #   * +:expression+ - *required* - (String) The expression to evaluate.
+      #   * +:pipeline_id+ - *required* - (String)
+      #   * +:object_id+ - *required* - (String)
+      #   * +:expression+ - *required* - (String)
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +evaluatedExpression+ - (String)
+      #   * +:evaluated_expression+ - (String)
 
       # @!method get_pipeline_definition(options = {})
       # Calls the GetPipelineDefinition API operation.
       # @param [Hash] options
       #   * +:pipeline_id+ - *required* - (String) The identifier of the
       #     pipeline.
-      #   * +:version+ - (String) The version of the pipeline definition to
-      #     retrieve. This parameter accepts the values latest and active.
-      #     Where latest indicates the last definition saved to the pipeline
-      #     and active indicates the last definition of the pipeline that was
-      #     activated.
+      #   * +:version+ - (String)
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +pipelineObjects+ - (Array<Hash>)
-      #     * +id+ - (String)
-      #     * +name+ - (String)
-      #     * +fields+ - (Array<Hash>)
-      #       * +key+ - (String)
-      #       * +stringValue+ - (String)
-      #       * +refValue+ - (String)
+      #   * +:pipeline_objects+ - (Array<Hash>)
+      #     * +:id+ - (String)
+      #     * +:name+ - (String)
+      #     * +:fields+ - (Array<Hash>)
+      #       * +:key+ - (String)
+      #       * +:string_value+ - (String)
+      #       * +:ref_value+ - (String)
 
       # @!method list_pipelines(options = {})
       # Calls the ListPipelines API operation.
@@ -156,32 +147,25 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +pipelineIdList+ - (Array<Hash>)
-      #     * +id+ - (String)
-      #     * +name+ - (String)
-      #   * +marker+ - (String)
-      #   * +hasMoreResults+ - (Boolean)
+      #   * +:pipeline_id_list+ - (Array<Hash>)
+      #     * +:id+ - (String)
+      #     * +:name+ - (String)
+      #   * +:marker+ - (String)
+      #   * +:has_more_results+ - (Boolean)
 
       # @!method poll_for_task(options = {})
       # Calls the PollForTask API operation.
       # @param [Hash] options
       #   * +:worker_group+ - *required* - (String) Indicates the type of task
-      #     the task runner is configured to accept and process. The worker
-      #     group is set as a field on objects in the pipeline when they are
-      #     created. You can only specify a single value for workerGroup in the
-      #     call to PollForTask. There are no wildcard values permitted in
-      #     workerGroup, the string must be an exact, case-sensitive, match.
-      #   * +:hostname+ - (String) The public DNS name of the calling task
-      #     runner.
+      #     the worker is configured to accept and process. The worker group is
+      #     set as a field on objects in the pipeline when they are created.
+      #     You can only specify a single value for workerGroup in the call to
+      #     PollForTask. There are no wildcard values permitted in workerGroup,
+      #     the string must be an exact match.
+      #   * +:hostname+ - (String) The public DNS name of the calling worker
+      #     client.
       #   * +:instance_identity+ - (Hash) Identity information for the Amazon
-      #     EC2 instance that is hosting the task runner. You can get this
-      #     value by calling the URI,
-      #     http://169.254.169.254/latest/meta-data/instance-id, from the EC2
-      #     instance. For more information, go to Instance Metadata in the
-      #     Amazon Elastic Compute Cloud User Guide. Passing in this value
-      #     proves that your task runner is running on an EC2 instance, and
-      #     ensures the proper AWS Data Pipeline service charges are applied to
-      #     your pipeline.
+      #     EC2 instance that is hosting the worker client.
       #     * +:document+ - (String) A description of an Amazon EC2 instance
       #       that is generated when the instance is launched and exposed to
       #       the instance via the instance meta-data service in the form of a
@@ -192,17 +176,17 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +taskObject+ - (Hash)
-      #     * +taskId+ - (String)
-      #     * +pipelineId+ - (String)
-      #     * +attemptId+ - (String)
-      #     * +objects+ - (Hash<String,Hash>)
-      #       * +id+ - (String)
-      #       * +name+ - (String)
-      #       * +fields+ - (Array<Hash>)
-      #         * +key+ - (String)
-      #         * +stringValue+ - (String)
-      #         * +refValue+ - (String)
+      #   * +:task_object+ - (Hash)
+      #     * +:task_id+ - (String)
+      #     * +:pipeline_id+ - (String)
+      #     * +:attempt_id+ - (String)
+      #     * +:objects+ - (Hash<String,Hash>)
+      #       * +:id+ - (String)
+      #       * +:name+ - (String)
+      #       * +:fields+ - (Array<Hash>)
+      #         * +:key+ - (String)
+      #         * +:string_value+ - (String)
+      #         * +:ref_value+ - (String)
 
       # @!method put_pipeline_definition(options = {})
       # Calls the PutPipelineDefinition API operation.
@@ -224,10 +208,10 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +validationErrors+ - (Array<Hash>)
-      #     * +id+ - (String)
-      #     * +errors+ - (Array<String>)
-      #   * +errored+ - (Boolean)
+      #   * +:validation_errors+ - (Array<Hash>)
+      #     * +:id+ - (String)
+      #     * +:errors+ - (Array<String>)
+      #   * +:errored+ - (Boolean)
 
       # @!method query_objects(options = {})
       # Calls the QueryObjects API operation.
@@ -237,41 +221,28 @@ module AWS
       #   * +:query+ - (Hash) Query that defines the objects to be returned.
       #     The Query object can contain a maximum of ten selectors. The
       #     conditions in the query are limited to top-level String fields in
-      #     the object. These filters can be applied to components, instances,
-      #     and attempts.
+      #     the object. These filters can be applied to both logical, physical,
+      #     and physical attempt objects.
       #     * +:selectors+ - (Array<Hash>) List of selectors that define the
       #       query. An object must satisfy all of the selectors to match the
       #       query.
       #       * +:field_name+ - (String) The name of the field that the
       #         operator will be applied to. The field name is the "key"
       #         portion of the field definition in the pipeline definition
-      #         syntax that is used by the AWS Data Pipeline API. If the field
-      #         is not set on the object, the condition fails.
+      #         syntax that is used by the AWS Data Pipeline API.
       #       * +:operator+ - (Hash)
       #         * +:type+ - (String) The logical operation to be performed:
       #           equal (EQ), equal reference (REF_EQ), less than or equal
       #           (LE), greater than or equal (GE), or between (BETWEEN). Equal
       #           reference (REF_EQ) can be used only with reference fields.
       #           The other comparison types can be used only with String
-      #           fields. The comparison types you can use apply only to
-      #           certain object fields, as detailed below. The comparison
-      #           operators EQ and REF_EQ act on the following fields: name
-      #           @sphere parent @componentParent @instanceParent @status
-      #           @scheduledStartTime @scheduledEndTime @actualStartTime
-      #           @actualEndTime The comparison operators GE, LE, and BETWEEN
-      #           act on the following fields: @scheduledStartTime
-      #           @scheduledEndTime @actualStartTime @actualEndTime Note that
-      #           fields beginning with the at sign (@) are read-only and set
-      #           by the web service. When you name fields, you should choose
-      #           names containing only alpha-numeric values, as symbols may be
-      #           reserved by AWS Data Pipeline. A best practice for
-      #           user-defined fields that you add to a pipeline is to prefix
-      #           their name with the string "my".
+      #           fields.
       #         * +:values+ - (Array<String>) The value that the actual field
       #           value will be compared with.
       #   * +:sphere+ - *required* - (String) Specifies whether the query
-      #     applies to components or instances. Allowable values: COMPONENT,
-      #     INSTANCE, ATTEMPT.
+      #     applies to logical objects or physical objects. Allowable values:
+      #     LO (logical object), PO (physical object), POAttempt (physical
+      #     object attempt).
       #   * +:marker+ - (String) The starting point for the results to be
       #     returned. The first time you call QueryObjects, this value should
       #     be empty. As long as the action returns HasMoreResults as True, you
@@ -283,43 +254,20 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +ids+ - (Array<String>)
-      #   * +marker+ - (String)
-      #   * +hasMoreResults+ - (Boolean)
+      #   * +:ids+ - (Array<String>)
+      #   * +:marker+ - (String)
+      #   * +:has_more_results+ - (Boolean)
 
       # @!method report_task_progress(options = {})
       # Calls the ReportTaskProgress API operation.
       # @param [Hash] options
       #   * +:task_id+ - *required* - (String) Identifier of the task assigned
-      #     to the task runner. This value is provided in the TaskObject that
-      #     the service returns with the response for the PollForTask action.
+      #     to the worker. This value is provided in the TaskObject that the
+      #     service returns with the response for the PollForTask action.
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +canceled+ - (Boolean)
-
-      # @!method report_task_runner_heartbeat(options = {})
-      # Calls the ReportTaskRunnerHeartbeat API operation.
-      # @param [Hash] options
-      #   * +:taskrunner_id+ - *required* - (String) The identifier of the task
-      #     runner. This value should be unique across your AWS account. In the
-      #     case of AWS Data Pipeline Task Runner launched on a resource
-      #     managed by AWS Data Pipeline, the web service provides a unique
-      #     identifier when it launches the application. If you have written a
-      #     custom task runner, you should assign a unique identifier for the
-      #     task runner.
-      #   * +:worker_group+ - (String) Indicates the type of task the task
-      #     runner is configured to accept and process. The worker group is set
-      #     as a field on objects in the pipeline when they are created. You
-      #     can only specify a single value for workerGroup in the call to
-      #     ReportTaskRunnerHeartbeat. There are no wildcard values permitted
-      #     in workerGroup, the string must be an exact, case-sensitive, match.
-      #   * +:hostname+ - (String) The public DNS name of the calling task
-      #     runner.
-      # @return [Core::Response]
-      #   The #data method of the response object returns
-      #   a hash with the following structure:
-      #   * +terminate+ - (Boolean)
+      #   * +:canceled+ - (Boolean)
 
       # @!method set_status(options = {})
       # Calls the SetStatus API operation.
@@ -328,22 +276,21 @@ module AWS
       #     contains the objects.
       #   * +:object_ids+ - *required* - (Array<String>) Identifies an array of
       #     objects. The corresponding objects can be either physical or
-      #     components, but not a mix of both types.
+      #     logical objects, but not a mix of both types.
       #   * +:status+ - *required* - (String) Specifies the status to be set on
-      #     all the objects in objectIds. For components, this can be either
-      #     PAUSE or RESUME. For instances, this can be either CANCEL, RERUN,
-      #     or MARK_FINISHED.
+      #     all the objects in objectIds.member.N. For logical objects, this
+      #     can be either pause or resume. For physical objects, this can be
+      #     either cancel or rerun.
       # @return [Core::Response]
 
       # @!method set_task_status(options = {})
       # Calls the SetTaskStatus API operation.
       # @param [Hash] options
       #   * +:task_id+ - *required* - (String) Identifies the task assigned to
-      #     the task runner. This value is set in the TaskObject that is
-      #     returned by the PollForTask action.
-      #   * +:task_status+ - *required* - (String) If FINISHED, the task
-      #     successfully completed. If FAILED the task ended unsuccessfully.
-      #     The FALSE value is used by preconditions.
+      #     the worker. This value is set in the TaskObject that is returned by
+      #     the PollForTask action.
+      #   * +:task_status+ - *required* - (String) If FINISHED, the task was
+      #     Status successfully completed.
       #   * +:error_code+ - (Integer) If an error occurred during the task,
       #     specifies a numerical value that represents the error. This value
       #     is set on the physical attempt object. It is used to display error
@@ -378,10 +325,10 @@ module AWS
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
-      #   * +validationErrors+ - (Array<Hash>)
-      #     * +id+ - (String)
-      #     * +errors+ - (Array<String>)
-      #   * +errored+ - (Boolean)
+      #   * +:validation_errors+ - (Array<Hash>)
+      #     * +:id+ - (String)
+      #     * +:errors+ - (Array<String>)
+      #   * +:errored+ - (Boolean)
 
       # end client methods #
 
