306a7549e03dd0261eb06104c009780d964b2d6e
Trevor Rowe <trevorrowe@gmail.com>
2013-02-11 09:54:38 -0800

    Moved AWS::SQS to signature version 4.

--- lib/aws/sqs/request.rb
+++ lib/aws/sqs/request.rb
@@ -17,7 +17,11 @@ module AWS
     # @private
     class Request < Core::Http::Request
 
-      include Core::Signature::Version2
+      include Core::Signature::Version4
+
+      def service
+        'sqs'
+      end
 
       def path
         full_url.path
