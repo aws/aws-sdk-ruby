c67468a2037983b117b91804652d4542e446ab7a
Trevor Rowe <trevorrowe@gmail.com>
2013-02-18 15:10:03 -0800

    Added support for Route53 health check operations.

--- lib/aws/route_53/client.rb
+++ lib/aws/route_53/client.rb
@@ -60,6 +60,23 @@ module AWS
       #         resource record sets that have the same combination of DNS name
       #         and type, a value that specifies the AWS region for the current
       #         resource record set.
+      #       * +:failover+ - (String) Failover resource record sets only:
+      #         Among resource record sets that have the same combination of
+      #         DNS name and type, a value that indicates whether the current
+      #         resource record set is a primary or secondary resource record
+      #         set. A failover set may contain at most one resource record set
+      #         marked as primary and one resource record set marked as
+      #         secondary. A resource record set marked as primary will be
+      #         returned if any of the following are +true+ : (1) an associated
+      #         health check is passing, (2) if the resource record set is an
+      #         alias with the evaluate target health and at least one target
+      #         resource record set is healthy, (3) both the primary and
+      #         secondary resource record set are failing health checks or (4)
+      #         there is no secondary resource record set. A secondary resource
+      #         record set will be returned if: (1) the primary is failing a
+      #         health check and either the secondary is passing a health check
+      #         or has no associated health check, or (2) there is no primary
+      #         resource record set. Valid values: PRIMARY | SECONDARY
       #       * +:ttl+ - (Integer) The cache time to live for the current
       #         resource record set.
       #       * +:resource_records+ - (Array<Hash>) A complex type that
@@ -86,6 +103,19 @@ module AWS
       #           zone ID for the LoadBalancer, see Creating Alias Resource
       #           Record Sets for Elastic Load Balancing in the Amazon Route 53
       #           Developer Guide.
+      #         * +:evaluate_target_health+ - *required* - (Boolean) Alias
+      #           resource record sets only: A boolean value that indicates
+      #           whether this Resource Record Set should respect the health
+      #           status of any health checks associated with the ALIAS target
+      #           record which it is linked to. Currently, Route 53 supports
+      #           health checks only for Elastic Load Balancing. For more
+      #           information, an example, see Creating Alias Resource Record
+      #           Sets for Elastic Load Balancing in the Amazon Route 53
+      #           Developer Guide.
+      #       * +:health_check_id+ - (String) Health Check resource record sets
+      #         only, not required for alias resource record sets: An
+      #         identifier that is used to identify health check associated
+      #         with the resource record set.
       # @return [Core::Response]
       #   The #data method of the response object returns
       #   a hash with the following structure:
@@ -94,6 +124,45 @@ module AWS
       #     * +:status+ - (String)
       #     * +:submitted_at+ - (Time)
       #     * +:comment+ - (String)
+
+      # @!method create_health_check(options = {})
+      # Calls the POST CreateHealthCheck API operation.
+      # @param [Hash] options
+      # * +:caller_reference+ - *required* - (String) A unique string that
+      #   identifies the request and that allows failed CreateHealthCheck
+      #   requests to be retried without the risk of executing the operation
+      #   twice. You must use a unique CallerReference string every time you
+      #   create a health check. CallerReference can be any unique string; you
+      #   might choose to use a string that identifies your project. Valid
+      #   characters are any Unicode code points that are legal in an XML 1.0
+      #   document. The UTF-8 encoding of the value must be less than 128
+      #   bytes.
+      # * +:health_check_config+ - *required* - (Hash) A complex type that
+      #   contains health check configuration.
+      #   * +:ip_address+ - *required* - (String) IP Address of the instance
+      #     being checked.
+      #   * +:port+ - (Integer) Port on which connection will be opened to the
+      #     instance to health check. For HTTP this defaults to 80 if the port
+      #     is not specified.
+      #   * +:type+ - *required* - (String) The type of health check to be
+      #     performed. Currently supported protocols are TCP and HTTP.
+      #   * +:resource_path+ - (String) Path to ping on the instance to check
+      #     the health. Required only for HTTP health checks, HTTP request is
+      #     issued to the instance on the given port and path.
+      #   * +:fully_qualified_domain_name+ - (String) Fully qualified domain
+      #     name of the instance to be health checked.
+      # @return [Core::Response]
+      #   The #data method of the response object returns
+      #   a hash with the following structure:
+      #   * +:health_check+ - (Hash)
+      #     * +:id+ - (String)
+      #     * +:caller_reference+ - (String)
+      #     * +:health_check_config+ - (Hash)
+      #       * +:ip_address+ - (String)
+      #       * +:port+ - (Integer)
+      #       * +:type+ - (String)
+      #       * +:resource_path+ - (String)
+      #       * +:fully_qualified_domain_name+ - (String)
 
       # @!method create_hosted_zone(options = {})
       # Calls the POST CreateHostedZone API operation.
@@ -139,6 +208,13 @@ module AWS
       #   * +:delegation_set+ - (Hash)
       #     * +:name_servers+ - (Array<String>)
 
+      # @!method delete_health_check(options = {})
+      # Calls the DELETE DeleteHealthCheck API operation.
+      # @param [Hash] options
+      # * +:health_check_id+ - *required* - (String) The ID of the health check
+      #   to delete.
+      # @return [Core::Response]
+
       # @!method delete_hosted_zone(options = {})
       # Calls the DELETE DeleteHostedZone API operation.
       # @param [Hash] options
@@ -170,6 +246,24 @@ module AWS
       #     * +:submitted_at+ - (Time)
       #     * +:comment+ - (String)
 
+      # @!method get_health_check(options = {})
+      # Calls the GET GetHealthCheck API operation.
+      # @param [Hash] options
+      # * +:health_check_id+ - *required* - (String) The ID of the health check
+      #   to retrieve.
+      # @return [Core::Response]
+      #   The #data method of the response object returns
+      #   a hash with the following structure:
+      #   * +:health_check+ - (Hash)
+      #     * +:id+ - (String)
+      #     * +:caller_reference+ - (String)
+      #     * +:health_check_config+ - (Hash)
+      #       * +:ip_address+ - (String)
+      #       * +:port+ - (Integer)
+      #       * +:type+ - (String)
+      #       * +:resource_path+ - (String)
+      #       * +:fully_qualified_domain_name+ - (String)
+
       # @!method get_hosted_zone(options = {})
       # Calls the GET GetHostedZone API operation.
       # @param [Hash] options
@@ -187,6 +281,32 @@ module AWS
       #     * +:resource_record_set_count+ - (Integer)
       #   * +:delegation_set+ - (Hash)
       #     * +:name_servers+ - (Array<String>)
+
+      # @!method list_health_checks(options = {})
+      # Calls the GET ListHealthChecks API operation.
+      # @param [Hash] options
+      # * +:marker+ - (String) If the request returned more than one page of
+      #   results, submit another request and specify the value of NextMarker
+      #   from the last response in the marker parameter to get the next page
+      #   of results.
+      # * +:max_items+ - (Integer) Specify the maximum number of health checks
+      #   to return per page of results.
+      # @return [Core::Response]
+      #   The #data method of the response object returns
+      #   a hash with the following structure:
+      #   * +:health_checks+ - (Array<Hash>)
+      #     * +:id+ - (String)
+      #     * +:caller_reference+ - (String)
+      #     * +:health_check_config+ - (Hash)
+      #       * +:ip_address+ - (String)
+      #       * +:port+ - (Integer)
+      #       * +:type+ - (String)
+      #       * +:resource_path+ - (String)
+      #       * +:fully_qualified_domain_name+ - (String)
+      #   * +:marker+ - (String)
+      #   * +:is_truncated+ - (Boolean)
+      #   * +:next_marker+ - (String)
+      #   * +:max_items+ - (String)
 
       # @!method list_hosted_zones(options = {})
       # Calls the GET ListHostedZones API operation.
@@ -243,12 +363,15 @@ module AWS
       #     * +:set_identifier+ - (String)
       #     * +:weight+ - (Integer)
       #     * +:region+ - (String)
+      #     * +:failover+ - (String)
       #     * +:ttl+ - (Integer)
       #     * +:resource_records+ - (Array<Hash>)
       #       * +:value+ - (String)
       #     * +:alias_target+ - (Hash)
       #       * +:hosted_zone_id+ - (String)
       #       * +:dns_name+ - (String)
+      #       * +:evaluate_target_health+ - (Boolean)
+      #     * +:health_check_id+ - (String)
       #   * +:is_truncated+ - (Boolean)
       #   * +:next_record_name+ - (String)
       #   * +:next_record_type+ - (String)
