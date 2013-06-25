b438b3ef1e6a97218aeaa15579da219d4daabb94
Trevor Rowe <trevorrowe@gmail.com>
2013-06-25 07:14:39 -0700

    Added a require statement for seahorse-client gem.

--- lib/aws-core.rb
+++ lib/aws-core.rb
@@ -11,4 +11,5 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
+require 'seahorse-client'
 require 'aws/core'
