8da6028da744f9306eed0e41e96fa5133c68fa18
Trevor Rowe <trevorrowe@gmail.com>
2013-06-28 09:42:00 -0700

    Sorted require statements.

--- lib/seahorse/client.rb
+++ lib/seahorse/client.rb
@@ -11,11 +11,11 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-require 'seahorse/client/version'
 require 'seahorse/client/configuration'
 require 'seahorse/client/endpoint'
 require 'seahorse/client/plugin'
 require 'seahorse/client/request'
+require 'seahorse/client/version'
 
 module Seahorse
   class Client
