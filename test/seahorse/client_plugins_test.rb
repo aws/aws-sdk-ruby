00f22b643e4283b6dd06dccb7a9894d00f753214
Trevor Rowe <trevorrowe@gmail.com>
2013-07-01 20:35:52 -0700

    Started converting Client plugin tests to spec format.

--- test/seahorse/client_plugins_test.rb
+++ test/seahorse/client_plugins_test.rb
@@ -13,64 +13,68 @@
 
 require 'test_helper'
 
-class TestSeahoreClientPlugins < MiniTest::Unit::TestCase
+module Seahorse
+  describe Client do
+    describe 'plugins' do
 
-  def new_plugin
-    @plugin_n ||= 0
-    @plugin_n += 1
-    plugin_name = "Plugin#{@plugin_n}"
-    plugin_class = Class.new
-    plugin_class.class.send(:define_method, :inspect) do
-      plugin_name
+      def new_plugin
+        @plugin_n ||= 0
+        @plugin_n += 1
+        plugin_name = "Plugin#{@plugin_n}"
+        plugin_class = Class.new
+        plugin_class.class.send(:define_method, :inspect) do
+          plugin_name
+        end
+        plugin_class
+      end
+
+      def client_class
+        @client_class ||= Class.new(Seahorse::Client)
+        @client_class
+      end
+
+      def test_adding_plugins
+        plugin = new_plugin
+        client_class.add_plugin(plugin)
+        assert_equal([plugin], client_class.plugins)
+      end
+
+      def test_removing_plugins
+        plugin1, plugin2, plugin3 = (1..3).map { new_plugin }
+        client_class.add_plugin(plugin1)
+        client_class.add_plugin(plugin2)
+        client_class.add_plugin(plugin3)
+        client_class.remove_plugin(plugin2)
+        assert_equal([plugin1, plugin3], client_class.plugins)
+      end
+
+      def test_client_inherit_plugins_from_parent_class
+        plugin1, plugin2 = [new_plugin, new_plugin]
+
+        klass = Class.new(Seahorse::Client)
+        klass.add_plugin(plugin1)
+
+        subklass = Class.new(klass)
+        subklass.add_plugin(plugin2)
+
+        klass.plugins.must_equal([plugin1])
+        subklass.plugins.must_equal([plugin1, plugin2])
+      end
+
+      def test_removing_plugin_does_not_affect_parent_class
+        plugin1, plugin2 = [new_plugin, new_plugin]
+
+        klass = Class.new(Seahorse::Client)
+        klass.add_plugin(plugin1)
+        klass.add_plugin(plugin2)
+
+        subklass = Class.new(klass)
+        subklass.remove_plugin(plugin1)
+
+        klass.plugins.must_equal([plugin1, plugin2])
+        subklass.plugins.must_equal([plugin2])
+      end
+
     end
-    plugin_class
   end
-
-  def client_class
-    @client_class ||= Class.new(Seahorse::Client)
-    @client_class
-  end
-
-  def test_adding_plugins
-    plugin = new_plugin
-    client_class.add_plugin(plugin)
-    assert_equal([plugin], client_class.plugins)
-  end
-
-  def test_removing_plugins
-    plugin1, plugin2, plugin3 = (1..3).map { new_plugin }
-    client_class.add_plugin(plugin1)
-    client_class.add_plugin(plugin2)
-    client_class.add_plugin(plugin3)
-    client_class.remove_plugin(plugin2)
-    assert_equal([plugin1, plugin3], client_class.plugins)
-  end
-
-  def test_client_inherit_plugins_from_parent_class
-    plugin1, plugin2 = [new_plugin, new_plugin]
-
-    klass = Class.new(Seahorse::Client)
-    klass.add_plugin(plugin1)
-
-    subklass = Class.new(klass)
-    subklass.add_plugin(plugin2)
-
-    klass.plugins.must_equal([plugin1])
-    subklass.plugins.must_equal([plugin1, plugin2])
-  end
-
-  def test_removing_plugin_does_not_affect_parent_class
-    plugin1, plugin2 = [new_plugin, new_plugin]
-
-    klass = Class.new(Seahorse::Client)
-    klass.add_plugin(plugin1)
-    klass.add_plugin(plugin2)
-
-    subklass = Class.new(klass)
-    subklass.remove_plugin(plugin1)
-
-    klass.plugins.must_equal([plugin1, plugin2])
-    subklass.plugins.must_equal([plugin2])
-  end
-
 end
