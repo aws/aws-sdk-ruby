522dbb3252a913669ad3e6c88765b47cce5abd20
Trevor Rowe <trevorrowe@gmail.com>
2013-07-05 15:51:11 -0700

    Documented the DynamoDB client with instructions on using the :api_version option.

--- lib/aws/dynamo_db/client.rb
+++ lib/aws/dynamo_db/client.rb
@@ -14,7 +14,31 @@
 module AWS
   class DynamoDB
 
-    # Client class for Amazon DynamoDB.
+    # Builds a client for Amazon DynamoDB.
+    #
+    #     ddb = AWS::DynamoDB::Client.new
+    #
+    # ## API Versions
+    #
+    # Amazon DynamoDB has multiple API versions.  It is important to know
+    # which API you are using.  Each API version accepts different parameters
+    # and returns data in a different format.
+    #
+    # By default, the oldest API version is used.  This ensures customers
+    # who started using DynamoDB early would not get broken by API updates.
+    # You can construct a client of a specific version by passing the
+    # `:api_version` option to the {new} method.
+    #
+    #     # defaults to the 2011-12-05 API version
+    #     ddb = AWS::DynamoDB::Client.new
+    #
+    #     # specify the API version
+    #     ddb = AWS::DynamoDB::Client.new(:api_version => '2011-12-05')
+    #     ddb = AWS::DynamoDB::Client.new(:api_version => '2012-08-10')
+    #
+    # @see V20111205
+    # @see V20120810
+    #
     class Client < ClientBase
 
       autoload :V20111205, 'aws/dynamo_db/client/v20111205'
