0d0b75894436c4ccb7eadb5ee528971865e66d42
Trevor Rowe <trevorrowe@gmail.com>
2013-07-15 23:41:10 -0700

    Fixed test and coverage tasks, so they wont run twice when run together.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,12 +11,9 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-require 'minitest'
-
 if ENV['COVERAGE']
   require 'simplecov'
 end
 
+require 'minitest/autorun'
 require 'seahorse-client'
-
-Minitest.run ARGV
