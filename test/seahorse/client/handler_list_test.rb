db56dcee0ef6a376671509b9184af6bb74b06737
Trevor Rowe <trevorrowe@gmail.com>
2013-07-17 17:49:38 -0700

    Refactored HandlerList tests for clarity.

--- test/seahorse/client/handler_list_test.rb
+++ test/seahorse/client/handler_list_test.rb
@@ -49,11 +49,11 @@ module Seahorse
           handlers.to_a.must_equal(%w(send sign build validate))
         end
 
-        %i(validate build sign send).each do |priority|
+        %w(validate build sign send).each do |priority|
 
-          describe(priority.inspect) do
+          describe(priority) do
 
-            it "has a :before_priority" do
+            it "has a before and after level for #{priority}" do
               handlers.add('before', priority: :"before_#{priority}")
               handlers.add('on', priority: priority)
               handlers.add('after', priority: :"after_#{priority}")
@@ -65,7 +65,6 @@ module Seahorse
         end
 
       end
-
     end
   end
 end
