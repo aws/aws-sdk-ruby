263f68d2cd47bedac4f9394befdd692b3d4b32bb
Trevor Rowe <trevorrowe@gmail.com>
2013-07-25 08:18:32 -0700

    Documented ResponseBody and its intended usage.

--- lib/seahorse/client/http/response_body.rb
+++ lib/seahorse/client/http/response_body.rb
@@ -14,18 +14,24 @@
 require 'thread'
 
 module Seahorse::Client::Http
+
+  # This module defines the interface an object must implement to be used
+  # as the {Response#body #body} property of a {Response}.
   module ResponseBody
 
     class ResetNotPossibleError < StandardError; end
 
+    # Write a chunk of data to the body.  Chunks should be written to the
+    # body as they are read off the response.
     # @param [String] chunk
-    # @return [String]
+    # @return [String] Returns the chunk passed in.
     def write(chunk)
       raise NotImplementedError
     end
 
-    # Returns the response body as a string if it is {#available?}.
-    # A response body may be unavailable if it has been streamed.
+    # Returns the body as a string if it is {#available?}.  If the body is not
+    # available, `nil` is returned.  The body may not be available if it has
+    # been streamed.
     # @return [String, nil]
     def read
       raise NotImplementedError
@@ -39,30 +45,34 @@ module Seahorse::Client::Http
       raise NotImplementedError
     end
 
-    # Returns the size of the response body.  This may return a value greater
-    # than zero, even if the body is not {#available?}.  This can be the case
-    # when the response body was streamed.
+    # Returns the size of the response body in bytes.  Even when the body
+    # is not {#available?}, this method should return the total number of
+    # bytes given to {#write}.
     # @return [Integer] Returns the size of the response body.
     def size
       raise NotImplementedError
     end
 
-    # @return [Boolean] Returns `true` if no data has been written to this
-    #   response body.
+    # Returns `true` if no data has been written to this body.  It also
+    # returns `true` if only the empty string has been written.
+    # @return [Boolean]
     def empty?
       size == 0
     end
 
+    # Returns `true` if it is safe to {#reset!} the response body.  A reset
+    # may be needed in the case of a network error.  The response body
+    # may return `false` if bytes have already been read from the body.
+    # This is especially important for streaming bodies.
     # @return [Boolean]
     def can_reset?
       raise NotImplementedError
     end
 
-    # Attempts to reset the response body.  Not all response bodies can be
-    # reset.  For example, streaming response bodies that have already
-    # yielded data can not be reset.
+    # Resets (empties) the response body.  If the body can not be reset,
+    # then a {ResetNotPossibleError} should be raised.
     # @return [void]
-    # @raise [ResetNotPossibleError] Raises if the response body can not be reset.
+    # @raise [ResetNotPossibleError] Raises when response body can not be reset.
     def reset!
       raise NotImplementedError
     end
