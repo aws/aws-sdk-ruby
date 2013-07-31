a8005f4245aff7374cff78f42f1d3daa92e53548
Trevor Rowe <trevorrowe@gmail.com>
2013-07-30 20:58:46 -0700

    Fixed a describe block title to show const name

--- spec/seahorse/version_spec.rb
+++ spec/seahorse/version_spec.rb
@@ -14,7 +14,7 @@
 require 'spec_helper'
 
 module Seahorse
-  describe VERSION do
+  describe 'VERSION' do
     it 'is a semver compatible version string' do
       expect(VERSION).to match(/\d+\.\d+\.\d+/)
     end
