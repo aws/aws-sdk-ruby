5e04a0d9bae9facdb2cb74e78467a6766dcb1843
Trevor Rowe <trevorrowe@gmail.com>
2013-08-01 21:08:20 -0700

    DRY'd up the plugin tests.

--- spec/seahorse/client/plugin_spec.rb
+++ spec/seahorse/client/plugin_spec.rb
@@ -21,21 +21,21 @@ module Seahorse
 
       let(:config) { Configuration.new }
 
-      let(:plugin_class) { Class.new(Plugin) }
+      let(:plugin) { Class.new(Plugin) }
 
       describe '#add_options' do
 
         it 'does nothing by default' do
           options = config.options
-          plugin_class.new.add_options(config)
+          plugin.new.add_options(config)
           expect(config.options).to eq(options)
         end
 
         it 'adds options registered by .option' do
-          plugin_class.option(:opt_without_default)
-          plugin_class.option(:opt_with_default, 'DEFAULT')
-          plugin_class.option(:opt_with_block) { 'BLOCK-DEFAULT' }
-          plugin_class.new.add_options(config)
+          plugin.option(:opt_without_default)
+          plugin.option(:opt_with_default, 'DEFAULT')
+          plugin.option(:opt_with_block) { 'BLOCK-DEFAULT' }
+          plugin.new.add_options(config)
           expect(config.opt_without_default).to eq(nil)
           expect(config.opt_with_default).to eq('DEFAULT')
           expect(config.opt_with_block).to eq('BLOCK-DEFAULT')
@@ -46,17 +46,17 @@ module Seahorse
       describe '#add_handlers' do
 
         it 'does nothing by default' do
-          plugin_class.new.add_handlers(handlers, config)
+          plugin.new.add_handlers(handlers, config)
         end
 
         it 'adds handlers registered by .handler' do
           build_handler = Class.new
           sign_handler = Class.new
           send_handler = Class.new
-          plugin_class.handler(build_handler)
-          plugin_class.handler(sign_handler, priority: :sign)
-          plugin_class.handler(send_handler, priority: :send)
-          plugin_class.new.add_handlers(handlers, config)
+          plugin.handler(build_handler)
+          plugin.handler(sign_handler, priority: :sign)
+          plugin.handler(send_handler, priority: :send)
+          plugin.new.add_handlers(handlers, config)
           expect(handlers.to_a).to eq([send_handler, sign_handler, build_handler])
         end
 
