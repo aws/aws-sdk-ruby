2ef5d25c179e4bcae8a9ecaf4f82e5eb41e3823a
Trevor Rowe <trevorrowe@gmail.com>
2013-08-01 21:41:32 -0700

    Refactored the endpoint plugin to use the new request_handler method.

--- lib/seahorse/client/plugins/endpoint.rb
+++ lib/seahorse/client/plugins/endpoint.rb
@@ -16,25 +16,17 @@ module Seahorse
     module Plugins
       class Endpoint < Plugin
 
-        # @api private
-        class EndpointHandler < Handler
-
-          def call(context)
-            context.http_request.endpoint = endpoint_for(context.config)
-            @handler.call(context)
-          end
-
-          def endpoint_for(config)
-            Http::Endpoint.new(config.endpoint, ssl_default: config.ssl_default)
-          end
-
-        end
-
         option(:endpoint) { |config| config.api.endpoint }
 
         option(:ssl_default, true)
 
-        handler(EndpointHandler, priority: :build)
+        request_handler(:EndpointHandler) do |context|
+          context.http_request.endpoint = endpoint_for(context.config)
+        end
+
+        def self.endpoint_for(config)
+          Http::Endpoint.new(config.endpoint, ssl_default: config.ssl_default)
+        end
 
       end
     end
