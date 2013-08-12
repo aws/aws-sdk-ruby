7cf31fdb62dafb2ec8994cc793e3b80786758b6a
Trevor Rowe <trevorrowe@gmail.com>
2013-08-12 15:30:39 -0700

    Documented the OperationMethods plugin.

--- lib/seahorse/client/plugins/operation_methods.rb
+++ lib/seahorse/client/plugins/operation_methods.rb
@@ -14,7 +14,30 @@
 module Seahorse
   module Client
     module Plugins
+
+      # Defines a helper method for each API operation that builds and
+      # sends the named request.
+      #
+      # # Helper Methods
+      #
+      # This plugin adds a helper method that lists the availalbe API
+      # operations.
+      #
+      #     client.operation_names
+      #     #=> [:api_operation_name1, :api_operation_name2, ...]
+      #
+      # Additionally, it adds a helper method for each operation.  This helper
+      # handles building and sending the appropriate {Request}.
+      #
+      #     # without OperationMethods plugin
+      #     req = client.build_request(:api_operation_name, request_params)
+      #     resp = req.send_request
+      #
+      #     # using the helper method defined by OperationMethods
+      #     resp = client.api_operation_name(request_params)
+      #
       class OperationMethods < Plugin
+
         initialize_client do |client|
           client.config.api.operations.keys.each do |name|
             client.class.send(:define_method, name) do |*args, &block|
@@ -22,6 +45,7 @@ module Seahorse
             end
           end
         end
+
       end
     end
   end
