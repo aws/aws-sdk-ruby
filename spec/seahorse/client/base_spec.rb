76863be832c0bb5d6fd4a3e546e8fde84bee91a2
Trevor Rowe <trevorrowe@gmail.com>
2013-08-13 12:56:20 -0700

    More cleanup/simplification of the client base specs.

--- spec/seahorse/client/base_spec.rb
+++ spec/seahorse/client/base_spec.rb
@@ -26,6 +26,10 @@ module Seahorse
 
       let(:required_plugins) { Base::REQUIRED_PLUGINS }
 
+      let(:plugin_a) { Class.new }
+
+      let(:plugin_b) { Class.new }
+
       describe '#config' do
 
         it 'returns a Configuration object' do
@@ -143,6 +147,133 @@ module Seahorse
 
       end
 
+      describe '.add_plugin' do
+
+        it 'adds plugins to the client' do
+          client_class.add_plugin(plugin_a)
+          expect(client_class.plugins).to include(plugin_a)
+        end
+
+        it 'does not add plugins to the client parent class' do
+          subclass = Class.new(client_class)
+          subclass.add_plugin(plugin_a)
+          expect(client_class.plugins).to_not include(plugin_a)
+          expect(subclass.plugins).to include(plugin_a)
+        end
+
+      end
+
+      describe '.remove_plugin' do
+
+        it 'removes a plugin from the client' do
+          client_class.add_plugin(plugin_a)
+          client_class.add_plugin(plugin_b)
+          client_class.remove_plugin(plugin_a)
+          expect(client_class.plugins).not_to include(plugin_a)
+          expect(client_class.plugins).to include(plugin_b)
+        end
+
+        it 'does not remove plugins from the client parent class' do
+          client_class.add_plugin(plugin_a)
+          subclass = client_class.define
+          subclass.remove_plugin(plugin_a)
+          expect(client_class.plugins).to include(plugin_a)
+          expect(subclass.plugins).not_to include(plugin_a)
+        end
+
+      end
+
+      describe '.set_plugins' do
+
+        it 'replaces existing plugins' do
+          client_class.add_plugin(plugin_a)
+          client_class.set_plugins([plugin_b])
+          expect(client_class.plugins).to eq(required_plugins + [plugin_b])
+        end
+
+      end
+
+      describe '.clear_plugins' do
+
+        it 'removes all plugins' do
+          client_class.add_plugin(plugin_a)
+          client_class.clear_plugins
+          expect(client_class.plugins).to eq(required_plugins + [])
+        end
+
+      end
+
+      describe '.plugins' do
+
+        it 'returns a list of plugins applied to the client' do
+          expect(client_class.plugins).to be_kind_of(Array)
+        end
+
+        it 'returns a frozen list of plugins' do
+          expect(client_class.plugins.frozen?).to eq(true)
+        end
+
+        it 'has a defualt list of plugins' do
+          client_class = Class.new(Base)
+          expect(client_class.plugins.to_a).to eq([
+            Plugins::Api,
+            Plugins::Endpoint,
+            Plugins::NetHttp,
+            Plugins::OperationMethods
+          ])
+        end
+
+        it 'replaces default plugins with the list specified in the API' do
+          stub_const('Seahorse::Client::PluginA', plugin_a)
+          api = { 'plugins' => ['Seahorse::Client::PluginA'] }
+          client_class = Base.define(api: api)
+          expect(client_class.plugins.count).to eq(5)
+          expect(client_class.plugins).to include(plugin_a)
+        end
+
+      end
+
+      describe '.client_class' do
+
+        it 'consults plugin list to see what client class to construct' do
+          subclient_class = Class.new(client_class)
+
+          plugin = double('plugin')
+          plugin.stub(:client_class_for) do |klass, config|
+            subclient_class
+          end
+
+          client_class.add_plugin(plugin)
+          expect(client_class.client_class).to be(subclient_class)
+        end
+
+        it 'does not change the client class if plugin#construct_client does not return Class' do
+          plugin = Class.new(Plugin)
+          plugin.stub(:construct_client) {|c,o| nil }
+          client_class.add_plugin(plugin)
+          expect(client_class.client_class).to be(client_class)
+        end
+
+        it 'takes the last returned Class object from plugins' do
+          stub_const("ClassA", Class.new)
+          stub_const("ClassB", Class.new)
+          stub_const("ClassC", Class.new)
+
+          plugin1 = double('plugin1')
+          plugin1.stub(:client_class_for) {|c,o| ClassA }
+          plugin2 = double('plugin2')
+          plugin2.stub(:client_class_for) {|c,o| ClassB }
+          plugin3 = double('plugin3')
+          plugin3.stub(:client_class_for) {|c,o| ClassC }
+
+          client_class.add_plugin(plugin1)
+          client_class.add_plugin(plugin2)
+          client_class.add_plugin(plugin3)
+
+          expect(client_class.client_class).to eq ClassC
+        end
+      end
+
       describe '.new' do
 
         it 'constructs the class specified by .client_class' do
@@ -152,176 +283,38 @@ module Seahorse
           expect(client).to be_kind_of(new_client_class)
         end
 
-      end
-
-      describe 'plugin methods' do
-
-        let(:plugin_a) { Class.new }
-
-        let(:plugin_b) { Class.new }
-
-        describe '.add_plugin' do
-
-          it 'adds plugins to the client' do
-            client_class.add_plugin(plugin_a)
-            expect(client_class.plugins).to include(plugin_a)
-          end
-
-          it 'does not add plugins to the client parent class' do
-            subclass = Class.new(client_class)
-            subclass.add_plugin(plugin_a)
-            expect(client_class.plugins).to_not include(plugin_a)
-            expect(subclass.plugins).to include(plugin_a)
-          end
-
+        it 'instructs plugins to #initialize_client' do
+          initialized_client = nil
+          plugin = double('plugin')
+          plugin.stub(:initialize_client) {|c| initialized_client = c }
+          client_class.add_plugin(plugin)
+          client = client_class.new
+          expect(initialized_client).to be(client)
         end
 
-        describe '.remove_plugin' do
-
-          it 'removes a plugin from the client' do
-            client_class.add_plugin(plugin_a)
-            client_class.add_plugin(plugin_b)
-            client_class.remove_plugin(plugin_a)
-            expect(client_class.plugins).not_to include(plugin_a)
-            expect(client_class.plugins).to include(plugin_b)
-          end
-
-          it 'does not remove plugins from the client parent class' do
-            client_class.add_plugin(plugin_a)
-            subclass = client_class.define
-            subclass.remove_plugin(plugin_a)
-            expect(client_class.plugins).to include(plugin_a)
-            expect(subclass.plugins).not_to include(plugin_a)
-          end
-
+        it 'instructs plugins to #add_options' do
+          plugin = double('plugin')
+          plugin.stub(:add_options) { |config| config.add_option(:foo) }
+          client_class.add_plugin(plugin)
+          expect(client_class.new.config).to respond_to(:foo)
         end
 
-        describe '.set_plugins' do
-
-          it 'replaces existing plugins' do
-            client_class.add_plugin(plugin_a)
-            client_class.set_plugins([plugin_b])
-            expect(client_class.plugins).to eq(required_plugins + [plugin_b])
-          end
-
+        it 'instructs plugins to #add_handlers' do
+          plugin = double('plugin')
+          expect(plugin).to receive(:is_a?).
+            at_least(:once).with(kind_of(Class)) { false }
+          expect(plugin).to receive(:add_handlers).with(
+            kind_of(HandlerList), kind_of(Configuration))
+          client_class.add_plugin(plugin)
+          client_class.new
         end
 
-        describe '.clear_plugins' do
-
-          it 'removes all plugins' do
-            client_class.add_plugin(plugin_a)
-            client_class.clear_plugins
-            expect(client_class.plugins).to eq(required_plugins + [])
-          end
-
+        it 'does not call methods that plugin does not respond to' do
+          plugin = Object.new
+          client_class.add_plugin(plugin)
+          client_class.new
         end
 
-        describe '.plugins' do
-
-          it 'returns a list of plugins applied to the client' do
-            expect(client_class.plugins).to be_kind_of(Array)
-          end
-
-          it 'returns a frozen list of plugins' do
-            expect(client_class.plugins.frozen?).to eq(true)
-          end
-
-          it 'has a defualt list of plugins' do
-            client_class = Class.new(Base)
-            expect(client_class.plugins.to_a).to eq([
-              Plugins::Api,
-              Plugins::Endpoint,
-              Plugins::NetHttp,
-              Plugins::OperationMethods
-            ])
-          end
-
-          it 'replaces default plugins with the list specified in the API' do
-            stub_const('Seahorse::Client::PluginA', plugin_a)
-            api = { 'plugins' => ['Seahorse::Client::PluginA'] }
-            client_class = Base.define(api: api)
-            expect(client_class.plugins.count).to eq(5)
-            expect(client_class.plugins).to include(plugin_a)
-          end
-
-        end
-
-        describe '.client_class' do
-
-          it 'consults plugin list to see what client class to construct' do
-            subclient_class = Class.new(client_class)
-
-            plugin = double('plugin')
-            plugin.stub(:construct_client) do |klass, config|
-              subclient_class
-            end
-
-            client_class.add_plugin(plugin)
-            expect(client_class.client_class).to be(subclient_class)
-          end
-
-          it 'does not change the client class if plugin#construct_client does not return Class' do
-            plugin = Class.new(Plugin)
-            plugin.stub(:construct_client) {|c,o| nil }
-            client_class.add_plugin(plugin)
-            expect(client_class.client_class).to be(client_class)
-          end
-
-          it 'takes the last returned Class object from plugins' do
-            stub_const("ClassA", Class.new)
-            stub_const("ClassB", Class.new)
-            stub_const("ClassC", Class.new)
-
-            plugin1 = double('plugin1')
-            plugin1.stub(:construct_client) {|c,o| ClassA }
-            plugin2 = double('plugin2')
-            plugin2.stub(:construct_client) {|c,o| ClassB }
-            plugin3 = double('plugin3')
-            plugin3.stub(:construct_client) {|c,o| ClassC }
-
-            client_class.add_plugin(plugin1)
-            client_class.add_plugin(plugin2)
-            client_class.add_plugin(plugin3)
-
-            expect(client_class.client_class).to eq ClassC
-          end
-        end
-
-        describe 'applying plugins' do
-
-          it 'instructs plugins to #initialize_client' do
-            initialized_client = nil
-            plugin = double('plugin')
-            plugin.stub(:initialize_client) {|c| initialized_client = c }
-            client_class.add_plugin(plugin)
-            client = client_class.new
-            expect(initialized_client).to be(client)
-          end
-
-          it 'instructs plugins to #add_options' do
-            plugin = double('plugin')
-            plugin.stub(:add_options) { |config| config.add_option(:foo) }
-            client_class.add_plugin(plugin)
-            expect(client_class.new.config).to respond_to(:foo)
-          end
-
-          it 'instructs plugins to #add_handlers' do
-            plugin = double('plugin')
-            expect(plugin).to receive(:is_a?).
-              at_least(:once).with(kind_of(Class)) { false }
-            expect(plugin).to receive(:add_handlers).with(
-              kind_of(HandlerList), kind_of(Configuration))
-            client_class.add_plugin(plugin)
-            client_class.new
-          end
-
-          it 'does not call methods that plugin does not respond to' do
-            plugin = Object.new
-            client_class.add_plugin(plugin)
-            client_class.new
-          end
-
-        end
       end
     end
   end
