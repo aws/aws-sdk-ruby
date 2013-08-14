ef3f083bf9fc8de2e3f2b6eda375a38ba294c727
Trevor Rowe <trevorrowe@gmail.com>
2013-08-13 22:37:02 -0700

    Documented the full interface for adding Handlers.

--- lib/seahorse/client/handler_list.rb
+++ lib/seahorse/client/handler_list.rb
@@ -28,8 +28,73 @@ module Seahorse
         @handlers[:validate] = []
       end
 
-      # @param [Class] handler_class
-      # @option options [Symbol] step (:build)
+      # Registers a handler.  Handlers are used to build a handler stack.
+      # Handlers default to the `:build` step with default priority of 50.
+      # The step and priority determine where in the stack a handler
+      # will be.
+      #
+      # ## Handler Stack Ordering
+      #
+      # A handler stack is built from the inside-out.  The stack is
+      # seeded with the send handler.  Handlers are constructed recursively
+      # in reverse step and priority order so that the highest priority
+      # handler is on the outside.
+      #
+      # By constructing the stack from the inside-out, this ensures
+      # that the validate handlers will be called first and the sign handlers
+      # will be called just before the final and only send handler is called.
+      #
+      # ## Steps
+      #
+      # Handlers are ordered first by step.  These steps represent the
+      # life-cycle of a request.  Valid steps are:
+      #
+      # * `:validate`
+      # * `:build`
+      # * `:sign`
+      # * `:send`
+      #
+      # Many handlers can be added to the same step, except for `:send`.
+      # There can be only one `:send` handler.  Adding an additional
+      # `:send` handler replaces the previous one.
+      #
+      # ## Priorities
+      #
+      # Handlers within a single step are executed in priority order.  The
+      # higher the priority, the earlier in the stack the handler will
+      # be called.
+      #
+      # * Handler priority is an integer between 0 and 99, inclusively.
+      # * Handler priority defaults to 50.
+      # * When multiple handlers are added to the same step with the same
+      #   priority, the last one added will have the highest priority and
+      #   the first one added will have the lowest priority.
+      #
+      # @param [Class<Handler>] handler_class This should be a subclass
+      #   of {Handler} or any class that construct an object that
+      #   responds to `#call`.
+      #
+      # @option options [Symbol] :step (:build) The request lifecycle
+      #   step the handler should run in.  Defaults to `:build`.  Valid
+      #   steps include:
+      #
+      #   * `:validate`
+      #   * `:build`
+      #   * `:sign`
+      #   * `:send`
+      #
+      #   There can only be one send handler. Registering an additional
+      #   `:send` handler replaces the previous one.
+      #
+      # @option options [Integer] :priority (50) The priority of this
+      #   handler within a step.  The priority must be between 0 and 99
+      #   inclusively.  It defaults to 50.  When two handlers have the
+      #   same `:step` and `:priority`, the handler registered last has
+      #   the highest priority.
+      #
+      # @note There can be only one `:send` handler.  Adding an additional
+      #   send handler replaces the previous.
+      #
       def add(handler_class, options = {})
         @handlers[options[:step] || :build].unshift(handler_class)
       end
