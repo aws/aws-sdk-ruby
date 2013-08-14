5ba3aaf8f99e60386e481a0418eeb31dce50e63b
Trevor Rowe <trevorrowe@gmail.com>
2013-08-14 01:31:37 -0700

    Grouped specs for HandlerList#add.

--- spec/seahorse/client/handler_list_spec.rb
+++ spec/seahorse/client/handler_list_spec.rb
@@ -39,84 +39,100 @@ module Seahorse
           expect(handlers.to_a).to eq([handler1, handler2])
         end
 
-        it 'sorts handlers by step priority order' do
-          handlers.add('validate', step: :validate)
-          handlers.add('build', step: :build)
-          handlers.add('sign', step: :sign)
-          handlers.add('send', step: :send)
-          expect(handlers.to_a).to eq(%w(send sign build validate))
+        describe ':step' do
+
+          it 'sorts handlers by step priority order' do
+            handlers.add('validate', step: :validate)
+            handlers.add('build', step: :build)
+            handlers.add('sign', step: :sign)
+            handlers.add('send', step: :send)
+            expect(handlers.to_a).to eq(%w(send sign build validate))
+          end
+
+          it 'defaults step to :build' do
+            handlers.add('validate', step: :validate)
+            handlers.add('build')
+            handlers.add('sign', step: :sign)
+            expect(handlers.to_a).to eq(['sign', 'build', 'validate'])
+          end
+
+          it 'accepts multiple handlers with the same step' do
+            handlers.add('h1', step: :validate)
+            handlers.add('h2', step: :validate)
+            handlers.add('h3', step: :build)
+            handlers.add('h4', step: :build)
+            expect(handlers.to_a).to eq(%w(h3 h4 h1 h2))
+          end
+
         end
 
-        it 'defaults step to :build' do
-          handlers.add('validate', step: :validate)
-          handlers.add('build')
-          handlers.add('sign', step: :sign)
-          expect(handlers.to_a).to eq(['sign', 'build', 'validate'])
+        describe ':priority' do
+
+          it 'accepts a priority' do
+            handlers.add('medium', priority: 50)
+            handlers.add('high', priority: 80)
+            handlers.add('low', priority: 20)
+            expect(handlers.to_a).to eq(%w(low medium high))
+          end
+
+          it 'sorts handler with the same priority in FIFO order' do
+            handlers.add('a', priority: 20, step: :sign)
+            handlers.add('b', priority: 20, step: :sign)
+            handlers.add('c', priority: 20, step: :sign)
+            handlers.add('m', priority: 20, step: :build)
+            handlers.add('n', priority: 20, step: :build)
+            handlers.add('o', priority: 20, step: :build)
+            handlers.add('x', priority: 20, step: :validate)
+            handlers.add('y', priority: 20, step: :validate)
+            handlers.add('z', priority: 20, step: :validate)
+            handlers.add('-', step: :send)
+            expect(handlers.to_a).to eq(%w(- a b c m n o x y z))
+          end
+
         end
 
-        it 'accepts multiple handlers with the same step' do
-          handlers.add('h1', step: :validate)
-          handlers.add('h2', step: :validate)
-          handlers.add('h3', step: :build)
-          handlers.add('h4', step: :build)
-          expect(handlers.to_a).to eq(%w(h3 h4 h1 h2))
+        describe 'errors' do
+
+          it 'raises an error if :step is not valid' do
+            msg = "invalid :step `:bogus', must be one of :validate, :build, "
+            msg << ":sign or :send"
+            expect {
+              handlers.add('handler', step: :bogus)
+            }.to raise_error(ArgumentError, msg)
+          end
+
+          it 'raises an error if :priority is less than 0' do
+            msg = "invalid :priority `-1', must be between 0 and 99"
+            expect {
+              handlers.add('handler', priority: -1)
+            }.to raise_error(ArgumentError, msg)
+          end
+
+          it 'raises an error if :priority is greater than 99' do
+            msg = "invalid :priority `100', must be between 0 and 99"
+            expect {
+              handlers.add('handler', priority: 100)
+            }.to raise_error(ArgumentError, msg)
+          end
+
         end
 
-        it 'accepts a priority' do
-          handlers.add('medium', priority: 50)
-          handlers.add('high', priority: 80)
-          handlers.add('low', priority: 20)
-          expect(handlers.to_a).to eq(%w(low medium high))
-        end
+        describe ':step => :send' do
 
-        it 'sorts handler with the same priority in FIFO order' do
-          handlers.add('a', priority: 20, step: :sign)
-          handlers.add('b', priority: 20, step: :sign)
-          handlers.add('c', priority: 20, step: :sign)
-          handlers.add('m', priority: 20, step: :build)
-          handlers.add('n', priority: 20, step: :build)
-          handlers.add('o', priority: 20, step: :build)
-          handlers.add('x', priority: 20, step: :validate)
-          handlers.add('y', priority: 20, step: :validate)
-          handlers.add('z', priority: 20, step: :validate)
-          handlers.add('-', step: :send)
-          expect(handlers.to_a).to eq(%w(- a b c m n o x y z))
-        end
+          it 'only keeps the latest send handler' do
+            handlers.add('handler1', step: :send)
+            handlers.add('handler2', step: :send)
+            handlers.add('handler3', step: :send)
+            expect(handlers.to_a).to eq(['handler3'])
+          end
 
-        it 'raises an error if :step is not valid' do
-          msg = "invalid :step `:bogus', must be one of :validate, :build, "
-          msg << ":sign or :send"
-          expect {
-            handlers.add('handler', step: :bogus)
-          }.to raise_error(ArgumentError, msg)
-        end
+          it 'ignores :priority when adding :send handlers' do
+            handlers.add('handler1', step: :send, priority: 80)
+            handlers.add('handler2', step: :send, priority: 50)
+            handlers.add('handler3', step: :send, priority: 20)
+            expect(handlers.to_a).to eq(['handler3'])
+          end
 
-        it 'raises an error if :priority is less than 0' do
-          msg = "invalid :priority `-1', must be between 0 and 99"
-          expect {
-            handlers.add('handler', priority: -1)
-          }.to raise_error(ArgumentError, msg)
-        end
-
-        it 'raises an error if :priority is greater than 99' do
-          msg = "invalid :priority `100', must be between 0 and 99"
-          expect {
-            handlers.add('handler', priority: 100)
-          }.to raise_error(ArgumentError, msg)
-        end
-
-        it 'only keeps the latest send handler' do
-          handlers.add('handler1', step: :send)
-          handlers.add('handler2', step: :send)
-          handlers.add('handler3', step: :send)
-          expect(handlers.to_a).to eq(['handler3'])
-        end
-
-        it 'ignores :priority when adding :send handlers' do
-          handlers.add('handler1', step: :send, priority: 80)
-          handlers.add('handler2', step: :send, priority: 50)
-          handlers.add('handler3', step: :send, priority: 20)
-          expect(handlers.to_a).to eq(['handler3'])
         end
 
       end
