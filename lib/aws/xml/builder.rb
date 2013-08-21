29bebd53ff91e6b3ffc1d6871968b97b31c3f502
Trevor Rowe <trevorrowe@gmail.com>
2013-08-21 10:35:48 -0700

    Updated the Xml::Builder, adding tests.
    Still incomplete, it handles basic xml building.  No support
    yet for attributes, maps, etc.

--- lib/aws/xml/builder.rb
+++ lib/aws/xml/builder.rb
@@ -11,57 +11,155 @@
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-
-require 'seahorse'
 require 'builder'
+require 'seahorse'
 
 module Aws
   module Xml
     class Builder
+
       include Seahorse::Model::Shapes
 
-      def initialize(builder = ::Builder::XmlMarkup.new)
-        @builder = builder
-      end
-
-      def build(data, shape)
-        build_shape(data, shape, shape.shape_name || "ROOT")
+      def to_xml(shape, value)
+        xml = []
+        builder = ::Builder::XmlMarkup.new(target: xml, indent: 2)
+        structure(shape.xmlname, shape, value, builder)
+        xml.join
       end
 
       private
 
-      def build_shape(data, shape, name)
-        meth = case data
-        when Hash; MapShape === shape ? :build_map : :build_structure
-        when Array; :build_list
-        else :build_scalar
-        end
-
-        send(meth, data, shape, name)
-      end
-
-      def build_structure(data, shape, name)
-        @builder.send(name) do
-          data.each do |key, value|
-            build_shape(value, shape ? shape.members[key] : nil, key)
+      def structure(name, shape, values, xml)
+        if values.empty?
+          xml.send(name)
+        else
+          xml.send(name) do
+            shape.members.each_pair do |member_name, member_shape|
+              if values.key?(member_name)
+                mname = member_shape.xmlname || member_name
+                member(mname, member_shape, values[member_name], xml)
+              end
+            end
           end
         end
       end
 
-      def build_list(data, shape, name)
-        @builder.send(name) do
-          data.each.with_index do |value, i|
-            name = shape && shape.members.xmlname || 'Item'
-            build_shape(value, shape ? shape.members : nil, name)
+      def list(name, shape, values, xml)
+        if shape.flattened
+          values.each do |value|
+            member(name, shape.members, value, xml)
+          end
+        else
+          xml.send(name) do
+            values.each do |value|
+              member(shape.members.xmlname || 'member', shape.members, value, xml)
+            end
           end
         end
       end
 
-      def build_map(data, shape, name)
+      def member(name, shape, value, xml)
+        case shape
+        when StructureShape then structure(name, shape, value, xml)
+        when ListShape then list(name, shape, value, xml)
+        else scalar(name, shape, value, xml)
+        end
       end
 
-      def build_scalar(data, shape, name)
-        @builder.send(name, data.to_s)
+      def scalar(name, shape, value, xml)
+        xml.send(name, value)
+      end
+
+      def node(name, shape, value = nil, &block)
+        args = []
+        args << name
+        args << value
+        args << attributes(shape)
+        xml.send(*args.compact, &block)
+      end
+
+      # @api private
+      class XmlDoc
+
+        # @option options [#<<] :target ('')
+        # @option options [String] :pad ('')
+        # @option options [String] :indent ('')
+        def initialize(options = {})
+          @target = options[:target] || ''
+          @indent = options[:indent] || ''
+          @pad = options[:pad] || ''
+          @end_of_line = @indent == '' ? '' : "\n"
+        end
+
+        attr_reader :target
+
+        # @overload node(name, attributes = {})
+        #   Adds a self closing element without any content.
+        #
+        # @overload node(name, value, attributes = {})
+        #   Adds an element that opens and closes on the same line with
+        #   simple text content.
+        #
+        # @overload node(name, attributes = {}, &block)
+        #   Adds a wrapping element.  Calling {#node} from inside
+        #   the yielded block creates nested elements.
+        #
+        # @return [void]
+        #
+        def node(name, *args, &block)
+          attrs = args.last.is_a?(Hash) ? args.pop : {}
+          if block_given?
+            @target << open_el(name, attrs)
+            @target << @end_of_line
+            increase_pad { yield }
+            @target << @pad
+            @target << close_el(name)
+          elsif args.empty?
+            @target << empty_element(name, attrs)
+          else
+            @target << inline_element(name, args.first, attrs)
+          end
+        end
+
+        private
+
+        def empty_element(name, attrs)
+          "#{@pad}<#{name}#{attributes(attrs)}/>#{@end_of_line}"
+        end
+
+        def inline_element(name, value, attrs)
+          "#{open_el(name, attrs)}#{escape(value)}#{close_el(name)}"
+        end
+
+        def open_el(name, attrs)
+          "#{@pad}<#{name}#{attributes(attrs)}>"
+        end
+
+        def close_el(name)
+          "</#{name}>#{@end_of_line}"
+        end
+
+        def escape(string)
+          string.to_s
+        end
+
+        def attributes(attr)
+          if attr.empty?
+            ''
+          else
+            ' ' + attr.map do |key, value|
+              "#{key}=\"#{escape(value).gsub('"', '&quot;')}\""
+            end.join(' ')
+          end
+        end
+
+        def increase_pad(&block)
+          pre_increase = @pad
+          @pad = @pad + @indent
+          yield
+          @pad = pre_increase
+        end
+
       end
     end
   end
---
+++ spec/aws/xml/builder_spec.rb
@@ -0,0 +1,239 @@
+# Copyright 2011-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+require 'spec_helper'
+
+module Aws
+  module Xml
+    describe Builder do
+
+      let(:rules) {{
+        'type' => 'structure',
+        'xmlname' => 'xml',
+        'members' => {},
+      }}
+
+      def xml(params)
+        shape = Seahorse::Model::Shapes::Shape.from_hash(rules)
+        Builder.new.to_xml(shape, params)
+      end
+
+      describe 'structures' do
+
+        it 'returns an empty xml doc when there are no params' do
+          expect(xml({})).to eq(<<-XML)
+<xml/>
+          XML
+        end
+
+        it 'omits members that are not present in the params' do
+          rules['members'] = {
+            'abc' => { 'type' => 'string' },
+            'mno' => { 'type' => 'string' },
+          }
+          expect(xml({})).to eq(<<-XML)
+<xml/>
+          XML
+        end
+
+        it 'includes members that are present in the params' do
+          rules['members'] = {
+            'abc' => { 'type' => 'string' },
+            'mno' => { 'type' => 'string' },
+          }
+          expect(xml(:mno => 'value')).to eq(<<-XML)
+<xml>
+  <mno>value</mno>
+</xml>
+          XML
+        end
+
+        it 'orders members by rule order, not param order' do
+          rules['members'] = {
+            'mno' => { 'type' => 'string' },
+            'abc' => { 'type' => 'string' },
+          }
+          expect(xml(:abc => 'v2', :mno => 'v1')).to eq(<<-XML)
+<xml>
+  <mno>v1</mno>
+  <abc>v2</abc>
+</xml>
+          XML
+        end
+
+        it 'serializes nested structures' do
+          rules['members'] = {
+            'name' => { 'type' => 'string' },
+            'details' => {
+              'type' => 'structure',
+              'members' => {
+                'size' => { 'type' => 'string' },
+                'color' => { 'type' => 'string' },
+              }
+            },
+          }
+          params = {
+            name: 'Name',
+            details: {
+              size: 'large',
+              color: 'red',
+            },
+          }
+          expect(xml(params)).to eq(<<-XML)
+<xml>
+  <name>Name</name>
+  <details>
+    <size>large</size>
+    <color>red</color>
+  </details>
+</xml>
+          XML
+        end
+
+      end
+
+      describe 'non-flattened lists' do
+
+        it 'serializes lists members as "member" by default' do
+          rules['members'] = {
+            'items' => {
+              'type' => 'list',
+              'members' => { 'type' => 'string' }
+            }
+          }
+          expect(xml(items: %w(abc xyz))).to eq(<<-XML)
+<xml>
+  <items>
+    <member>abc</member>
+    <member>xyz</member>
+  </items>
+</xml>
+          XML
+        end
+
+        it 'applies xmlnames to the list wrapper and member entries' do
+          rules['members'] = {
+            'items' => {
+              'type' => 'list',
+              'xmlname' => 'ItemList',
+              'members' => { 'type' => 'string', 'xmlname' => 'Item' }
+            }
+          }
+          expect(xml(items: %w(abc xyz))).to eq(<<-XML)
+<xml>
+  <ItemList>
+    <Item>abc</Item>
+    <Item>xyz</Item>
+  </ItemList>
+</xml>
+          XML
+        end
+
+        it 'can serialize lists of complex types' do
+          rules['members'] = {
+            'items' => {
+              'type' => 'list',
+              'members' => {
+                'type' => 'structure',
+                'members' => {
+                  'name' => { 'type' => 'string' }
+                }
+              }
+            }
+          }
+          expect(xml(items: [{ name: 'abc' }, { name: 'mno' }])).to eq(<<-XML)
+<xml>
+  <items>
+    <member>
+      <name>abc</name>
+    </member>
+    <member>
+      <name>mno</name>
+    </member>
+  </items>
+</xml>
+          XML
+        end
+
+      end
+
+      describe 'flattened lists' do
+
+        it 'serializes lists without a wrapping element' do
+          rules['members'] = {
+            'items' => {
+              'type' => 'list',
+              'flattened' => true,
+              'members' => { 'type' => 'string' }
+            }
+          }
+          expect(xml(items: %w(abc mno xyz))).to eq(<<-XML)
+<xml>
+  <items>abc</items>
+  <items>mno</items>
+  <items>xyz</items>
+</xml>
+          XML
+        end
+
+        it 'applies xmlnames to the list elements' do
+          rules['members'] = {
+            'items' => {
+              'type' => 'list',
+              'flattened' => true,
+              'xmlname' => 'item',
+              'members' => { 'type' => 'string' }
+            }
+          }
+          expect(xml(items: %w(abc mno xyz))).to eq(<<-XML)
+<xml>
+  <item>abc</item>
+  <item>mno</item>
+  <item>xyz</item>
+</xml>
+          XML
+        end
+
+        it 'can serialize a list of complex types' do
+          rules['members'] = {
+            'items' => {
+              'type' => 'list',
+              'flattened' => true,
+              'xmlname' => 'item',
+              'members' => {
+                'type' => 'structure',
+                'members' => {
+                  'name' => { 'type' => 'string' }
+                }
+              }
+            }
+          }
+          expect(xml(items: [{ name: 'abc' }, { name: 'mno' }])).to eq(<<-XML)
+<xml>
+  <item>
+    <name>abc</name>
+  </item>
+  <item>
+    <name>mno</name>
+  </item>
+</xml>
+          XML
+        end
+
+      end
+
+      describe 'scalars' do
+      end
+    end
+  end
+end
