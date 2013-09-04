717172dd4c5ffac6d4721fec9661f881969d22ed
Trevor Rowe <trevorrowe@gmail.com>
2013-09-04 00:22:30 -0700

    Added skeleton Query::Builder class and spec.

---
+++ lib/aws/query/builder.rb
@@ -0,0 +1,38 @@
+# Copyright 2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+module Aws
+  module Query
+    class Builder
+
+      # @param [Seahorse::Model::Shapes::Shape] rules
+      def initialize(rules)
+        @rules = rules
+      end
+
+      # @param [Hash] params
+      # @return [ParamList]
+      def to_query_params(params)
+        list = ParamList.new
+      end
+
+      # @param [Seahorse::Model::Shapes::Shape] rules
+      # @param [Hash] params
+      # @return [ParamList]
+      def self.to_query_params(rules, params)
+        new(rules).to_query_params(params)
+      end
+
+    end
+  end
+end
---
+++ spec/aws/query/builder_spec.rb
@@ -0,0 +1,57 @@
+# Copyright 2011-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+require 'spec_helper'
+
+module Aws
+  module Query
+    describe Builder do
+
+      let(:rules) {{
+        'type' => 'input',
+        'members' => {}
+      }}
+
+      def list(params = {})
+        shape = Seahorse::Model::Shapes::Shape.from_hash(rules)
+        Builder.to_query_params(shape, params)
+      end
+
+      describe '#to_query_params' do
+
+        it 'returns a param list' do
+          expect(list).to be_kind_of(ParamList)
+        end
+
+        describe 'structures' do
+        end
+
+        describe 'flattened lists' do
+        end
+
+        describe 'non-flattened lists' do
+        end
+
+        describe 'flattened maps' do
+        end
+
+        describe 'non-flattened maps' do
+        end
+
+        describe 'scalars' do
+        end
+
+      end
+    end
+  end
+end
