d826f9f446fa9420f714153d3de0e42af517c001
Trevor Rowe <trevorrowe@gmail.com>
2013-09-04 23:22:00 -0700

    All aws services now apply the Rest plugin.

--- lib/aws/api_translator.rb
+++ lib/aws/api_translator.rb
@@ -136,7 +136,7 @@ module Aws
       plugins << 'Aws::Plugins::RegionalEndpoint'
       plugins << 'Aws::Plugins::EnvironmentCredentials'
       plugins << 'Aws::Plugins::ContentLength'
-      plugins << 'Aws::Plugins::RestProtocol' #if type =~ /rest/
+      plugins << 'Aws::Plugins::RestProtocol'
       plugins <<
         case type
         when 'query' then 'Aws::Plugins::QuerySerializer'
