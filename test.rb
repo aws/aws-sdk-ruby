9f7a7ddb178507930d1b07b955887cad9a14f901
Trevor Rowe <trevorrowe@gmail.com>
2013-09-09 23:32:31 -0700

    The test.rb script now uses the service helper methods.

--- test.rb
+++ test.rb
@@ -15,34 +15,6 @@ require 'bundler/setup'
 require 'aws-sdk-core'
 require 'pp'
 
-swf = Aws::SWF::Client.new
-ddb = Aws::DynamoDB::Client.new
-emr = Aws::EMR::Client.new
-cloudfront = Aws::CloudFront::Client.new
-s3 = Aws::S3::Client.new
-opsworks = Aws::OpsWorks::Client.new
-ec2 = Aws::EC2::Client.new
-
-#resp = s3.put_object Bucket: 'lorenfoo', Key: 'foo', Body: 'hello', ContentType: 'text/plain'
-#pp resp.http_request
-# resp = s3.get_object Bucket: 'lorenfoo', Key: 'foo'
-#pp resp.data
-
-#pp s3.put_bucket_logging
-
-# cloudfront.create_distribution2013_05_12(DistributionConfig: {
-#   CallerReference: "Foo",
-#   Enabled: true,
-#   Aliases: {
-#     Quantity: 1,
-#     Items: [
-#       "Foo", "Bar", "Baz"
-#     ]
-#   }
-# }).data
-
-# pp opsworks.describe_stacks.data
-
-pp ec2.describe_instances.data
-pp swf.list_domains(registration_status: 'DEPRECATED').data
-pp ddb.list_tables.data
+pp Aws.ec2.client.describe_instances.data
+pp Aws.swf.client.list_domains(registration_status: 'DEPRECATED').data
+pp Aws.dynamo_db.client.list_tables.data
