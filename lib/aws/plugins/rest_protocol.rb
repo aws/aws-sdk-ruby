e24dc29faf1293527d6a0e78a37c45d0d408449e
Trevor Rowe <trevorrowe@gmail.com>
2013-09-11 23:29:00 -0700

    Fleshed out the RestProtocol plugin more.
    Added support for:
    * typed header values
    * header maps (sending and receiving)

--- lib/aws/plugins/rest_protocol.rb
+++ lib/aws/plugins/rest_protocol.rb
@@ -14,60 +14,128 @@
 module Aws
   module Plugins
     class RestProtocol < Seahorse::Client::Plugin
-
       class Handler < Seahorse::Client::Handler
 
+        include Seahorse::Model::Shapes
+
         def call(context)
-          context.http_request.http_method = context.operation.http_method
-          context.http_request.path = interpolated_uri(context)
-          build_input_params(context)
-          handler.call(context).on_complete do |response|
-            response.data = {}
-            build_output_data(context, response)
+          build_request(context)
+          @handler.call(context).on_complete do |response|
+            parse_response(response)
           end
         end
 
         private
 
-        def interpolated_uri(context)
-          path = context.operation.http_path
+        def build_request(context)
+          populate_method(context)
+          populate_path(context)
+          populated_headers_and_body(context)
+        end
+
+        def populate_method(context)
+          context.http_request.http_method = context.operation.http_method
+        end
+
+        def populate_path(context)
+          context.http_request.path =
+            interpolate_path(context.operation.http_path, context.params)
+        end
+
+        def interpolate_path(path, params)
           path = path.gsub(/([^&\?= ]+?)=\{(.+?)\}(?:&|$)/) do
-            value = context.params[$2.to_sym]
+            value = params[$2.to_sym]
             value ? "#{$1}=#{value}" : ""
           end
-          path = path.gsub(/\{(.+?)\}/) { context.params[$1.to_sym] }
+          path = path.gsub(/\{(.+?)\}/) { params[$1.to_sym] }
           path
         end
 
-        def build_input_params(context)
-          # add all top-level headers
-          context.params.each do |key, value|
-            input = context.operation.input
-            next unless member = input.members[key]
-            if member.location == 'header'
-              context.http_request.headers[member.serialized_name] = value
-            elsif input.raw_payload
-              context.http_request.body = input.body_members.values.first
-            end
-          end
-        end
-
-        def build_output_data(context, response)
-          return if context.operation.output.nil?
-
-          # add all top-level headers
-          context.operation.output.members.each do |name, member|
-            if member.location == 'header'
-              header_name = member.serialized_name
-              if response.http_response.headers.has_key?(header_name)
-                response.data[name.to_sym] =
-                  response.http_response.headers[header_name]
+        def populated_headers_and_body(context)
+          if rules = context.operation.input
+            context.params.each do |param_name, value|
+              rule = rules.members[param_name]
+              if rule.location == 'header'
+                build_header(rule, context.http_request.headers, value)
+              elsif rule.metadata['streaming']
+                context.http_request.body = value
               end
-            else
-              response.data[name.to_sym] = response.http_response.body
             end
           end
         end
+
+        def build_header(rule, headers, value)
+          if rule.is_a?(MapShape)
+            build_header_map(rule, headers, value)
+          else
+            headers[rule.serialized_name] = cast_header_value(rule, value)
+          end
+        end
+
+        def build_header_map(shape, headers, hash)
+          hash.each_pair do |suffix, value|
+            headers["#{shape.serialized_name}#{suffix}"] =
+              cast_header_value(shape.members, value)
+          end
+        end
+
+        def cast_header_value(rule, value)
+          case shape
+            when UnixTimestampShape then value.to_i.to_s
+            when Rfc822TimestampShape then value.utc.iso8601
+            when Iso8601TimestampShape then value.utc.rfc822
+            else value.to_s
+          end
+        end
+
+        def parse_response(response)
+          if rules = response.context.operation.output
+            response.data = Structure.new(rules.members.keys)
+            extract_headers_and_body(rules, response)
+          end
+        end
+
+        def extract_headers_and_body(rules, response)
+          headers = response.http_response.headers
+          rules.members.each do |member_name, member|
+            if member.location == 'header'
+              response.data[member_name] = header_value(member, headers)
+            elsif member.metadata['streaming']
+              response.data[member_name] = response.http_response.body
+            end
+          end
+        end
+
+        def header_value(shape, headers)
+          if shape.is_a?(MapShape)
+            header_map(shape, headers)
+          else
+            cast_header_value(shape, headers[shape.serialized_name])
+          end
+        end
+
+        def header_map(shape, headers)
+          hash = {}
+          headers.each do |header, value|
+            if match = header.match(/^#{shape.serialized_name}(.+)/)
+              hash[match[1]] = cast_header_value(shape.members, value)
+            end
+          end
+          hash
+        end
+
+        def cast_header_value(shape, value)
+          if value
+            case shape
+            when UnixTimestampShape then Time.at(value.to_i)
+            when TimestampShape then Time.parse(value)
+            when IntegerShape then value.to_i
+            when FloatShape then value.to_f
+            else value
+            end
+          end
+        end
+
       end
 
       handle(Handler)
