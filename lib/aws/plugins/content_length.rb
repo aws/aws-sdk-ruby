bfd3b350c7ae0a1e79e2b97aabe4a4cb171346c5
Trevor Rowe <trevorrowe@gmail.com>
2013-09-17 08:48:37 -0700

    The content-length plugin now defines a concrete handler class.
    This makes it easier to test.

--- lib/aws/plugins/content_length.rb
+++ lib/aws/plugins/content_length.rb
@@ -16,11 +16,22 @@ require 'multi_xml'
 module Aws
   module Plugins
     class ContentLength < Seahorse::Client::Plugin
-      handle_request(:Handler, step: :sign, priority: 0) do |context|
-        next if context.http_request.headers['Content-Length']
-        length = context.http_request.body.size
-        context.http_request.headers['Content-Length'] = length
+
+      # @api private
+      class Handler < Seahorse::Client::Handler
+
+        def call(context)
+          unless context.http_request.headers['Content-Length']
+            length = context.http_request.body.size
+            context.http_request.headers['Content-Length'] = length
+          end
+          @handler.call(context)
+        end
+
       end
+
+      handler(Handler, step: :sign, priority: 0)
+
     end
   end
 end
