47d5d5414aca4dd7675811bd46616a571f7dea0d
Felipe Garcia <felipecpg@gmail.com>
2013-09-22 18:51:54 -0300

    removing require

--- lib/aws/record/naming.rb
+++ lib/aws/record/naming.rb
@@ -16,8 +16,6 @@ module AWS
 
     # @api private
     module Naming
-
-      require 'active_model' unless defined?(ActiveModel)
       # This method should only ever get called in a Rails 3+ context
       # where active model and active support have been loaded.  Rails 2
       # does not call model name on object.
