f6e387a1f347a6e71babd9341d9a7f9c09bb16fd
Trevor Rowe <trevorrowe@gmail.com>
2013-09-30 08:43:23 -0700

    Simplified the query request handler.

--- lib/aws/query/handler.rb
+++ lib/aws/query/handler.rb
@@ -3,17 +3,19 @@ module Aws
     class Handler < Xml::Handler
 
       def build_request(context)
-        context.http_request.headers['Content-Type'] =
-          'application/x-www-form-urlencoded; charset=utf-8'
-
-        param_list = Query::Builder.to_query_params(
-          context.operation.input,
-          context.params)
-
+        param_list = build_param_list(context)
         param_list.set('Version', context.config.api.version)
         param_list.set('Action', context.operation.name)
-
+        context.http_request.headers['Content-Type'] = content_type
         context.http_request.body = param_list.to_io
+      end
+
+      def build_param_list(context)
+        Query::Builder.to_query_params(context.operation.input, context.params)
+      end
+
+      def content_type
+        'application/x-www-form-urlencoded; charset=utf-8'
       end
 
     end
