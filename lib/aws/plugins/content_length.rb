a41e8a975309eaa953cc21c3281115ec3f173bad
Trevor Rowe <trevorrowe@gmail.com>
2013-09-30 09:19:30 -0700

    The ContentLength plugin can now be constructed with a specific step and priority.

--- lib/aws/plugins/content_length.rb
+++ lib/aws/plugins/content_length.rb
@@ -4,21 +4,28 @@ module Aws
   module Plugins
     class ContentLength < Seahorse::Client::Plugin
 
+      def initialize(options = {})
+        @step = options[:step] || :sign
+        @priority = options[:priority] || 0
+      end
+
+      def add_handlers(handlers, config)
+        handlers.add(Handler, step: @step, priority: @priority)
+      end
+
       # @api private
       class Handler < Seahorse::Client::Handler
 
         def call(context)
-          unless context.http_request.headers['Content-Length']
-            length = context.http_request.body.size
-            context.http_request.headers['Content-Length'] = length
-          end
+          context.http_request.headers['Content-Length'] = length(context)
           @handler.call(context)
         end
 
+        def length(context)
+          context.http_request.body.size
+        end
+
       end
-
-      handler(Handler, step: :sign, priority: 0)
-
     end
   end
 end
