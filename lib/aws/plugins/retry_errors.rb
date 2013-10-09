72945ae4dca8e2900259539a5203c2207d4b13e9
Trevor Rowe <trevorrowe@gmail.com>
2013-10-08 23:56:26 -0700

    Added an error inspector that the retry plugin can use to determine what errors are safe to retry.

--- lib/aws/plugins/retry_errors.rb
+++ lib/aws/plugins/retry_errors.rb
@@ -6,6 +6,47 @@ module Aws
 
       option(:max_retries, 3)
 
+      # @api private
+      class ErrorInspector
+
+        EXPIRED_CREDS = Set.new([
+          'InvalidClientTokenId',        # query services
+          'UnrecognizedClientException', # json services
+          'InvalidAccessKeyId',          # s3
+          'AuthFailure',                 # ec2
+        ])
+
+        RETRYABLE_400S = Set.new([
+          'ProvisionedThroughputExceededException',
+          'RequestLimitExceeded',
+          'BandwidthLimitExceeded',
+          'CRC32CheckFailed',
+          'RequestTimeout',
+        ])
+
+        def initialize(error, http_status_code)
+          @error = error
+          @name = error.class.name.split('::').last
+          @http_status_code = http_status_code
+        end
+
+        def expired_credentials?
+          !!(EXPIRED_CREDS.include?(@name) || @name.match(/expired/i))
+        end
+
+        def server_error?
+          (500..599).include?(@http_status_code)
+        end
+
+        def client_error?
+          (400..499).include?(@http_status_code)
+        end
+
+        def networking_error?
+          @http_status_code == 0
+        end
+
+      end
     end
   end
 end
--- spec/aws/plugins/retry_errors_spec.rb
+++ spec/aws/plugins/retry_errors_spec.rb
@@ -11,6 +11,92 @@ module Aws
         expect(config.max_retries).to eq(3)
       end
 
+      describe 'ErrorInspector' do
+
+        def inspector(error, http_status_code = 404)
+          RetryErrors::ErrorInspector.new(error, http_status_code)
+        end
+
+        describe '#client_error?' do
+
+          it 'returns true if the error is a 400 level error' do
+            error = Aws::EC2::Errors::RandomError.new
+            expect(inspector(error, 400).client_error?).to be(true)
+          end
+
+          it 'returns false if the error is not a 500 level error' do
+            error = Aws::EC2::Errors::RandomError.new
+            expect(inspector(error, 500).client_error?).to be(false)
+          end
+
+        end
+
+        describe '#server_error?' do
+
+          it 'returns true if the error is a 500 level error' do
+            error = Aws::EC2::Errors::RandomError.new
+            expect(inspector(error, 500).server_error?).to be(true)
+          end
+
+          it 'returns false if the error is not a 500 level error' do
+            error = Aws::EC2::Errors::RandomError.new
+            expect(inspector(error, 404).server_error?).to be(false)
+          end
+
+        end
+
+        describe '#networking_error?' do
+
+          it 'returns true if the http status code is 0' do
+            error = double('error')
+            expect(inspector(error, 0).networking_error?).to be(true)
+          end
+
+          it 'returns false if the http status code is not 0' do
+            error = double('error')
+            expect(inspector(error, 307).networking_error?).to be(false)
+          end
+
+        end
+
+        describe '#expired_credentials?' do
+
+          # aws query
+          it 'returns true if the error code is InvalidClientTokenId' do
+            error = Aws::IAM::Errors::InvalidClientTokenId.new
+            expect(inspector(error).expired_credentials?).to be(true)
+          end
+
+          # json rpc
+          it 'returns true if the error code is UnrecognizedClientException' do
+            error = Aws::IAM::Errors::UnrecognizedClientException.new
+            expect(inspector(error).expired_credentials?).to be(true)
+          end
+
+          # s3
+          it 'returns true if the error code is InvalidAccessKeyId' do
+            error = Aws::IAM::Errors::InvalidAccessKeyId.new
+            expect(inspector(error).expired_credentials?).to be(true)
+          end
+
+          # ec2
+          it 'returns true if the error code is AuthFailure' do
+            error = Aws::IAM::Errors::AuthFailure.new
+            expect(inspector(error).expired_credentials?).to be(true)
+          end
+
+          it 'returns true if the error code matches /expired/' do
+            error = Aws::IAM::Errors::SomethingExpiredError.new
+            expect(inspector(error).expired_credentials?).to be(true)
+          end
+
+          it 'returns false for other errors' do
+            error = Aws::IAM::Errors::SomeRandomError.new
+            expect(inspector(error).expired_credentials?).to be(false)
+          end
+
+        end
+      end
     end
   end
 end
