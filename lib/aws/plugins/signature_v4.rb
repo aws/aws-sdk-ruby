89c35643536b6c38ba701538fc1778a5bc75e2ed
Trevor Rowe <trevorrowe@gmail.com>
2013-10-17 21:09:32 -0700

    Corrected two typos in the SigV4 documentation.

--- lib/aws/plugins/signature_v4.rb
+++ lib/aws/plugins/signature_v4.rb
@@ -2,9 +2,9 @@ module Aws
   module Plugins
 
     # @seahorse_client_option [String] :sigv4_name
-    #   Override the default service named used for signing sigv4 requests.
+    #   Override the default service name used for signing sigv4 requests.
     # @seahorse_client_option [String] :sigv4_region
-    #   Override the default region named used for signing sigv4 requests.
+    #   Override the default region name used for signing sigv4 requests.
     class SignatureV4 < Seahorse::Client::Plugin
 
       option(:sigv4_name) do |config|
