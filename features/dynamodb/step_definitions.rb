c3affcb0eca0b6a50eb4e70df0aca39966b25e8c
Trevor Rowe <trevorrowe@gmail.com>
2013-10-19 00:39:15 -0700

    Removed a few duplicate step definitions.

--- features/dynamodb/step_definitions.rb
+++ features/dynamodb/step_definitions.rb
@@ -1,19 +1,3 @@
 Given(/^I disable translation features Aws::DynamoDB$/) do
   @dynamodb = Aws.dynamodb(raw_json: true)
 end
-
-Then(/^the HTTP request body should be:$/) do |string|
-  expect(@response.http_request.body_contents).to eq(string)
-end
-
-Then(/^I expect response data to be a hash$/) do
-  expect(@response.data).to be_a(Hash)
-end
-
-Then(/^I expect response data\["(.*?)"\] to be an array$/) do |key|
-  expect(@response.data[key]).to be_an(Array)
-end
-
-Then(/^I expect the response error to be "(.*?)"$/) do |error_class|
-  expect(@error.class.name).to match(error_class)
-end
