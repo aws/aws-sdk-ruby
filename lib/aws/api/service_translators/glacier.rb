41122efeee6fe8375a01cd4d60c6e7dd9ae60146
Trevor Rowe <trevorrowe@gmail.com>
2013-10-23 14:02:00 -0700

    Converting a few shapes from strings to integers for glacier params.

--- lib/aws/api/service_translators/glacier.rb
+++ lib/aws/api/service_translators/glacier.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module Aws::Api::ServiceTranslators::Glacier
 
   extend Aws::Api::Visitor
@@ -5,21 +7,26 @@ module Aws::Api::ServiceTranslators::Glacier
   class << self
 
     def translate(api)
-      convert_dates(api)
       remove_checksum(api)
-    end
-
-    # convert all of the "string" date shapes to timestamps
-    def convert_dates(api)
-      each_shape(api) do |shape_name, shape|
-        shape.type = 'iso8601_timestamp' if shape.serialized_name =~ /date/i
-      end
+      convert_types(api)
     end
 
     # this checksum parameter - it must be calculated on xml that is not user
     # supplied, so the sdk has to compute the checksum after building the xml
     def remove_checksum(api)
       api.operations[:complete_multipart_upload].input.members.delete(:checksum)
+    end
+
+    # Shapes that are bound to headers and the request URI are all typed as
+    # strings, even when they *should* be integers or dates, crawling the
+    # api to correct these
+    def convert_types(api)
+      each_shape(api) do |shape_name, shape|
+        case shape_name
+        when /date/i then shape.type = 'iso8601_timestamp'
+        when :limit, :part_size, :archive_size then shape.type = 'integer'
+        end
+      end
     end
 
   end
