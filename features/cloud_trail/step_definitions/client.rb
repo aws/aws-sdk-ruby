c3052539484d5eff9cec1c4768601038b9bbad71
Alex Wood <awood45@gmail.com>
2013-11-14 10:21:54 -0800

    Initial Support for AWS CloudTrail
    AWS CloudTrail is a web service that records AWS API calls for your account and
    delivers log files to you. The recorded information includes the identity of the
    API caller, the time of the API call, the source IP address of the API caller,
    the request parameters, and the response elements returned by the AWS service.

---
+++ features/cloud_trail/step_definitions/client.rb
@@ -0,0 +1,21 @@
+# Copyright 2011-2013 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+
+When(/^I use the client to describe trails$/) do
+  @response = @cloud_trail_client.describe_trails
+end
+
+Then(/^the result should have an array of trails$/) do
+  @response[:trail_list].should be_an(Array)
+end
