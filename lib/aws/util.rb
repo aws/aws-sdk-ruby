def15787a5757e3375d33ea5217310bdb10910f7
Trevor Rowe <trevorrowe@gmail.com>
2014-02-13 17:38:24 -0800

    Updates Aws::Util#underscore to better address irregular inflections.
    Updated underscore logic to deal better with trailing numbers.  Also
    replaced the map of irregular phrases with a map of irregular words.
    By doing so we can correct entire classes of irregular phrases with
    a single rule.
    
    Also added a suite of unit tests for the inflector which includes
    a map of known irregular inflections.
    
    Fixes #44

--- lib/aws/util.rb
+++ lib/aws/util.rb
@@ -4,28 +4,27 @@ module Aws
   # @api private
   module Util
 
+    # @api private
+    IRREGULAR_INFLECTIONS = {
+      'ARNs' => 'arns',
+      'CNAMEs' => 'cnames',
+      'Ec2' => 'ec2',
+      'ElastiCache' => 'elasticache',
+      'ETag' => 'etag',
+      'iSCSI' => 'iscsi',
+    }
+
+    # @api private
+    IRREGULAR_REGEX = Regexp.new(IRREGULAR_INFLECTIONS.keys.join('|'))
+
     # @param [String] string
     # @return [String] Returns the underscored version of the given string.
     def underscore(string)
-      inflector = Hash.new do |hash, key|
-        key.
-          gsub(/([A-Z0-9]+)([A-Z][a-z])/, '\1_\2'). # split acronyms
-          scan(/[a-z]+|\d+|[A-Z0-9]+[a-z]*/).       # split words
-          join('_').downcase                        # join parts
-      end
-
-      # add a few irregular inflections
-      inflector['ETag'] = 'etag'
-      inflector['s3Bucket'] = 's3_bucket'
-      inflector['s3Key'] = 's3_key'
-      inflector['Ec2KeyName'] = 'ec2_key_name'
-      inflector['Ec2SubnetId'] = 'ec2_subnet_id'
-      inflector['Ec2VolumeId'] = 'ec2_volume_id'
-      inflector['Ec2InstanceId'] = 'ec2_instance_id'
-      inflector['ElastiCache'] = 'elasticache'
-      inflector['NotificationARNs'] = 'notification_arns'
-
-      inflector[string]
+        string.
+          gsub(IRREGULAR_REGEX) { |word| '_' + IRREGULAR_INFLECTIONS[word] }.
+          gsub(/([A-Z0-9]+)([A-Z][a-z])/, '\1_\2').
+          scan(/[a-z0-9]+|\d+|[A-Z0-9]+[a-z]*/).
+          join('_').downcase
     end
     module_function :underscore
 
