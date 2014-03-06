7886566830c7a38938264218eb45235394506941
Trevor Rowe <trevorrowe@gmail.com>
2014-03-06 14:45:50 -0800

    Updated AWS::CloudFront::Client to the 2014-01-31 API version.

--- lib/aws/cloud_front/client.rb
+++ lib/aws/cloud_front/client.rb
@@ -16,7 +16,7 @@ module AWS
 
     class Client < Core::RESTXMLClient
 
-      API_VERSION = '2013-11-22'
+      API_VERSION = '2014-01-31'
 
       signature_version :Version4, 'cloudfront'
 
@@ -43,6 +43,10 @@ module AWS
 
     class Client::V20131122 < Client
       define_client_methods('2013-11-22')
+    end
+
+    class Client::V20140131 < Client
+      define_client_methods('2014-01-31')
     end
 
   end
