e8909536cd58795fbadee17961182044523b3bf3
Trevor Rowe <trevorrowe@gmail.com>
2014-07-03 08:51:00 -0700

    Moved HandlerList::Entry to a its own ruby file.

--- vendor/seahorse/lib/seahorse/client/handler_list.rb
+++ vendor/seahorse/lib/seahorse/client/handler_list.rb
@@ -5,118 +5,6 @@ module Seahorse
   module Client
     class HandlerList
 
-      class InvalidStepError < ArgumentError
-        def initialize(step)
-          msg = "invalid :step `%s', must be one of :initialize, :validate, "
-          msg << ":build, :sign or :send"
-          super(msg % step.inspect)
-        end
-      end
-
-      class InvalidPriorityError < ArgumentError
-        def initialize(priority)
-          msg = "invalid :priority `%s', must be between 0 and 99"
-          super(msg % priority.inspect)
-        end
-      end
-
-      class Entry
-
-        # @api private
-        STEPS = {
-          initialize: 400,
-          validate: 300,
-          build: 200,
-          sign: 100,
-          send: 0,
-        }
-
-        # @option options [required, Class<Handler>] :handler_class
-        # @option options [required, Integer] :inserted The insertion
-        #   order/position. This is used to determine sort order when two
-        #   entries have the same priority.
-        # @option options [Symbol] :step (:build)
-        # @option options [Integer] :priority (50)
-        # @option options [Set<String>] :operations
-        def initialize(options)
-          @options = options
-          @handler_class = option(:handler_class, options)
-          @inserted = option(:inserted, options)
-          @operations = Set.new((options[:operations] || []).map(&:to_s))
-          set_step(options[:step] || :build)
-          set_priority(options[:priority] || 50)
-          compute_weight
-        end
-
-        # @return [Handler, Class<Handler>] Returns the handler.  This may
-        #   be a constructed handler object or a handler class.
-        attr_reader :handler_class
-
-        # @return [Integer] The insertion order/position.  This is used to
-        #   determine sort order when two entries have the same priority.
-        #   Entries inserted later (with a higher inserted value) have a
-        #   lower priority.
-        attr_reader :inserted
-
-        # @return [Symbol]
-        attr_reader :step
-
-        # @return [Integer]
-        attr_reader :priority
-
-        # @return [Set<String>]
-        attr_reader :operations
-
-        # @return [Integer]
-        attr_reader :weight
-
-        # @api private
-        def <=>(other)
-          if weight == other.weight
-            other.inserted <=> inserted
-          else
-            weight <=> other.weight
-          end
-        end
-
-        # @option options (see #initialize)
-        # @return [Entry]
-        def copy(options = {})
-          Entry.new(@options.merge(options))
-        end
-
-        private
-
-        def option(name, options)
-          if options.key?(name)
-            options[name]
-          else
-            raise ArgumentError, "missing required option #{name.inspect}"
-          end
-        end
-
-        def set_step(step)
-          if STEPS.key?(step)
-            @step = step
-          else
-            raise InvalidStepError, step
-          end
-        end
-
-        def set_priority(priority)
-          if (0..99).include?(priority)
-            @priority = priority
-          else
-            raise InvalidPriorityError, priority
-          end
-        end
-
-        def compute_weight
-          @weight = STEPS[@step] + @priority
-        end
-
-      end
-
       include Enumerable
 
       # @api private
@@ -128,7 +16,7 @@ module Seahorse
         add_entries(entries) unless entries.empty?
       end
 
-      # @return [Array<Entry>]
+      # @return [Array<HandlerListEntry>]
       def entries
         @mutex.synchronize do
           @entries.values
@@ -215,7 +103,7 @@ module Seahorse
       def add(handler_class, options = {})
         @mutex.synchronize do
           add_entry(
-            Entry.new(options.merge(
+            HandlerListEntry.new(options.merge(
               handler_class: handler_class,
               inserted: next_index
             ))
