b89600832b867462bd55185bf162ee5979ba4258
Trevor Rowe <trevorrowe@gmail.com>
2014-07-08 09:17:46 -0700

    Large updates on Resource batch documentation.

--- lib/aws/resource/batch.rb
+++ lib/aws/resource/batch.rb
@@ -1,98 +1,42 @@
 module Aws
   module Resource
 
-    # A resource batch provides array like access to a list of resources.
-    # Additionally, it provides the ability to invoke operations against
+    # A batch provides array like access to a list of resources. Batches
+    # also provide the ability to invoke certain operations against
     # the entire batch.
     #
-    #     # S3 object batch
-    #     batch.size #=> 1000
+    # ## Getting a Batch
     #
-    #     # you can enumerate a batch
-    #     batch.each do |obj|
-    #       puts obj.key
-    #     end
+    # You should normally not need to construct a batch. Anywhere a list
+    # of resources is returned, they are returned as a batch:
     #
-    #     # you can invoke operations on the batch, this would delete
-    #     # all 1k S3 objects in one API call.
-    #     batch.delete
-    #
-    # ## Getting Batch objects
-    #
-    # You should normally never need to construct a batch.  They are returned
-    # by resource methods where you might normally expect an array.  For
-    # example, accessing {AWS::Resources::EC2::Instance#security_groups}
-    # returns a batch of security group resource objects.
-    #
+    #     # security_groups is a batch
     #     security_groups = ec2.instance('i-12345678').security_groups
     #
-    #     security_groups.size
-    #     #=> 2
+    # When the possible number of resources is unknown or large, the
+    # resources will be returned in a collection. Collections can enumerate
+    # individual resources or batches. They manage paging over the
+    # AWS request/responses to produce batches.
     #
-    # Some resource operations return *many* resources via a collection.
-    # This happens when the number of resources is unknown and possibly
-    # large. Fetching all of the resources might require many API calls.
+    #     # objects is a collection
+    #     objects = s3.bucket('aws-sdk').objects
     #
-    #     s3.bucket('aws-sdk').
+    # You can invoke batch operations against collections and they will
+    # invoke them on each batch.
     #
-    # If the resource operation returns a collection, you can access 
-    # * Array-like access to a list of resource objects.
-    # * The ability to invoke operations on the 
-    #
-    # * array-like access to a list of resource objects
-    # * the 
-    # a list of resources.
-    #
-    #     objects = s3.bucket('aws-sdk').objects(prefix: 'tmp/')
+    #     # delets all all objects in this bucket in batches of 1k
+    #     objects = s3.bucket('aws-sdk').objects
     #     objects.delete
-    #
-    # A batch object is an Enumerable object that provides array like
-    # access to each resource in the batch.  A batch has a fixed size
-    # and is initialized with an array of resource objects.
-    #
-    #     batch = Aws::Resources::S3::Object::Batch.new([obj1, obj2])
-    #
-    # ## Getting Batch objects
-    #
-    # You should never need to construct a batch object directly. Instead
-    # all resource operations that return some or many resource objects
-    # will return a batch or a collection that can yield batches.
-    #
-    #     ec2 = Aws::Resources::EC2.new
-    #
-    #     # instances is a collection of instances, with an unknown size
-    #     instances = ec2.instances
-    #
-    #     instances.batches.each do |batch|
-    #     end
-    #
-    #     objects =
-    #
-    # ## Batch Classes
-    #
-    # Every resource class has an associated Batch class.
-    #
-    #     Aws::Resources::S3::Object
-    #     Aws::Resources::S3::Object::Batch
-    #
-    # Currently, not all resource types have
     #
     # ## Batch Operations
     #
-    # Many resource classes have operations that can be invoked on more
-    # than one of that resource type. These batch operations are exposed
-    # on that resource's batch class.
+    # Batches provide operations that operate on the entire batch. These
+    # operations are only defined for resources where the AWS API accepts
+    # multiple inputs. This means a batch operation for n elements will
+    # only make one request.
     #
-    #   # Deletes all objects from the 'aws-sdk' bucket that
-    #   # have the key prefix of 'tmp/' - this is done in batches
-    #   # of 1k objects
-    #   tmp_objects.delete
-    #
-    #
-    # list of resources
-    # A batch is
-    # returned from every resource method that would normally return an
-    # array.
+    # Resource classes document each of their own batch operations.
+    # See {S3::Object} for an example.
     #
     class Batch
 
