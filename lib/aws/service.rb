55a098778e25beb3be71c2fde3b88afaff46bce0
Trevor Rowe <trevorrowe@gmail.com>
2014-07-17 09:51:44 -0700

    Added deprecation warnings to Aws::SVC.new

--- lib/aws/service.rb
+++ lib/aws/service.rb
@@ -13,6 +13,12 @@ module Aws
     # @deprecated Deprecated as of 2.0.0.rc11
     # @api private
     def new(options = {})
+      unless @warned
+        @warned = true
+        warn(<<-MSG.strip)
+#{self.name}.new deprecated as of v2.0.0.rc11 and will be removed as of v2.0.0.0 final; use #{self.name}::Client.new instead
+        MSG
+      end
       const_get(:Client).new(options)
     end
 
