c59769bb6baf75ee379d627ac4955afb5ecea180
Alex Wood <awood45@gmail.com>
2014-09-19 10:06:31 -0700

    Make PresignHandler a Subclass of Presigner

--- aws-sdk-core/lib/aws/s3/presigner.rb
+++ aws-sdk-core/lib/aws/s3/presigner.rb
@@ -22,7 +22,7 @@ module Aws
       #   (one week).
       def presigned_url(method, params = {})
         request = @client.build_request(method, params)
-        request.handle(S3PresignHandler, step: :sign, priority: 99)
+        request.handle(PresignHandler, step: :sign, priority: 99)
         expires_in = params.delete(:expires_in) || 86400
         validate_expires_in_header(expires_in)
         request.context[:presigned_expires_in] = expires_in
@@ -37,27 +37,27 @@ module Aws
           )
         end
       end
-    end
 
-    # @api private
-    class PresignHandler < Seahorse::Client::Handler
-      def call(context)
-        Seahorse::Client::Response.new(
-          context: context,
-          data: presigned_url(context)
-        )
-      end
+      # @api private
+      class PresignHandler < Seahorse::Client::Handler
+        def call(context)
+          Seahorse::Client::Response.new(
+            context: context,
+            data: presigned_url(context)
+          )
+        end
 
-      def presigned_url(context)
-        signer = Signers::V4.new(
-          context.config.credentials, 's3',
-          context.config.region
-        )
-        signer.presigned_url(
-          context.http_request,
-          expires_in: context[:presigned_expires_in],
-          body_digest: "UNSIGNED-PAYLOAD"
-        )
+        def presigned_url(context)
+          signer = Signers::V4.new(
+            context.config.credentials, 's3',
+            context.config.region
+          )
+          signer.presigned_url(
+            context.http_request,
+            expires_in: context[:presigned_expires_in],
+            body_digest: "UNSIGNED-PAYLOAD"
+          )
+        end
       end
     end
   end
