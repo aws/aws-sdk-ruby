53f97b527824dc177fcc6f067404eaed1e8ba617
Aaron Suggs <aaron@ktheory.com>
2014-10-01 15:41:51 -0400

    [CloudFormation] Add waiter for stack completion

--- aws-sdk-core/lib/aws-sdk-core/cloudformation.rb
+++ aws-sdk-core/lib/aws-sdk-core/cloudformation.rb
@@ -2,4 +2,5 @@ Aws.add_service(:CloudFormation, {
   api: File.join(Aws::APIS_DIR, 'CloudFormation.api.json'),
   docs: File.join(Aws::APIS_DIR, 'CloudFormation.docs.json'),
   paginators: File.join(Aws::APIS_DIR, 'CloudFormation.paginators.json'),
+  waiters: File.join(Aws::APIS_DIR, 'CloudFormation.waiters.json'),
 })
