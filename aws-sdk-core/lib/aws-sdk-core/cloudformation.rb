52931db05381994efd3d1bd635b49fd620237d0d
Aaron Suggs <aaron@ktheory.com>
2014-10-01 20:41:06 -0400

    [CloudFormation] Add resources for stacks

--- aws-sdk-core/lib/aws-sdk-core/cloudformation.rb
+++ aws-sdk-core/lib/aws-sdk-core/cloudformation.rb
@@ -2,5 +2,6 @@ Aws.add_service(:CloudFormation, {
   api: File.join(Aws::APIS_DIR, 'CloudFormation.api.json'),
   docs: File.join(Aws::APIS_DIR, 'CloudFormation.docs.json'),
   paginators: File.join(Aws::APIS_DIR, 'CloudFormation.paginators.json'),
+  resources: File.join(Aws::APIS_DIR, 'CloudFormation.resources.json'),
   waiters: File.join(Aws::APIS_DIR, 'CloudFormation.waiters.json'),
 })
