d14079bdfcf92ec06830889d0269d4f329427c62
Trevor Rowe <trevorrowe@gmail.com>
2014-11-07 00:40:05 -0800

    Added an assertion about how the password data is received.

--- aws-sdk-resources/spec/services/ec2/instance/decrypt_windows_password_spec.rb
+++ aws-sdk-resources/spec/services/ec2/instance/decrypt_windows_password_spec.rb
@@ -17,6 +17,9 @@ module Aws
           client.stub_responses(:get_password_data, {
             password_data: "dALtWVWEb9LL1rBz0oluXvTK5sHcJzzS4PtiiX/17pOFs0D6+JV7mWMPG9I4\ngRoW64SZ7hmRz7fFRuJcR5sDzfmgDs6Fimd456WR9/HJ29XBv1HyPHN7EvtR\nxJ4/+iXXhVWrQeZy8aCGhcqQhRw5/xrHE7rpc014rzxRtqybqKoeEJPV+4WP\ngUWyo3BD0ui06B7zLJL2sYfALphelAYzlzehwD63M0nuovleuMeVSJKcqqR9\nuMiXvm8nogZhLUEFvUucWxSHECB9cdTH3rJ0ANs+kl4hSgV/9KX/I3MlQYuY\ngLR4DUSpuYj/XTMyl+zjxPAPbk63zTpMd+T0NKgeWA==\n"
           })
+          expect(client).to receive(:get_password_data).
+            with(instance_id:'id').
+            and_return(client.next_stub(:get_password_data))
           instance = Instance.new('id', client: client)
           expect(instance.decrypt_windows_password(keypair)).to eq('password')
         end
