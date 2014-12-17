f069ee6494ef4c280663257232e6d7f442d776e5
Trevor Rowe <trevorrowe@gmail.com>
2014-12-17 08:54:19 -0800

    Added some additional documentation to the new crc32 checksum feature.

--- aws-sdk-core/lib/aws-sdk-core/plugins/dynamodb_crc32_validation.rb
+++ aws-sdk-core/lib/aws-sdk-core/plugins/dynamodb_crc32_validation.rb
@@ -1,5 +1,11 @@
 module Aws
   module Plugins
+
+    # @seahorse.client.option [Boolean] :compute_checksums (true)
+    #   When `true`, a CRC32 checksum is computed of every HTTP
+    #   response body and compared against the `X-Amz-Crc32` header.
+    #   If the checksums do not match, the request is re-sent.
+    #   Failures can be retried up to `:retry_limit` times.
     class DynamoDBCRC32Validation < Seahorse::Client::Plugin
 
       option(:compute_checksums, true)
@@ -13,7 +19,11 @@ module Aws
       class Handler < Seahorse::Client::Handler
 
         def call(context)
-          context.http_request.headers['accept-encoding'] = '' # disables response gzipping
+          # disable response gzipping - Net::HTTP unzips these responses
+          # before we can see the body, making it impossible to verify
+          # the CRC32 checksum against the compressed body stream
+          context.http_request.headers['accept-encoding'] = ''
+
           @handler.call(context).on_success do |response|
             response.error = validate(context)
           end
@@ -30,14 +40,10 @@ module Aws
 
         def crc32_is_valid?(response)
           if crc_checksum = response.headers['x-amz-crc32']
-            crc_checksum.to_i == calculate_crc32(response.body_contents)
+            crc_checksum.to_i == Zlib.crc32(response.body_contents)
           else
             true
           end
-        end
-
-        def calculate_crc32(body)
-          Zlib.crc32(body)
         end
 
       end
