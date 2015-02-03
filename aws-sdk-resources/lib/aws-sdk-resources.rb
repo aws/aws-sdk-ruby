fbe8fc6c06d604147ace45762ef19426417493f7
Trevor Rowe <trevorrowe@gmail.com>
2015-02-03 13:06:42 -0800

    Corrected resources model.

--- aws-sdk-resources/lib/aws-sdk-resources.rb
+++ aws-sdk-resources/lib/aws-sdk-resources.rb
@@ -1,8 +1,8 @@
 require 'aws-sdk-core'
 
 module Aws
-  module Resources
 
+  module Resources
     autoload :Batch, 'aws-sdk-resources/batch'
     autoload :Builder, 'aws-sdk-resources/builder'
     autoload :BuilderSources, 'aws-sdk-resources/builder_sources'
@@ -10,6 +10,7 @@ module Aws
     autoload :Definition, 'aws-sdk-resources/definition'
     autoload :Documenter, 'aws-sdk-resources/documenter'
     autoload :Errors, 'aws-sdk-resources/errors'
+    autoload :HasOperations, 'aws-sdk-resources/has_operations'
     autoload :Operations, 'aws-sdk-resources/operations'
     autoload :OperationMethods, 'aws-sdk-resources/operation_methods'
     autoload :Options, 'aws-sdk-resources/options'
@@ -18,38 +19,27 @@ module Aws
     autoload :Resource, 'aws-sdk-resources/resource'
     autoload :Source, 'aws-sdk-resources/source'
     autoload :Validator, 'aws-sdk-resources/validator'
-
   end
 
-  class << self
-
-    private
-
-    def define_resource_classes(svc_module, definition)
-      build_definition(definition).apply(svc_module)
-    end
-
-    def build_definition(definition)
-      case definition
+  service_added do |name, svc_module, options|
+    definition = options[:resources]
+    definition = case definition
       when nil then Resources::Definition.new({})
       when Resources::Definition then definition
       when Hash then Resources::Definition.new(definition)
-      when String, Pathname then Resources::Definition.new(Aws.load_json(definition), source_path: definition.to_s)
+      when String then Resources::Definition.new(Aws.load_json(definition))
       else raise ArgumentError, "invalid resource definition #{definition}"
-      end
     end
+    definition.apply(svc_module)
 
-    def load_resource_customizations(svc_name)
-      require "aws-sdk-resources/services/#{svc_name.downcase}"
-    rescue LoadError
-      # no customizations
-    end
+    # load customizations
+    svc = File.join(
+      File.dirname(__FILE__),
+      'aws-sdk-resources',
+      'services',
+      "#{name.downcase}.rb")
 
-  end
-
-  service_added do |svc_name, svc_module, options|
-    define_resource_classes(svc_module, options[:resources])
-    load_resource_customizations(svc_name)
+    require(svc) if File.exists?(svc)
   end
 
 end
