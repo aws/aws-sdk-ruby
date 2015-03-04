0686f58d3ab0d249cbd19e526ea9593660af3133
Trevor Rowe <trevorrowe@gmail.com>
2015-03-04 12:49:09 -0800

    Improved API reference doc class list.

--- doc-src/templates/default/fulldoc/html/setup.rb
+++ doc-src/templates/default/fulldoc/html/setup.rb
@@ -1,67 +1,56 @@
-def class_list(root = Registry.root)
-
-  out = ""
+def class_list(root = Registry.root, tree = TreeContext.new)
+  out = ''
+  # non-service classes
+  out << "<li class='#{tree.classes.join(' ')}'>"
+  out << "<div class='item' style='padding-left:#{tree.indent}'>"
+  out << "<a class='toggle'></a> Non-Service Classes"
+  out << "</div><ul>"
   children = run_verifier(root.children)
-  if root == Registry.root
-    children += @items.select {|o| o.namespace.is_a?(CodeObjects::Proxy) }
+  tree.nest do
+    out << class_list_children(children, tree:tree, skip_services:true)
   end
+  out << '</ul></li>'
 
-  if root == Registry.root
-    out << '<li class="expander"><a class="toggle"></a> Non-Service Classes</li><ul>'
+  # service classes
+  out << "<li class='#{tree.classes.join(' ')}'>"
+  out << "<div class='item' style='padding-left:#{tree.indent}'>"
+  out << "<a class='toggle'></a> Services"
+  out << "</div><ul>"
+  children = Registry.at('Aws').children.select { |c| c.has_tag?(:service) }
+  tree.nest do
+    out << class_list_children(children.sort_by(&:name), tree:tree)
   end
+  out << '</ul></li>'
+  out
+end
 
-  children.reject {|c| c.nil? }.sort_by {|child| child.path }.map do |child|
+def class_list_children(children, options)
+  tree = options[:tree]
+  skip_services = options[:skip_services]
+  out = ''
+  children.compact.sort_by(&:path).each do |child|
     if child.is_a?(CodeObjects::NamespaceObject)
+      next if child.tag(:service) if skip_services
       name = child.namespace.is_a?(CodeObjects::Proxy) ? child.path : child.name
-      next if child.tag(:service)
-      has_children = run_verifier(child.children).any? {|o| o.is_a?(CodeObjects::NamespaceObject) }
-      out << "<li>"
-      out << "<a class='toggle'></a> " if has_children
+      grand_children = run_verifier(child.children)
+      is_parent = grand_children.any? {|o| o.is_a?(CodeObjects::NamespaceObject) }
+      out << "<li id='object_#{child.path}' class='#{tree.classes.join(' ')}'>"
+      out << "<div class='item' style='padding-left:#{tree.indent}'>"
+      out << "<a class='toggle'></a> " if is_parent
       out << linkify(child, name)
       out << " &lt; #{child.superclass.name}" if child.is_a?(CodeObjects::ClassObject) && child.superclass
       out << "<small class='search_info'>"
       out << child.namespace.title
       out << "</small>"
-      out << "</li>"
-      out << "<ul>#{class_list(child)}</ul>" if has_children
-    end
-  end
-
-  out << '</ul>' if root == Registry.root
-
-  if root == Registry.root
-    out << '<li class="expander"><a class="toggle"></a> Services</li><ul>'
-    svcs = Registry.at('Aws').children.select {|c| c.has_tag?(:service) }
-
-    services = Registry.at('Aws').children.select { |c| c.has_tag?(:service) }
-
-    services.sort_by(&:name).each do |svc|
-      out << "<li><a class='toggle'></a>"
-      out << linkify(svc, svc.name)
-      out << " &lt; Service"
-      out << "<small class='search_info'>"
-      out << svc.namespace.title
-      out << "</small>"
-      out << "</li>"
-      out << "<ul>"
-      sorted_service_classes(svc) do |child|
-        name = child.namespace.is_a?(CodeObjects::Proxy) ? child.path : child.name
-        has_children = run_verifier(child.children).any? {|o| o.is_a?(CodeObjects::NamespaceObject) }
-        out << "<li>"
-        out << "<a class='toggle'></a> " if has_children
-        out << linkify(child, name)
-        out << " &lt; #{child.superclass.name}" if child.is_a?(CodeObjects::ClassObject) && child.superclass
-        out << "<small class='search_info'>"
-        out << child.namespace.title
-        out << "</small>"
-        out << "</li>"
-        out << "<ul>#{class_list(child)}</ul>" if has_children
+      out << "</div>"
+      if is_parent
+        tree.nest do
+          out << "<ul>#{class_list_children(grand_children, options)}</ul>"
+        end
       end
-      out << "</ul>"
+      out << "</li>"
     end
-    out << '</ul>'
   end
-
   out
 end
 
