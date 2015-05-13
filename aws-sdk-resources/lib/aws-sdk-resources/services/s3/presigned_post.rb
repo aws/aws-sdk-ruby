772fcbab4f1e00a6cb07231564345b62fb5cff27
Trevor Rowe <trevorrowe@gmail.com>
2015-05-13 16:01:08 -0700

    Removed the use of a deprecated method in an integration test.

--- aws-sdk-resources/lib/aws-sdk-resources/services/s3/presigned_post.rb
+++ aws-sdk-resources/lib/aws-sdk-resources/services/s3/presigned_post.rb
@@ -209,7 +209,7 @@ module Aws
       # @option options [String] :server_side_encryption_customer_algorithm See {PresignedPost#server_side_encryption_customer_algorithm}.
       # @option options [String] :server_side_encryption_customer_key See {PresignedPost#server_side_encryption_customer_key}.
       def initialize(credentials, bucket_region, bucket_name, options = {})
-        @credentials = credentials
+        @credentials = credentials.credentials
         @bucket_region = bucket_region
         @bucket_name = bucket_name
         @url = bucket_url
