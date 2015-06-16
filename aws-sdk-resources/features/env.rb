56c3553abd3446574c8404e2f4f52cf9d9ebd80b
Trevor Rowe <trevorrowe@gmail.com>
2015-06-16 14:34:54 -0700

    No longer running integration tests on the presense of a test config file, you must set the ENV variable.

--- aws-sdk-resources/features/env.rb
+++ aws-sdk-resources/features/env.rb
@@ -7,17 +7,13 @@ SimpleCov.command_name('test:integration:aws-sdk-resources')
 
 cfg = './integration-test-config.json'
 
-if File.exist?(cfg)
-  Aws.config = Aws::Json.load_file(cfg).inject({}) do |h, (k, v)|
-    h[k.to_sym] = v; h
-  end
-elsif ENV['AWS_INTEGRATION']
+if ENV['AWS_INTEGRATION']
   # run integration tests, just don't read a configuration file from disk
 else
   msg = <<-MSG
 
 *** skipping aws-sdk-resource integration tests ***
-  To enable integration tests, create a #{cfg} file or export AWS_INTEGRATION=1
+  Export AWS_INTEGRATION=1 to enable integration tests
 
   MSG
   puts msg
