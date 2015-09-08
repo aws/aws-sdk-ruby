74c0a8f991541f95f95f8cf04c21ac1d46c3f0ac
Trevor Rowe <trevorrowe@gmail.com>
2015-09-08 09:17:38 -0700

    Added delegator methods from Aws::S3::ObjectSummary to Object.

--- aws-sdk-resources/lib/aws-sdk-resources/services/s3/object_summary.rb
+++ aws-sdk-resources/lib/aws-sdk-resources/services/s3/object_summary.rb
@@ -4,6 +4,46 @@ module Aws
 
       alias content_length size
 
+      # @param (see Object#copy_from)
+      # @options (see Object#copy_from)
+      # @return (see Object#copy_from)
+      # @see Object#copy_from
+      def copy_from(source, options = {})
+        object.copy_from(target, options)
+      end
+
+      # @param (see Object#copy_to)
+      # @options (see Object#copy_to)
+      # @return (see Object#copy_to)
+      # @see Object#copy_to
+      def copy_to(taret, options = {})
+        object.copy_to(target, options)
+      end
+
+      # @param (see Object#presigned_url)
+      # @options (see Object#presigned_url)
+      # @return (see Object#presigned_url)
+      # @see Object#presigned_url
+      def presigned_url(http_method, params = {})
+        object.presigned_url(http_method, params)
+      end
+
+      # @param (see Object#presigned_post)
+      # @options (see Object#presigned_post)
+      # @return (see Object#presigned_post)
+      # @see Object#presigned_post
+      def presigned_post(options = {})
+        object.presigned_post(options)
+      end
+
+      # @param (see Object#public_url)
+      # @options (see Object#public_url)
+      # @return (see Object#public_url)
+      # @see Object#public_url
+      def public_url(options = {})
+        object.public_url(options)
+      end
+
       # @param (see Object#upload_file)
       # @options (see Object#upload_file)
       # @return (see Object#upload_file)
