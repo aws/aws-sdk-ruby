35507eb7eacc545b199c18a00d8cdbd796e55e53
Trevor Rowe <trevorrowe@gmail.com>
2015-11-11 12:08:20 -0800

    Fixed doc example formatting.

--- aws-sdk-core/lib/aws-sdk-core/assume_role_credentials.rb
+++ aws-sdk-core/lib/aws-sdk-core/assume_role_credentials.rb
@@ -5,13 +5,13 @@ module Aws
   # An auto-refreshing credential provider that works by assuming
   # a role via {Aws::STS::Client#assume_role}.
   #
-  #    role_credentials = Aws::AssumeRoleCredentials.new(
-  #      client: Aws::STS::Client.new(...),
-  #      role_arn: "linked::account::arn",
-  #      role_session_name: "session-name"
-  #    )
+  #     role_credentials = Aws::AssumeRoleCredentials.new(
+  #       client: Aws::STS::Client.new(...),
+  #       role_arn: "linked::account::arn",
+  #       role_session_name: "session-name"
+  #     )
   #
-  #    ec2 = Aws::EC2::Client.new(credentials: role_credentials)
+  #     ec2 = Aws::EC2::Client.new(credentials: role_credentials)
   #
   # If you omit `:client` option, a new {STS::Client} object will be
   # constructed.
