22fc0de9526a4c92792fe8393dd1201be5826424
Trevor Rowe <trevorrowe@gmail.com>
2015-11-23 10:34:08 -0800

    Reformatted code and fixed doc formatting.

--- aws-sdk-resources/lib/aws-sdk-resources/services/s3/bucket.rb
+++ aws-sdk-resources/lib/aws-sdk-resources/services/s3/bucket.rb
@@ -22,28 +22,33 @@ module Aws
       #
       #   bucket.delete!
       #
-      # @option options [Float]  :initial_wait (1.3) Initial wait time. Exponentially increased for each attempt.
-      #                 [Fixnum] :max_attempts (3) Maximum number of attempts to make before raising Errors::BucketNotEmpty.
+      # @option options [Integer] :max_attempts (3) Maximum number of times to
+      #   attempt to delete the empty bucket before raising
+      #   `Aws::S3::Errors::BucketNotEmpty`.
+      #
+      # @option options [Float] :initial_wait (1.3) Seconds to wait before
+      #   retrying the call to delete the bucket, exponentially increased for
+      #   each attempt.
       #
       # @return [void]
-      DELETE_BANG_DEFAULTS = { initial_wait: 1.3, max_attempts: 3 }
-
       def delete! options = { }
-        options = DELETE_BANG_DEFAULTS.merge options
+        options = {
+          initial_wait: 1.3,
+          max_attempts: 3,
+        }.merge(options)
 
         attempts = 0
-
         begin
           clear!
           delete
-        rescue Errors::BucketNotEmpty => e
-          attempts += 1
-
-          raise e if attempts >= options[:max_attempts]
-
-          Kernel.sleep options[:initial_wait] ** attempts
-
-          retry
+        rescue Errors::BucketNotEmpty => error
+          if attempts < options[:max_attempts]
+            attempts += 1
+            Kernel.sleep(options[:initial_wait] ** attempts)
+            retry
+          else
+            raise
+          end
         end
       end
 
