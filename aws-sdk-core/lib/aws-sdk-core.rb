a20744c91bfbfd2a9eba8c0a32556c500a884d09
Trevor Rowe <trevorrowe@gmail.com>
2016-05-10 09:52:09 -0700

    Disabled S3 and DynamoDB customizations (until they are moved into code gen).

--- aws-sdk-core/lib/aws-sdk-core.rb
+++ aws-sdk-core/lib/aws-sdk-core.rb
@@ -489,38 +489,38 @@ module Aws
     end
   end
 
-  module S3
-    autoload :Presigner, 'aws-sdk-core/s3/presigner'
-    autoload :BucketRegionCache, 'aws-sdk-core/s3/bucket_region_cache'
-    # @api private
-    BUCKET_REGIONS = BucketRegionCache.new
-  end
-
-  module DynamoDB
-    autoload :AttributeValue, 'aws-sdk-core/dynamodb/attribute_value'
-    class Client
-      def data_to_http_resp(operation_name, data)
-        api = config.api
-        operation = api.operation(operation_name)
-        translator = Plugins::DynamoDBSimpleAttributes::ValueTranslator
-        translator = translator.new(operation.output, :marshal)
-        data = translator.apply(data)
-        ParamValidator.validate!(operation.output, data)
-        protocol_helper.stub_data(api, operation, data)
-      end
-
-      def stub_data(operation_name, data = {})
-        if config.simple_attributes
-          rules = config.api.operation(operation_name).output
-          translator = Plugins::DynamoDBSimpleAttributes::ValueTranslator
-          data = translator.apply(rules, :marshal, data)
-          data = super(operation_name, data)
-          translator.apply(rules, :unmarshal, data)
-        else
-          super
-        end
-      end
-    end
-  end
+#  module S3
+#    autoload :Presigner, 'aws-sdk-core/s3/presigner'
+#    autoload :BucketRegionCache, 'aws-sdk-core/s3/bucket_region_cache'
+#    # @api private
+#    BUCKET_REGIONS = BucketRegionCache.new
+#  end
+#
+#  module DynamoDB
+#    autoload :AttributeValue, 'aws-sdk-core/dynamodb/attribute_value'
+#    class Client
+#      def data_to_http_resp(operation_name, data)
+#        api = config.api
+#        operation = api.operation(operation_name)
+#        translator = Plugins::DynamoDBSimpleAttributes::ValueTranslator
+#        translator = translator.new(operation.output, :marshal)
+#        data = translator.apply(data)
+#        ParamValidator.validate!(operation.output, data)
+#        protocol_helper.stub_data(api, operation, data)
+#      end
+#
+#      def stub_data(operation_name, data = {})
+#        if config.simple_attributes
+#          rules = config.api.operation(operation_name).output
+#          translator = Plugins::DynamoDBSimpleAttributes::ValueTranslator
+#          data = translator.apply(rules, :marshal, data)
+#          data = super(operation_name, data)
+#          translator.apply(rules, :unmarshal, data)
+#        else
+#          super
+#        end
+#      end
+#    end
+#  end
 
 end
