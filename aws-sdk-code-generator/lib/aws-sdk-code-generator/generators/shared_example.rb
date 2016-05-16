1342817c49d32e3be0fd5cc2049bcbaca7eac441
Trevor Rowe <trevorrowe@gmail.com>
2016-05-16 16:27:33 -0700

    Shared examples now inline their comments.

--- aws-sdk-code-generator/lib/aws-sdk-code-generator/generators/shared_example.rb
+++ aws-sdk-code-generator/lib/aws-sdk-code-generator/generators/shared_example.rb
@@ -14,125 +14,110 @@ module AwsSdkCodeGenerator
 
       def to_s
         lines = []
+        lines << ""
         lines << "@example Example: #{@example['description']}"
         lines << ""
-        lines << input_example
-        lines << output_example
+        apply_input_example(lines)
+        apply_output_example(lines)
         lines.join("\n")
       end
 
       private
 
-      def input_example
-        "  resp = client.#{@method_name}(#{input_params})"
+      def apply_input_example(lines)
+        lines << "  resp = client.#{@method_name}(#{input_params})"
       end
 
       def input_params
         if @example['input'] && @operation['input']
-          entry(@example['input'], @operation['input'], indent = '  ', path = []).join("\n")
+          entry(
+            @example['input'],
+            @operation['input'],
+            indent = '  ',
+            path = [],
+            @example['comments']['input']
+          ).join("\n")
         else
           ''
         end
       end
 
-      def output_example
-        ""
+      def apply_output_example(lines)
+        if @example['output'] && @operation['output']
+          lines << ""
+          lines << "  # resp.to_h outputs the following:"
+          lines << "  " + entry(
+              @example['output'],
+              @operation['output'],
+              indent = '  ',
+              path = [],
+              @example['comments']['output']
+          ).join("\n")
+        end
       end
-
-
-
-
-
-#      def foo
-#        apply_lines
-#
-#        line = []
-#        lines << "@example Example: #{@description}"
-#
-#        parts << "  # #{json_ex['description']}\n\n"
-#        parts += input.lines.map { |line| "  " + line }
-#        if json_ex['output']
-#          output_comments = json_ex['comments']['output']
-#          output = SharedExample.new(json_ex['output'], method_name, operation, output_comments).to_str_output
-#          parts << "\n\n  # resp.to_h outputs the following:\n"
-#          parts += output.lines.map { |line| "  " + line }
-#        end
-#      end
-#
-#      def to_str_input
-#        lines = structure(@params, @operation.input, "", [])
-#        params = lines.join("\n")
-#        "resp = client.#{@method_name}(#{params})"
-#      end
-#
-#      def to_str_output
-#        lines = structure(@params, @operation.output, "", [])
-#        params = lines.join("\n")
-#        "#{params}"
-#      end
 
       def entry(json, ref, indent, path, comments)
         case shape(ref)['type']
-        when 'structure' then structure(json, ref, indent, path)
-        when 'map' then map(json, ref, indent, path)
-        when 'list' then list(json, ref, indent, path)
+        when 'structure' then structure(json, ref, indent, path, comments)
+        when 'map' then map(json, ref, indent, path, comments)
+        when 'list' then list(json, ref, indent, path, comments)
         when 'timestamp' then "Time.parse(#{json.inspect})"
-        when 'string', 'blob', 'integer', 'long', 'float', 'double'
+        when 'string', 'blob', 'integer', 'long', 'float', 'double', 'boolean'
           json.inspect
         else
           raise ArgumentError, "unhandled type #{shape(ref)['type'].inspect}"
         end
       end
 
-      def structure(json, ref, indent, path)
+      def structure(json, ref, indent, path, comments)
         lines = ["{"]
         json.each_pair do |key, val|
           path << ".#{key}"
-          shape_val = entry(val, shape(ref)['members'][key], "#{indent}  ", path)
+          shape_val = entry(val, shape(ref)['members'][key], "#{indent}  ", path, comments)
           if shape_val.is_a?(Array)
             shape_val = shape_val.join("\n")
           end
-          lines << "#{indent}  #{underscore(key)}: #{shape_val},#{comments(path)}"
+          lines << "#{indent}  #{underscore(key)}: #{shape_val},#{comments_for(path, comments)}"
           path.pop
         end
         lines << "#{indent}}"
         lines
       end
 
-      def map(json, ref, indent, path)
+      def map(json, ref, indent, path, comments)
         lines = ["{"]
         json.each do |key, val|
           path << ".#{key}"
-          shape_val = entry(val, shape(ref)['value'], "#{indent}  ", path)
+          shape_val = entry(val, shape(ref)['value'], "#{indent}  ", path, comments)
           if shape_val.is_a?(Array)
             shape_val = shape_val.join("\n")
           end
-          lines << "#{indent}  \"#{key}\" => #{shape_val},#{comments(path)}"
+          lines << "#{indent}  \"#{key}\" => #{shape_val},#{comments_for(path, comments)}"
           path.pop
         end
         lines << "#{indent}}"
         lines
       end
 
-      def list(json, ref, indent, path)
+      def list(json, ref, indent, path, comments)
         lines = ["["]
         json.each_with_index do |value, index|
           path << "[#{index}]"
-          shape_val = entry(value, shape(ref)['member'], "#{indent}  ", path)
+          shape_val = entry(value, shape(ref)['member'], "#{indent}  ", path, comments)
           if shape_val.is_a?(Array)
             shape_val = shape_val.join("\n")
           end
-          lines << "#{indent}  #{shape_val},#{comments(path)}"
+          lines << "#{indent}  #{shape_val},#{comments_for(path, comments)}"
           path.pop
         end
         lines << "#{indent}]"
         lines
       end
 
-      def comments(path)
-        key = path.join().sub(/^\./, '')
-        if @comments && @comments[key]
-          " # #{@comments[key]}"
+      def comments_for(path, comments)
+        path = path.join().sub(/^\./, '')
+        if comments.key?(path)
+          " # #{comments[path]}"
         else
           ""
         end
