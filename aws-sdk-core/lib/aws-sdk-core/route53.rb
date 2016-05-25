cebea88b4b6765d17809a44d18e29f2a21ba749c
Trevor Rowe <trevorrowe@gmail.com>
2016-05-25 15:31:30 -0700

    Bulk updated APIs.

--- aws-sdk-core/lib/aws-sdk-core/route53.rb
+++ aws-sdk-core/lib/aws-sdk-core/route53.rb
@@ -3,4 +3,5 @@ Aws.add_service(:Route53, {
   docs: "#{Aws::API_DIR}/route53/2013-04-01/docs-2.json",
   examples: "#{Aws::API_DIR}/route53/2013-04-01/examples-1.json",
   paginators: "#{Aws::API_DIR}/route53/2013-04-01/paginators-1.json",
+  waiters: "#{Aws::API_DIR}/route53/2013-04-01/waiters-2.json",
 })
