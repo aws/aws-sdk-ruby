d49a9117ccee0c9cc8b6e25d68e3ebf344d1c130
Alex Wood <awood45@gmail.com>
2016-06-07 09:48:47 -0700

    Add Waiters for Aws::CloudWatch
    Adds the `:alarm_exists` waiter for `Aws::CloudWatch`. Additionally,
    this is wired into `Aws::CloudWatch::Resource` as the
    `Aws::CloudWatch::Alarm#exists?` method.
    
    Resolves GitHub issue #1171

--- aws-sdk-core/lib/aws-sdk-core/cloudwatch.rb
+++ aws-sdk-core/lib/aws-sdk-core/cloudwatch.rb
@@ -4,4 +4,5 @@ Aws.add_service(:CloudWatch, {
   examples: "#{Aws::API_DIR}/monitoring/2010-08-01/examples-1.json",
   paginators: "#{Aws::API_DIR}/monitoring/2010-08-01/paginators-1.json",
   resources: "#{Aws::API_DIR}/monitoring/2010-08-01/resources-1.json",
+  waiters: "#{Aws::API_DIR}/monitoring/2010-08-01/waiters-2.json",
 })
