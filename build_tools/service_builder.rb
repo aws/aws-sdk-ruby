6ee8a075785bbc0ccbe3f3cd840b89c3f02cd49d
Trevor Rowe <trevorrowe@gmail.com>
2016-06-23 15:58:51 -0700

    Re-enabled source code generation.

--- build_tools/service_builder.rb
+++ build_tools/service_builder.rb
@@ -16,71 +16,9 @@ module BuildTools
     def build
       ServiceBuilder::VersionFile.new(@service).build
       ServiceBuilder::Gemspec.new(@service).build
-      #ServiceBuilder::Source.new(@service).build
+      ServiceBuilder::Source.new(@service).build
       ServiceBuilder::Specs.new(@service).build
       ServiceBuilder::Features.new(@service).build
-    end
-
-    private
-
-    def boostrap_version_file
-      version_file = File.join(@service.gem_dir, 'VERSION')
-      write_file(version_file, "1.0.0") unless File.exists?(version_file)
-      File.read(version_file).strip
-    end
-
-    def build_src(gem_version)
-      g = AwsSdkCodeGenerator::Generator.new(generate_opts) do |svc_module|
-
-        if File.exists?("#{@service.gem_dir}/lib/#{@service.gem_name}/customizations.rb")
-          svc_module.top("\n# customizations for generated code")
-          svc_module.require_relative("#{@service.gem_name}/customizations.rb")
-        end
-
-        svc_module.code("GEM_VERSION = '#{gem_version}'")
-
-      end
-
-      g.generate_src_files(prefix: @service.gem_name).each do |path, contents|
-        write_file(File.join(@service.gem_dir, 'lib', path), contents)
-      end
-    end
-
-    def generate_opts
-      options = {}
-      options[:module_names] = ['Aws', @service.name]
-      @service.models.each_pair do |model_name, model_path|
-        options[model_name] = load_model(@service.name, model_name, model_path)
-      end
-      options[:gem_requires] = @service.dependencies.keys
-      options
-    end
-
-    def build_gemspec(gem_version)
-      gemspec_path = File.join(@service.gem_dir, "#{@service.gem_name}.gemspec")
-      write_file(gemspec_path, gemspec)
-    end
-
-    # Loads a service model, applying customizations if necessary.
-    def load_model(svc_name, model_name, path)
-      model = load_json(path)
-      case model_name
-      when :api  then Customizations.apply_api_customizations(svc_name, model)
-      when :docs then Customizations.apply_doc_customizations(svc_name, model)
-      end
-      model
-    end
-
-    def load_json(path)
-      JSON.load(File.open(path, 'rb') { |f| f.read })
-    end
-
-    def write_file(path, contents)
-      puts path
-      FileUtils.mkdir_p(File.dirname(path))
-      File.open(path, 'wb') do |file|
-        file.write(contents)
-      end
     end
 
   end
---
+++ build_tools/service_builder/source.rb
@@ -0,0 +1,57 @@
+module BuildTools
+  class ServiceBuilder
+    class Source
+
+      def initialize(service)
+        @service = service
+        @generator = AwsSdkCodeGenerator::Generator.new(generator_options) do |svc_module|
+          svc_module.top("\n# customizations for generated code")
+          svc_module.require_relative("#{gem_name}/customizations.rb")
+          svc_module.code("GEM_VERSION = '#{gem_version}'")
+        end
+      end
+
+      def build
+        FileWriter.new(customizations_path).bootstrap('')
+        @generator.generate_src_files(prefix: gem_name).each do |path, contents|
+          FileWriter.new(path).write(contents)
+        end
+      end
+
+      private
+
+      def generator_options
+        options = {}
+        options[:module_names] = ['Aws', @service.name]
+        @service.models.each_pair do |model_name, model_path|
+          options[model_name] = load_model(@service.name, model_name, model_path)
+        end
+        options[:gem_requires] = @service.dependencies.keys
+        options
+      end
+
+      # Loads a service model, applying customizations if necessary.
+      def load_model(svc_name, model_name, path)
+        model = JSON.load(File.open(path, 'rb') { |f| f.read })
+        case model_name
+        when :api  then Customizations.apply_api_customizations(svc_name, model)
+        when :docs then Customizations.apply_doc_customizations(svc_name, model)
+        end
+        model
+      end
+
+      def customizations_path
+        File.join(@service.gem_dir, 'lib', gem_name, 'customizations.rb')
+      end
+
+      def gem_name
+        @service.gem_name
+      end
+
+      def gem_version
+        File.read(File.join(@service.gem_dir, 'VERSION')).strip
+      end
+
+    end
+  end
+end
