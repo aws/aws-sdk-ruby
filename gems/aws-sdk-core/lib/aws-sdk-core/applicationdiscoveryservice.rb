c8dfaeb820353989996b33a6be09af7d5793ca49
Trevor Rowe <trevorrowe@gmail.com>
2016-06-28 22:57:28 -0700

    Removed specs for the now deprecated Aws.add_service.

--- gems/aws-sdk-core/lib/aws-sdk-core/applicationdiscoveryservice.rb
+++
@@ -1,5 +0,0 @@
-Aws.add_service(:ApplicationDiscoveryService, {
-  api: "#{Aws::API_DIR}/discovery/2015-11-01/api-2.json",
-  docs: "#{Aws::API_DIR}/discovery/2015-11-01/docs-2.json",
-  examples: "#{Aws::API_DIR}/discovery/2015-11-01/examples-1.json",
-})
