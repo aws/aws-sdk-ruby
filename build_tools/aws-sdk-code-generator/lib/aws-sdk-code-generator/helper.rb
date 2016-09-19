61d7f4352b34412d06df4f5c95eb84731d3ad160
chejingy <chejingy@amazon.com>
2016-09-19 12:44:37 -0700

    Fix important tag

--- build_tools/aws-sdk-code-generator/lib/aws-sdk-code-generator/helper.rb
+++ build_tools/aws-sdk-code-generator/lib/aws-sdk-code-generator/helper.rb
@@ -142,12 +142,17 @@ module AwsSdkCodeGenerator
         # turn them into YARD links.
         html = html.gsub(/<a>(.+?)<\/a>/) { $1 }
 
+        # <important> tag doesn't render well
+        html = html.gsub(/<important>(.+?)<\/important>/){ "<p>#{$1}</p>" }
+
         markdown = Kramdown::Document.new(
           html,
           input: 'html',
           line_width: line_width,
           auto_ids: false
         ).to_kramdown.strip
+
+        # remove extra escape
         markdown.gsub(/\\(`|'|")/, '\1')
       end
     end
