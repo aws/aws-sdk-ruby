cb674048282ba36ddd12073b43e3f78090e87eb9
Trevor Rowe <trevorrowe@gmail.com>
2016-11-02 14:14:40 -0700

    Reduced the final yard doc plugin to a single tag.

--- doc-src/plugins/apis.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.expand_path('../../../gems/aws-sdk-core/lib', __FILE__))
-
-require 'aws-sdk-core'
-
-YARD::Tags::Library.define_tag('CONFIGURATION_OPTIONS', :seahorse_client_option)
-YARD::Tags::Library.define_tag('SERVICE', :service)
-YARD::Tags::Library.define_tag('API_VERSION', :api_version)
-
-YARD::Templates::Engine.register_template_path(File.join(File.dirname(__FILE__), '..', 'templates'))
