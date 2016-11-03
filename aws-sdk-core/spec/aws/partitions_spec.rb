2429b18f7fe0198314c627c3e4c87b9583ca9fd7
Trevor Rowe <trevrowe@amazon.com>
2016-11-03 20:00:27 +0000

    Removed an order test issue.

--- aws-sdk-core/spec/aws/partitions_spec.rb
+++ aws-sdk-core/spec/aws/partitions_spec.rb
@@ -26,7 +26,7 @@ module Aws
 
         it 'returns a list of Partition objects' do
           partitions = Aws.partitions
-          expect(partitions.map(&:name)).to eq(%w(aws aws-cn aws-us-gov))
+          expect(partitions.map(&:name).sort).to eq(%w(aws aws-cn aws-us-gov).sort)
           partitions.each do |p|
             expect(p).to be_kind_of(Partition)
           end
