99c1b7cd4318d9e981de1928db515a24e8f30269
chejingy <chejingy@amazon.com>
2016-11-10 13:31:18 -0800

    Add support for Rds Auth Token

--- aws-sdk-core/lib/aws-sdk-core/rds.rb
+++ aws-sdk-core/lib/aws-sdk-core/rds.rb
@@ -6,3 +6,11 @@ Aws.add_service(:RDS, {
   resources: "#{Aws::API_DIR}/rds/2014-10-31/resources-1.json",
   waiters: "#{Aws::API_DIR}/rds/2014-10-31/waiters-2.json",
 })
+
+module Aws
+  module RDS
+
+    autoload :AuthTokenGenerator, 'aws-sdk-core/rds/auth_token_generator'
+
+  end
+end
