13dfc2e3a7d365a33247340413a2851d52c25ed9
Trevor Rowe <trevorrowe@gmail.com>
2017-01-09 09:08:42 -0800

    Corrected a wording error in the generated src warning.

--- gems/aws-sdk-iam/lib/aws-sdk-iam/client.rb
+++ gems/aws-sdk-iam/lib/aws-sdk-iam/client.rb
@@ -1,6 +1,6 @@
 # WARNING ABOUT GENERATED CODE
 #
-# This file is generated. See the contributing for info on making contributions:
+# This file is generated. See the contributing guide for more information:
 # https://github.com/aws/aws-sdk-ruby/blob/master/CONTRIBUTING.md
 #
 # WARNING ABOUT GENERATED CODE
