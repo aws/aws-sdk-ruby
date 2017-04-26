c910f3bffa1c47689453ab89bb9a186137cb47c6
Alex Wood <awood45@gmail.com>
2017-04-26 12:17:58 -0700

    Add Back Resources and Waiters

--- aws-sdk-core/lib/aws-sdk-core/rds.rb
+++ aws-sdk-core/lib/aws-sdk-core/rds.rb
@@ -3,6 +3,8 @@ Aws.add_service(:RDS, {
   docs: "#{Aws::API_DIR}/rds/2014-10-31/docs-2.json",
   examples: "#{Aws::API_DIR}/rds/2014-10-31/examples-1.json",
   paginators: "#{Aws::API_DIR}/rds/2014-10-31/paginators-1.json",
+  resources: "#{Aws::API_DIR}/rds/2014-10-31/resources-1.json",
+  waiters: "#{Aws::API_DIR}/rds/2014-10-31/waiters-2.json",
 })
 
 module Aws
