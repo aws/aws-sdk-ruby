78f24e8502487c28809352f37443eb92cf401688
Alex Wood <awood45@gmail.com>
2017-04-26 12:12:57 -0700

    Fix Versioning for RDS API

--- aws-sdk-core/lib/aws-sdk-core/rds.rb
+++ aws-sdk-core/lib/aws-sdk-core/rds.rb
@@ -1,8 +1,8 @@
 Aws.add_service(:RDS, {
-  api: "#{Aws::API_DIR}/rds/2014-09-01/api-2.json",
-  docs: "#{Aws::API_DIR}/rds/2014-09-01/docs-2.json",
-  examples: "#{Aws::API_DIR}/rds/2014-09-01/examples-1.json",
-  paginators: "#{Aws::API_DIR}/rds/2014-09-01/paginators-1.json",
+  api: "#{Aws::API_DIR}/rds/2014-10-31/api-2.json",
+  docs: "#{Aws::API_DIR}/rds/2014-10-31/docs-2.json",
+  examples: "#{Aws::API_DIR}/rds/2014-10-31/examples-1.json",
+  paginators: "#{Aws::API_DIR}/rds/2014-10-31/paginators-1.json",
 })
 
 module Aws
