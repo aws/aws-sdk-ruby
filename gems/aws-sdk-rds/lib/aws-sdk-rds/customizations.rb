d8cae42f35f1983298693b85852a292b7080beca
cjyclaire <chejingy@amazon.com>
2017-04-27 14:58:09 -0700

    [Code Gen] Port over Rds auth token generator to code-gen (#1500)
    * Port over Rds auth token generator to code-gen
    * port over ox fix as well

--- gems/aws-sdk-rds/lib/aws-sdk-rds/customizations.rb
+++ gems/aws-sdk-rds/lib/aws-sdk-rds/customizations.rb
@@ -1,7 +1,2 @@
-# WARNING ABOUT GENERATED CODE
-#
-# This file is generated. See the contributing for info on making contributions:
-# https://github.com/aws/aws-sdk-ruby/blob/master/CONTRIBUTING.md
-#
-# WARNING ABOUT GENERATED CODE
-
+# customizations to generated classes
+require 'aws-sdk-rds/customizations/auth_token_generator'
