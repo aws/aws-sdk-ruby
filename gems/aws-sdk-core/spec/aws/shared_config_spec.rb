651a1dd72fd54004ee95db270d5be787c59a2b69
Mark Lee <aws+ruby@lazymalevolence.com>
2017-09-03 19:05:30 -0700

    Allow setting config/credential files via environment variables
    Adds support for setting the paths to the config and credentials files
    via environment variables, named the same as the ones used by
    botocore/awscli.
    
    * config file: `AWS_CONFIG_FILE`
    * credentials file: `AWS_SHARED_CREDENTIALS_FILE`

--- gems/aws-sdk-core/spec/aws/shared_config_spec.rb
+++ gems/aws-sdk-core/spec/aws/shared_config_spec.rb
@@ -37,6 +37,20 @@ module Aws
         )
       end
 
+      it 'will use the ENV variable AWS_SHARED_CREDENTIALS_FILE if set' do
+        expected_credentials_path = '/tmp/aws-test-credentials.ini'
+        stub_const('ENV', 'AWS_SHARED_CREDENTIALS_FILE' => expected_credentials_path)
+        config = SharedConfig.new
+        expect(config.credentials_path).to eq(expected_credentials_path)
+      end
+
+      it 'will use the ENV variable AWS_CONFIG_FILE if set' do
+        expected_config_path = '/tmp/aws-test-config.ini'
+        stub_const('ENV', 'AWS_CONFIG_FILE' => expected_config_path)
+        config = SharedConfig.new(config_enabled: true)
+        expect(config.config_path).to eq(expected_config_path)
+      end
+
       it 'will not load the shared config file if no ENV variable set' do
         config = SharedConfig.new
         expect(config.config_path).to be_nil
