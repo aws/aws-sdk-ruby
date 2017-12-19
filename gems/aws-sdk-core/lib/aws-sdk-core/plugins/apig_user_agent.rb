36e54467639f5bf05597517da1d820d114e8911b
cjyclaire <cjyclaire730@gmail.com>
2017-12-19 12:31:13 -0800

    add Ruby info

--- gems/aws-sdk-core/lib/aws-sdk-core/plugins/apig_user_agent.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/plugins/apig_user_agent.rb
@@ -15,6 +15,14 @@ module Aws
 
         def set_user_agent(context)
           ua = "aws-apig-ruby/#{CORE_GEM_VERSION}"
+
+          begin
+            ua += " #{RUBY_ENGINE}/#{RUBY_VERSION}"
+          rescue
+            ua += " RUBY_ENGINE_NA/#{RUBY_VERSION}"
+          end
+
+          ua += " #{RUBY_PLATFORM}"
           ua += " #{context.config.user_agent_suffix}" if context.config.user_agent_suffix
 
           context.http_request.headers['User-Agent'] = ua.strip
