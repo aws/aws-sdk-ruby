9b440b406d72768480d2136fde3d97f6dcade07c
cjyclaire <cjyclaire730@gmail.com>
2017-12-19 12:26:38 -0800

    Clean up user agent pattern

--- gems/aws-sdk-core/lib/aws-sdk-core/plugins/apig_user_agent.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/plugins/apig_user_agent.rb
@@ -14,21 +14,7 @@ module Aws
         end
 
         def set_user_agent(context)
-          ua = "aws-apig-ruby"
-          ua += "aws-sdk-ruby3/#{CORE_GEM_VERSION}"
-
-          begin
-            ua += " #{RUBY_ENGINE}/#{RUBY_VERSION}"
-          rescue
-            ua += " RUBY_ENGINE_NA/#{RUBY_VERSION}"
-          end
-
-          ua += " #{RUBY_PLATFORM}"
-
-          if context[:gem_name] && context[:gem_version]
-            ua += " #{context[:gem_name]}/#{context[:gem_version]}"
-          end
-
+          ua = "aws-apig-ruby/#{CORE_GEM_VERSION}"
           ua += " #{context.config.user_agent_suffix}" if context.config.user_agent_suffix
 
           context.http_request.headers['User-Agent'] = ua.strip
