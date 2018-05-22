be246413ffef6ba4f768f71db1f557da64f4846b
cjyclaire <cjyclaire730@gmail.com>
2018-05-21 19:31:16 -0700

    Make event emitter under module Aws

--- gems/aws-sdk-core/lib/aws-sdk-core/event_emitter.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/event_emitter.rb
@@ -1,18 +1,20 @@
-class EventEmitter
+module Aws
+  class EventEmitter
 
-  def initialize
-    @listeners = {}
-  end
-
-  def on(type, callback)
-    (@listeners[type] ||= []) << callback
-  end
-
-  def signal(type, event)
-    return unless @listeners[type]
-    @listeners[type].each do |listener|
-      listener.call(event) if event.event_type == type
+    def initialize
+      @listeners = {}
     end
-  end
 
+    def on(type, callback)
+      (@listeners[type] ||= []) << callback
+    end
+
+    def signal(type, event)
+      return unless @listeners[type]
+      @listeners[type].each do |listener|
+        listener.call(event) if event.event_type == type
+      end
+    end
+
+  end
 end
