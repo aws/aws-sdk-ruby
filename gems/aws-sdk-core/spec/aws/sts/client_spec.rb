98e6ce48d98eb9f7a598b5d8828606bcd4efa689
S.Cavallo <smcavallo@hotmail.com>
2018-08-06 10:50:49 -0400

    stub utc Signed-off-by: S.Cavallo <smcavallo@hotmail.com>

--- gems/aws-sdk-core/spec/aws/sts/client_spec.rb
+++ gems/aws-sdk-core/spec/aws/sts/client_spec.rb
@@ -28,19 +28,26 @@ module Aws
       end
 
       describe 'get_caller_identity_presigned_url' do
+
+        let(:now) { double('now') }
+        let(:utc) { double('utc-time') }
+        let(:datetime) { '20180805T000000Z' }
+
+        before (:each) do
+          allow(Time).to receive(:now).and_return(now)
+          allow(now).to receive(:utc).and_return(utc)
+          allow(utc).to receive(:strftime).and_return(datetime)
+        end
+
         it 'gets a presigned_url' do
           client = Aws::STS::Client.new(region: 'us-west-2', credentials: Credentials.new('akid', 'secret'))
-          client.handle(step: :send) do |context|
-            Seahorse::Client::Response.new(context: context)
-          end
-
-          now = Time.mktime(2018, 8, 6)
-          allow(Time).to receive(:now).and_return(now)
 
           resp = client.get_caller_identity_presigned_url(
-            headers: { 'X-K8s-Aws-Id' => 'my-eks-cluster' }
+            headers: { 'X-K8s-Aws-Id' => 'my-eks-cluster' },
+            expires_in: 59
           )
-          expect(resp).to eq('https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=akid%2F20180806%2Fus-west-2%2Fsts%2Faws4_request&X-Amz-Date=20180806T040000Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host%3Bx-k8s-aws-id&X-Amz-Signature=6b79fa6f572514cda41b91d6d4eae9d8ea0f04a271c29d80925b9177f2575274')
+
+          expect(resp).to eq('https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=akid%2F20180805%2Fus-west-2%2Fsts%2Faws4_request&X-Amz-Date=20180805T000000Z&X-Amz-Expires=59&X-Amz-SignedHeaders=host%3Bx-k8s-aws-id&X-Amz-Signature=d1f6551aafc0f5c57971fe161a12cbe0539fc1681698ea4a4c0fde5abc51e32e')
         end
       end
     end
