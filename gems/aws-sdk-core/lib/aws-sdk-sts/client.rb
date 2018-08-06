8e8551bed8758518537594f37ab05f694ca1339c
S.Cavallo <smcavallo@hotmail.com>
2018-08-06 09:33:55 -0400

    Add method for getting a presigned_url for the get_caller_identity sts service Signed-off-by: S.Cavallo <smcavallo@hotmail.com>

--- gems/aws-sdk-core/lib/aws-sdk-sts/client.rb
+++ gems/aws-sdk-core/lib/aws-sdk-sts/client.rb
@@ -1120,6 +1120,62 @@ module Aws::STS
       req.send_request(options)
     end
 
+    # Returns a presigned url for the get_caller_identity service.
+    # The primary use is to generate a token which can be used to authenticate
+    # with the EKS service.
+    #
+    # @option options [Hash] :headers
+    #   Headers that should be signed and sent along with the request.
+    #   All x-amz-* headers must be present during signing.
+    #   Other headers are optional.
+    #
+    # @option options [Integer] :expires_in
+    #   How long the presigned URL should be valid for. Defaults to 1 minute
+    #
+    # @return [String] Returns a presigned url string
+    #
+    # @example Example: To get a presigned URL which can be used to authenticate with eks
+    #
+    #   resp = client.get_caller_identity_presigned_url({
+    #     headers: {"X-K8s-Aws-Id" => 'my-eks-cluster'},
+    #   })
+    #
+    #   resp outputs the following:
+    #
+    # https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIOSFODNN7EXAMPLE%2F20180806%2Fus-east-1%2Fsts%2Faws4_request&X-Amz-Date=20180806T130142Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host%3Bx-k8s-aws-id&X-Amz-Signature=wEXAMPLEtc764bNrC9SAPBSM22wDOk4x4HIZ8j4FZTwdQWLWsKWHGBuFqwAeMicRXmx
+    #
+    # This can easily be converted to a token which can be used by the eks service:
+    # https://ruby-doc.org/stdlib-2.3.1/libdoc/base64/rdoc/Base64.html#method-i-encode64
+    # "k8s-aws-v1." + Base64.urlsafe_encode64(presignedURLString.to_s).chomp("==")
+    #
+    #
+    # @param [Hash] params ({})
+    def get_caller_identity_presigned_url(options = {})
+
+      req = build_request(:get_caller_identity, {})
+
+      param_list = Aws::Query::ParamList.new
+      param_list.set('Action', 'GetCallerIdentity')
+      param_list.set('Version', req.context.config.api.version)
+      Aws::Query::EC2ParamBuilder.new(param_list).apply(req.context.operation.input, {})
+
+      signer = Aws::Sigv4::Signer.new(
+          service: 'sts',
+          region: req.context.config.region,
+          credentials_provider: req.context.config.credentials
+      )
+      url = Aws::Partitions::EndpointProvider.resolve(req.context.config.region, 'sts')
+      url += "/?#{param_list.to_s}"
+
+      signer.presign_url(
+          http_method: 'GET',
+          url: url,
+          body: '',
+          headers: options[:headers],
+          expires_in: options[:expires_in] || 60
+      ).to_s
+    end
+
     # Returns a set of temporary security credentials (consisting of an
     # access key ID, a secret access key, and a security token) for a
     # federated user. A typical use is in a proxy application that gets
--- gems/aws-sdk-core/spec/aws/sts/client_spec.rb
+++ gems/aws-sdk-core/spec/aws/sts/client_spec.rb
@@ -3,14 +3,13 @@ require_relative '../../spec_helper'
 module Aws
   module STS
     describe Client do
-
       it 'constructs the proper endpoint in gov-cloud' do
-        client = Client.new(region:'us-gov-west-1', credentials: Credentials.new('akid', 'secret'))
+        client = Client.new(region: 'us-gov-west-1', credentials: Credentials.new('akid', 'secret'))
         expect(client.config.endpoint.to_s).to eq('https://sts.us-gov-west-1.amazonaws.com')
       end
 
       it 'makes assume_role_with_* calls without authentication' do
-        client = Aws::STS::Client.new(region:'us-east-1', credentials:nil)
+        client = Aws::STS::Client.new(region: 'us-east-1', credentials: nil)
         client.handle(step: :send) do |context|
           Seahorse::Client::Response.new(context: context)
         end
@@ -28,6 +27,25 @@ module Aws
         expect(resp.context.http_request.headers['authorization']).to be(nil)
       end
 
+      describe 'get_caller_identity_presigned_url' do
+        let(:client) do
+          Client.new(
+            region: 'us-west-2',
+            credentials: Credentials.new('akid', 'secret'),
+            stub_responses: true
+          )
+        end
+
+        it 'gets a presigned_url' do
+          now = Time.mktime(2018, 8, 6)
+          allow(Time).to receive(:now).and_return(now)
+
+          resp = client.get_caller_identity_presigned_url(
+            headers: { 'X-K8s-Aws-Id' => 'my-eks-cluster' }
+          )
+          expect(resp).to eq('https://sts.amazonaws.com/?Action=GetCallerIdentity&Version=2011-06-15&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=akid%2F20180806%2Fus-west-2%2Fsts%2Faws4_request&X-Amz-Date=20180806T040000Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host%3Bx-k8s-aws-id&X-Amz-Signature=6b79fa6f572514cda41b91d6d4eae9d8ea0f04a271c29d80925b9177f2575274')
+        end
+      end
     end
   end
 end
