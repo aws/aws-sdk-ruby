718bfafb0d73a4fbccebb7ce0535947c72e77014
Alex Wood <awood45@gmail.com>
2018-09-05 15:26:25 -0700

    Add CSM Client Plugins and Minimum Core Version

--- gems/aws-sdk-swf/lib/aws-sdk-swf/client.rb
+++ gems/aws-sdk-swf/lib/aws-sdk-swf/client.rb
@@ -110,7 +110,7 @@ module Aws::SWF
     #   Required for publishing client metrics. The port that the client side monitoring
     #   agent is running on, where client metrics will be published via UDP.
     #
-    # @option options [] :client_side_monitoring_publisher (#<Aws::ClientSideMonitoring::Publisher:0x00007f20e3c7b9f0 @agent_port=nil, @mutex=#<Thread::Mutex:0x00007f20e3c7b9a0>>)
+    # @option options [] :client_side_monitoring_publisher (#<Aws::ClientSideMonitoring::Publisher:0x007fc512238758 @agent_port=nil, @mutex=#<Thread::Mutex:0x007fc5122386e0>>)
     #   Allows you to provide a custom client-side monitoring publisher class. By default,
     #   will use the Client Side Monitoring Agent Publisher.
     #
--- gems/aws-sdk-waf/aws-sdk-waf.gemspec
+++ gems/aws-sdk-waf/aws-sdk-waf.gemspec
@@ -23,7 +23,7 @@ Gem::Specification.new do |spec|
     'changelog_uri'   => 'https://github.com/aws/aws-sdk-ruby/tree/master/gems/aws-sdk-waf/CHANGELOG.md'
   }
 
-  spec.add_dependency('aws-sdk-core', '~> 3')
+  spec.add_dependency('aws-sdk-core', '~> 3', '>= 3.26.0')
   spec.add_dependency('aws-sigv4', '~> 1.0')
 
 end
