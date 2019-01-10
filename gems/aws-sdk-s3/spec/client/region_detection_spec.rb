f9563373d5e75b447686d7cc44dd38faa86117bc
Alex Wood <awood45@gmail.com>
2019-01-10 12:02:48 -0800

    CSM Unit Test for S3 400 Redirect

--- gems/aws-sdk-s3/spec/client/region_detection_spec.rb
+++ gems/aws-sdk-s3/spec/client/region_detection_spec.rb
@@ -41,7 +41,7 @@ module Aws
         before(:each) do
 
           stub_request(:put, 'https://bucket.s3.us-west-2.amazonaws.com/key').
-            to_return(status: [400, 'Bad Request'], body: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Error><Code>AuthorizationHeaderMalformed</Code><Message>The authorization header is malformed; the region 'us-east-1' is wrong; expecting 'eu-central-1'</Message><Region>eu-central-1</Region><RequestId>531B68B3613F5C96</RequestId><HostId>TMnOREh0Ms0touCRX0XkJinw7xqsF0v/iFyA+nCC4d3PpF+k2oekWlSrUk+8d2/rvcnEv2QXer0=</HostId></Error>")
+            to_return(status: [400, 'Bad Request'], body: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Error><Code>AuthorizationHeaderMalformed</Code><Message>The authorization header is malformed; the region 'us-west-2' is wrong; expecting 'eu-central-1'</Message><Region>eu-central-1</Region><RequestId>531B68B3613F5C96</RequestId><HostId>TMnOREh0Ms0touCRX0XkJinw7xqsF0v/iFyA+nCC4d3PpF+k2oekWlSrUk+8d2/rvcnEv2QXer0=</HostId></Error>")
 
           stub_request(:put, 'https://bucket.s3-fips.us-west-2.amazonaws.com/keya').
             to_return(status: [400, 'Bad Request'], body: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Error><Code>AuthorizationHeaderMalformed</Code><Message>The authorization header is malformed; the region 'us-west-2' is wrong; expecting 'eu-central-1'</Message><Region>eu-central-1</Region><RequestId>531B68B3613F5C96</RequestId><HostId>TMnOREh0Ms0touCRX0XkJinw7xqsF0v/iFyA+nCC4d3PpF+k2oekWlSrUk+8d2/rvcnEv2QXer0=</HostId></Error>")
@@ -85,6 +85,60 @@ module Aws
         end
 
       end
+
+      describe "Client Side Monitoring" do
+        let(:stub_publisher) do
+          StubPublisher.new
+        end
+
+        let(:client_opts) {{
+          region: 'us-west-2',
+          credentials: Credentials.new('akid', 'secret'),
+          client_side_monitoring: true,
+          client_side_monitoring_publisher: stub_publisher
+        }}
+
+        let(:client) { Client.new(client_opts) }
+
+        before(:each) do
+          stub_publisher.metrics = []
+        end
+
+        it 'updates fqdn and region in metrics when an HTTP 400 redirect occurs' do
+          stub_request(:put, 'https://bucket.s3.us-west-2.amazonaws.com/key').
+            to_return(status: [400, 'Bad Request'], body: "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Error><Code>AuthorizationHeaderMalformed</Code><Message>The authorization header is malformed; the region 'us-east-1' is wrong; expecting 'eu-central-1'</Message><Region>eu-central-1</Region><RequestId>531B68B3613F5C96</RequestId><HostId>TMnOREh0Ms0touCRX0XkJinw7xqsF0v/iFyA+nCC4d3PpF+k2oekWlSrUk+8d2/rvcnEv2QXer0=</HostId></Error>")
+          stub_request(:put, 'https://bucket.s3.eu-central-1.amazonaws.com/key').
+            to_return(status: [200, 'Ok'])
+          resp = client.put_object(bucket:'bucket', key:'key', body:'body')
+          expect(stub_publisher.metrics.size).to eq(1)
+          metric = stub_publisher.metrics.first
+          expect(metric.api_call.region).to eq("eu-central-1")
+          expect(metric.api_call_attempts.size).to eq(2)
+          fail_attempt = metric.api_call_attempts[0]
+          good_attempt = metric.api_call_attempts[1]
+          expect(fail_attempt.region).to eq("us-west-2")
+          expect(fail_attempt.fqdn).to eq("bucket.s3.us-west-2.amazonaws.com")
+          expect(fail_attempt.http_status_code).to eq(400)
+          expect(good_attempt.region).to eq("eu-central-1")
+          expect(good_attempt.fqdn).to eq("bucket.s3.eu-central-1.amazonaws.com")
+          expect(good_attempt.http_status_code).to eq(200)
+        end
+      end
+
     end
+
+    class StubPublisher
+      attr_accessor :metrics
+      attr_accessor :agent_port
+      
+      def initialize
+        @metrics = []
+      end
+
+      def publish(request_metrics)
+        @metrics << request_metrics
+      end
+    end
+
   end
 end
