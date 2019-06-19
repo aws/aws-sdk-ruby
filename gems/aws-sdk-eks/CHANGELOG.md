9235c8ad898f6c4852fb3873fb3f1917025826fc
AWS SDK for Ruby <aws-dr-rubygems@amazon.com>
2019-06-19 19:20:48 +0000

    Updated API models and rebuilt service gems.

--- gems/aws-sdk-eks/CHANGELOG.md
+++ gems/aws-sdk-eks/CHANGELOG.md
@@ -1,6 +1,11 @@
 Unreleased Changes
 ------------------
 
+1.21.0 (2019-06-19)
+------------------
+
+* Feature - Changing Amazon EKS full service name to Amazon Elastic Kubernetes Service.
+
 1.20.0 (2019-06-17)
 ------------------
 
--- gems/aws-sdk-eks/lib/aws-sdk-eks/client.rb
+++ gems/aws-sdk-eks/lib/aws-sdk-eks/client.rb
@@ -978,7 +978,7 @@ module Aws::EKS
         params: params,
         config: config)
       context[:gem_name] = 'aws-sdk-eks'
-      context[:gem_version] = '1.20.0'
+      context[:gem_version] = '1.21.0'
       Seahorse::Client::Request.new(handlers, context)
     end
 
