fc84ab31711c202485d84b1ee95c19f390ee5b5f
Matt Muller <53055821+mullermp@users.noreply.github.com>
2019-10-07 11:01:59 -0700

    Fix up documentation around customized S3 object (#2133)

--- gems/aws-sdk-s3/spec/object_spec.rb
+++ gems/aws-sdk-s3/spec/object_spec.rb
@@ -3,6 +3,19 @@ require_relative 'spec_helper'
 module Aws
   module S3
     class Object
+
+      describe '#copy_from' do
+        it 'pending'
+      end
+
+      describe '#copy_to' do
+        it 'pending'
+      end
+
+      describe '#move_to' do
+        it 'pending'
+      end
+
       describe '#presigned_post' do
 
         let(:object) { Object.new('bucket', 'key', stub_responses: true) }
@@ -14,6 +27,27 @@ module Aws
         end
 
       end
+
+      describe '#presigned_url' do
+        it 'pending'
+      end
+
+      describe '#public_url' do
+        it 'pending'
+      end
+
+      describe '#upload_stream' do
+        it 'pending'
+      end
+
+      describe '#upload_file' do
+        it 'pending'
+      end
+
+      describe '#download_file' do
+        it 'pending'
+      end
+
     end
   end
 end
