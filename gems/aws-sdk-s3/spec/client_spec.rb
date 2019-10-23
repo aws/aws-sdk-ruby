c9a0f04db3c573c7362f95388d45524bfa352bb5
Matt Muller <mamuller@amazon.com>
2019-10-23 14:58:13 -0700

    Heredoc formatting

--- gems/aws-sdk-s3/spec/client_spec.rb
+++ gems/aws-sdk-s3/spec/client_spec.rb
@@ -323,9 +323,9 @@ module Aws
           expect(
             resp.context.http_request.body_contents.strip
           ).to eq(<<-XML.strip)
-            <CreateBucketConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-              <LocationConstraint>us-west-2</LocationConstraint>
-            </CreateBucketConfiguration>
+<CreateBucketConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+  <LocationConstraint>us-west-2</LocationConstraint>
+</CreateBucketConfiguration>
           XML
         end
 
@@ -344,9 +344,9 @@ module Aws
           expect(
             resp.context.http_request.body_contents.strip
           ).to eq(<<-XML.strip)
-            <CreateBucketConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-              <LocationConstraint>EU</LocationConstraint>
-            </CreateBucketConfiguration>
+<CreateBucketConfiguration xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+  <LocationConstraint>EU</LocationConstraint>
+</CreateBucketConfiguration>
           XML
         end
       end
@@ -586,16 +586,16 @@ module Aws
             }
           )
           expect(resp.context.http_request.body_contents).to eq(<<-XML)
-            <AccessControlPolicy xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
-              <AccessControlList>
-                <Grant>
-                  <Grantee xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="CanonicalUser">
-                    <DisplayName>name</DisplayName>
-                  </Grantee>
-                  <Permission>READ</Permission>
-                </Grant>
-              </AccessControlList>
-            </AccessControlPolicy>
+<AccessControlPolicy xmlns="http://s3.amazonaws.com/doc/2006-03-01/">
+  <AccessControlList>
+    <Grant>
+      <Grantee xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:type="CanonicalUser">
+        <DisplayName>name</DisplayName>
+      </Grantee>
+      <Permission>READ</Permission>
+    </Grant>
+  </AccessControlList>
+</AccessControlPolicy>
           XML
         end
       end
