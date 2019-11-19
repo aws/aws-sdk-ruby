831d686a01dc4b2df2327148084049540d79ebd5
Matt Muller <53055821+mullermp@users.noreply.github.com>
2019-11-19 11:14:28 -0800

    Revert "EC2 IMDS security updates (#2167)" (#2169)
    This reverts commit 71ace3c253dc15bf82a8479eab89fe22930a0ce2.

--- aws-sdk-core/spec/spec_helper.rb
+++ aws-sdk-core/spec/spec_helper.rb
@@ -23,10 +23,8 @@ RSpec.configure do |config|
     allow(Dir).to receive(:home).and_raise(ArgumentError)
 
     # disable instance profile credentials
-    token_path = '/latest/api/token'
-    path = '/latest/meta-data/iam/security-credentials/'
-    stub_request(:get, "http://169.254.169.254#{path}").to_raise(SocketError)
-    stub_request(:put, "http://169.254.169.254#{token_path}").to_raise(SocketError)
+    ec2_md_path = '/latest/meta-data/iam/security-credentials/'
+    stub_request(:get, "http://169.254.169.254#{ec2_md_path}").to_raise(SocketError)
 
     Aws.shared_config.fresh
   end
