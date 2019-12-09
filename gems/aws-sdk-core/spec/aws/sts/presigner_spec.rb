41beac4cc244ef687f895648337f7720ec6773ad
Matt Muller <mamuller@amazon.com>
2019-12-09 09:54:09 -0800

    Fix STS presigner endpoints to be regional

--- gems/aws-sdk-core/spec/aws/sts/presigner_spec.rb
+++ gems/aws-sdk-core/spec/aws/sts/presigner_spec.rb
@@ -21,15 +21,16 @@ module Aws
           )
           pre = Presigner.new(client: client)
 
-          expected_url = 'https://sts.amazonaws.com/?Action=GetCallerIdentity'\
-                         '&Version=2011-06-15&X-Amz-Algorithm=AWS4-HMAC-SHA256'\
+          expected_url = 'https://sts.us-west-2.amazonaws.com/'\
+                         '?Action=GetCallerIdentity&Version=2011-06-15'\
+                         '&X-Amz-Algorithm=AWS4-HMAC-SHA256'\
                          '&X-Amz-Credential=akid%2F20160101'\
                          '%2Fus-west-2%2Fsts%2Faws4_request'\
                          '&X-Amz-Date=20160101T112233Z'\
                          '&X-Amz-Expires=900'\
                          '&X-Amz-SignedHeaders=host%3Bx-k8s-aws-id'\
-                         '&X-Amz-Signature=eb82f89680e51b8f315703878fb360674f6'\
-                         '0998df071047f95ea52dfd4f101db'
+                         '&X-Amz-Signature=98828ca931430d3c58b35c01dfa44469658'\
+                         '4d19f131272e9b55cb18a7491f51a'
           actual_url = pre.get_caller_identity_presigned_url(
             headers: { 'X-K8s-Aws-Id' => 'my-eks-cluster' }
           )
