061bbbff404077981646865b2f40c44d9711614d
Matt Muller <53055821+mullermp@users.noreply.github.com>
2020-01-13 14:11:58 -0800

    Add Content-Type for all SSO operations (#2207)

--- gems/aws-sdk-sso/lib/aws-sdk-sso/plugins/content_type.rb
+++ gems/aws-sdk-sso/lib/aws-sdk-sso/plugins/content_type.rb
@@ -4,13 +4,13 @@ module Aws
       class ContentType < Seahorse::Client::Plugin
 
         def add_handlers(handlers, config)
-          handlers.add(Handler, operations: [:get_role_credentials])
+          handlers.add(Handler)
         end
 
         class Handler < Seahorse::Client::Handler
           def call(context)
-            # SSO GetRoleCredentials operation breaks when given an empty
-            # content-type header. The SDK adds this blank content-type header
+            # Some SSO operations break when given an empty content-type header.
+            # The SDK adds this blank content-type header
             # since Net::HTTP provides a default that can break services.
             # We're setting one here even though it's not used or necessary.
             context.http_request.headers['content-type'] = 'application/json'
