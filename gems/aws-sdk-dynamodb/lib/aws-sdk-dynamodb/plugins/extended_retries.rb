17e6a0eee7a605365ca1f4cb460770fe8678ac19
Matt Muller <mamuller@amazon.com>
2020-02-25 16:07:28 -0800

    Fix annoying ddb documentation bug

--- gems/aws-sdk-dynamodb/lib/aws-sdk-dynamodb/plugins/extended_retries.rb
+++ gems/aws-sdk-dynamodb/lib/aws-sdk-dynamodb/plugins/extended_retries.rb
@@ -5,7 +5,6 @@ module Aws
 
         option(:retry_limit,
           default: 10,
-          required: false,
           doc_type: Integer,
           docstring: <<-DOCS)
 The maximum number of times to retry failed requests.  Only
