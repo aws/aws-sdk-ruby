903f6e7983674b9afa0e4d56a28dedee225b4a77
Sarun Rattanasiri <midnight_w@gmx.tw>
2020-03-20 02:09:45 +0700

    fix unseen string mutation

--- gems/aws-sigv4/lib/aws-sigv4/signer.rb
+++ gems/aws-sigv4/lib/aws-sigv4/signer.rb
@@ -564,7 +564,7 @@ module Aws
           OpenSSL::Digest::SHA256.file(value).hexdigest
         elsif value.respond_to?(:read)
           sha256 = OpenSSL::Digest::SHA256.new
-          while chunk = value.read(1024 * 1024, buffer ||= "") # 1MB
+          while chunk = value.read(1024 * 1024, buffer ||= ::String.new) # 1MB
             sha256.update(chunk)
           end
           value.rewind
