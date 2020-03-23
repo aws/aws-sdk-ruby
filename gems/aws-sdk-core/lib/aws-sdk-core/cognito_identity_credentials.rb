1f325f0608d31bbb2dedd217ea138d83710a2aac
Alex Woods <alexwoo@amazon.com>
2020-03-23 11:39:23 -0700

    Working cognito identity provider - needs specs

---
+++ gems/aws-sdk-core/lib/aws-sdk-core/cognito_identity_credentials.rb
@@ -0,0 +1,91 @@
+require 'set'
+require 'securerandom'
+require 'base64'
+
+module Aws
+
+  # An auto-refreshing credential provider
+  # TODO: Add good documentation....
+  class CognitoIdentityCredentials
+
+    include CredentialProvider
+    include RefreshingCredentials
+
+    # TODO: Document me
+    # @option options [String] :identity_id the Cognito identity_id.  Required
+    # unless identity_pool_id is given. A unique
+    # identifier in the format REGION:GUID
+    #
+    # @option options [String] :identity_pool_id Required unless identity_id is
+    # provided. An identity pool ID in the format REGION:GUID.
+    #
+    # @option options [Hash<String,String>] :logins A set of optional
+    # name-value pairs that map provider names to provider tokens.
+    # The name-value pair will follow the syntax
+    # "provider_name": "provider_user_identifier".
+    #
+    # @option options [String] :custom_role_arn (optional) The Amazon Resource
+    # Name (ARN) of the role to be assumed when multiple roles were received
+    # in the token from the identity provider. For example, a SAML-based
+    # identity provider. This parameter is optional for identity providers
+    # that do not support role customization.
+    #
+    # @option options [STS::Client] :client
+    def initialize(options = {})
+      @identity_pool_id = options.delete(:identity_pool_id)
+      @identity_id = options.delete(:identity_id)
+      @custom_role_arn = options.delete(:custom_role_arn)
+      @logins = options.delete(:logins) || {}
+
+      unless @identity_pool_id || @identity_id
+        raise ArgumentError,
+              'Must provide either identity_pool_id or identity_id'
+      end
+
+      @client = options[:client] || CognitoIdentity::Client.new(
+        options.merge(credentials: false)
+      )
+      super
+    end
+
+    # @return [CognitoIdentity::Client]
+    attr_reader :client
+
+    # @return [Hash<String,String>]
+    attr_reader :logins
+
+    # Set the logins, triggers a refresh of credentials
+    # TODO: Trigger refresh when logins[:value] is set
+    def logins=(logins)
+      @logins = logins
+      refresh!
+    end
+
+    # @return [String]
+    def identity_id
+      return @identity_id if @identity_id
+
+      @identity_id = @client
+                     .get_id(identity_pool_id: @identity_pool_id)
+                     .identity_id
+    end
+
+    private
+
+    def refresh
+      resp = @client.get_credentials_for_identity(
+        identity_id: identity_id,
+        custom_role_arn: @custom_role_arn
+      )
+
+      @credentials = Credentials.new(
+        resp.credentials.access_key_id,
+        resp.credentials.secret_key,
+        resp.credentials.session_token
+      )
+      @expiration = resp.credentials.expiration
+    end
+
+  end
+end
+
---
+++ gems/aws-sdk-core/spec/aws/cognito_identity_credentials_spec.rb
@@ -0,0 +1,168 @@
+
+require_relative '../spec_helper'
+
+module Aws
+  describe AssumeRoleWebIdentityCredentials do
+
+    let(:client) {
+      STS::Client.new(
+        region: 'us-west-2',
+        credentials: credentials,
+        stub_responses: true
+      )
+    }
+
+    let(:in_one_hour) { Time.now + 60 * 60 }
+
+    let(:expiration) { in_one_hour }
+
+    let(:credentials) {
+      double('credentials',
+             access_key_id: 'akid',
+             secret_access_key: 'secret',
+             session_token: 'session',
+             expiration: expiration,
+             )
+    }
+
+    let(:token_file) {
+      Tempfile.new("token.jwt")
+    }
+
+    let(:token_file_path) {
+      token_file.path
+    }
+
+    let(:uuid) {
+      "2d931510-d99f-494a-8c67-87feb05e1594"
+    }
+
+    let(:generate_name) {
+      Base64.strict_encode64(uuid)
+    }
+
+    let(:resp) {double('client-resp', credentials: credentials)}
+
+    before(:each) do
+      allow(STS::Client).to receive(:new).and_return(client)
+      allow(SecureRandom).to receive(:uuid).and_return(uuid)
+      allow(client).to receive(:assume_role_with_web_identity).and_return(resp)
+    end
+
+    it 'contructs a default client when not given' do
+      creds = AssumeRoleWebIdentityCredentials.new(
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        role_session_name: "session-name"
+      )
+      expect(creds.client).to be(client)
+    end
+
+    it 'auto populates :session_name when not provided' do
+      expect(client).to receive(:assume_role_with_web_identity).with(
+        role_arn: 'arn',
+        web_identity_token: '',
+        role_session_name: generate_name
+      )
+      AssumeRoleWebIdentityCredentials.new(
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        )
+    end
+
+    it 'populates :web_identity_token from file when valid' do
+      expect {
+        AssumeRoleWebIdentityCredentials.new(
+          role_arn: 'arn')
+      }.to raise_error(Aws::Errors::MissingWebIdentityTokenFile)
+      expect {
+        AssumeRoleWebIdentityCredentials.new(
+          role_arn: 'arn',
+          web_identity_token_file: '/not/exist/file/foo',
+          )
+      }.to raise_error(Aws::Errors::MissingWebIdentityTokenFile)
+
+      token_file.write('token')
+      token_file.flush
+      token_file.close
+
+      expect(client).to receive(:assume_role_with_web_identity).with(
+        role_arn: 'arn',
+        web_identity_token: 'token',
+        role_session_name: "session-name"
+      )
+      AssumeRoleWebIdentityCredentials.new(
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        role_session_name: "session-name"
+      )
+    end
+
+    it 'accepts a client' do
+      creds = AssumeRoleWebIdentityCredentials.new(
+        client: client,
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        )
+      expect(creds.client).to be(client)
+    end
+
+    it 'accepts client options' do
+      expected_client = STS::Client.new(
+        credentials: false, stub_responses: true)
+      expect(STS::Client).to receive(:new).
+        with(region: 'region-name', credentials: false).
+        and_return(expected_client)
+      creds = AssumeRoleWebIdentityCredentials.new(
+        region: 'region-name',
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        )
+      expect(creds.client).to be(expected_client)
+    end
+
+    it 'assumes role with web identity using the client' do
+      expect(client).to receive(:assume_role_with_web_identity).with(
+        role_arn: 'arn',
+        web_identity_token: '',
+        role_session_name: "session-name",
+        provider_id: "urlType",
+        policy: "sessionPolicyDocumentType"
+      )
+      AssumeRoleWebIdentityCredentials.new(
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        role_session_name: "session-name",
+        provider_id: "urlType",
+        policy: "sessionPolicyDocumentType"
+      )
+    end
+
+    it 'extracts credentials from response' do
+      c = AssumeRoleWebIdentityCredentials.new(
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        )
+      expect(c).to be_set
+      expect(c.credentials.access_key_id).to eq('akid')
+      expect(c.credentials.secret_access_key).to eq('secret')
+      expect(c.credentials.session_token).to eq('session')
+      expect(c.expiration).to eq(in_one_hour)
+    end
+
+    it 'auto refreshes credentials when near expiration' do
+      allow(credentials).to receive(:expiration).and_return(Time.now)
+      expect(client).to receive(:assume_role_with_web_identity).exactly(4).times
+      expect(File).to receive(:read).with(token_file_path).exactly(4).times
+
+      c = AssumeRoleWebIdentityCredentials.new(
+        role_arn: 'arn',
+        web_identity_token_file: token_file_path,
+        role_session_name: 'session')
+      c.credentials
+      c.credentials
+      c.credentials
+    end
+
+  end
+end
