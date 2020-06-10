87c3ac545ce9476ed75867359101a8dd195982b1
Matt Muller <mamuller@amazon.com>
2020-06-10 15:38:19 -0700

    Specs for param filter

--- gems/aws-sdk-core/spec/aws/log/param_filter_spec.rb
+++ gems/aws-sdk-core/spec/aws/log/param_filter_spec.rb
@@ -3,55 +3,78 @@ require_relative '../../spec_helper'
 module Aws
   module Log
     describe ParamFilter do
+      let(:service) { 'Peccy Service' }
+      let(:hash_filter) { { service => [:password] } }
+      let(:array_filter) { [:peccy_name] }
+
+      describe '#initialize' do
+        it 'accepts a filter as a hash' do
+          filter = ParamFilter.new(filter: hash_filter)
+          filters = filter.instance_variable_get(:@filters)
+          expect(filters).to include(hash_filter)
+        end
+
+        it 'supports a filter as an array (legacy)' do
+          filter = ParamFilter.new(filter: array_filter)
+          filters = filter.instance_variable_get(:@filters)
+          expect(filters.values).to all(include(*array_filter))
+        end
+      end
+
       describe '#filter' do
+        subject { ParamFilter.new(filter: hash_filter) }
+
         context 'with an array' do
-          context 'with a filtered parameter name' do
-            it 'filters lowercase parameter names' do
-              expect(subject.filter([{ password: 'p@assw0rd'}])).to eq([{ password: '[FILTERED]'} ])
-            end
+          it 'filters lowercase parameter names' do
+            filtered = subject.filter(service, [{ password: 'p@assw0rd' }])
+            expect(filtered).to eq([{ password: '[FILTERED]' }])
+          end
 
-            it 'filters uppercase parameter names' do
-              expect(subject.filter([{ PASSWORD: 'p@assw0rd'}])).to eq([{ PASSWORD: '[FILTERED]'} ])
-            end
+          it 'filters uppercase parameter names' do
+            filtered = subject.filter(service, [{ PASSWORD: 'p@assw0rd' }])
+            expect(filtered).to eq([{ PASSWORD: '[FILTERED]' }])
+          end
 
-            it 'filters mixed-case parameter names' do
-              expect(subject.filter([{ Password: 'p@assw0rd'}])).to eq([{ Password: '[FILTERED]'} ])
-            end
+          it 'filters mixed-case parameter names' do
+            filtered = subject.filter(service, [{ Password: 'p@assw0rd' }])
+            expect(filtered).to eq([{ Password: '[FILTERED]' }])
           end
         end
 
         context 'with a Struct' do
-          context 'with a filtered parameter name' do
-            it 'filters lowercase parameter names' do
-              instance = Struct.new(:password).new('p@assw0rd')
-              expect(subject.filter(instance)).to eq(password: '[FILTERED]')
-            end
+          it 'filters lowercase parameter names' do
+            instance = Struct.new(:password).new('p@assw0rd')
+            filtered = subject.filter(service, instance)
+            expect(filtered).to eq(password: '[FILTERED]')
+          end
 
-            it 'filters uppercase parameter names' do
-              instance = Struct.new(:PASSWORD).new('p@assw0rd')
-              expect(subject.filter(instance)).to eq(PASSWORD: '[FILTERED]')
-            end
+          it 'filters uppercase parameter names' do
+            instance = Struct.new(:PASSWORD).new('p@assw0rd')
+            filtered = subject.filter(service, instance)
+            expect(filtered).to eq(PASSWORD: '[FILTERED]')
+          end
 
-            it 'filters mixed-case parameter names' do
-              instance = Struct.new(:Password).new('p@assw0rd')
-              expect(subject.filter(instance)).to eq(Password: '[FILTERED]')
-            end
+          it 'filters mixed-case parameter names' do
+            instance = Struct.new(:Password).new('p@assw0rd')
+            filtered = subject.filter(service, instance)
+            expect(filtered).to eq(Password: '[FILTERED]')
           end
         end
 
         context 'with a hash' do
-          context 'with a filtered parameter name' do
-            it 'filters lowercase parameter names' do
-              expect(subject.filter(password: 'p@ssw0rd')).to eq(password: '[FILTERED]')
-            end
+          it 'filters lowercase parameter names' do
+            filtered = subject.filter(service, password: 'p@ssw0rd')
+            expect(filtered).to eq(password: '[FILTERED]')
+          end
 
-            it 'filters uppercase parameter names' do
-              expect(subject.filter(PASSWORD: 'p@ssw0rd')).to eq(PASSWORD: '[FILTERED]')
-            end
+          it 'filters uppercase parameter names' do
+            filtered = subject.filter(service, PASSWORD: 'p@ssw0rd')
+            expect(filtered).to eq(PASSWORD: '[FILTERED]')
+          end
 
-            it 'filters mixed-case parameter names' do
-              expect(subject.filter(Password: 'p@ssw0rd')).to eq(Password: '[FILTERED]')
-            end
+          it 'filters mixed-case parameter names' do
+            filtered = subject.filter(service, Password: 'p@ssw0rd')
+            expect(filtered).to eq(Password: '[FILTERED]')
           end
         end
       end
