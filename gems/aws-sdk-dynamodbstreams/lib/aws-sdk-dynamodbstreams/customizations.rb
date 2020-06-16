1dc5c06e2ae396a96627ae45c339c961e8caf2c9
Jean Boussier <jean.boussier@gmail.com>
2020-06-16 13:53:36 +0200

    Add frozen_string_literals comment aws-sdk-dynamodb*

--- gems/aws-sdk-dynamodbstreams/lib/aws-sdk-dynamodbstreams/customizations.rb
+++
@@ -1,7 +0,0 @@
-# WARNING ABOUT GENERATED CODE
-#
-# This file is generated. See the contributing for info on making contributions:
-# https://github.com/aws/aws-sdk-ruby/blob/master/CONTRIBUTING.md
-#
-# WARNING ABOUT GENERATED CODE
-
