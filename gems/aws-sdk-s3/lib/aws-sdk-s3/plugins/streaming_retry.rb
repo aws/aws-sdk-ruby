8af0fa32494f3be366849601deb04c790fef9cfd
Alex Woods <alexwoo@amazon.com>
2020-06-22 16:33:37 -0700

    Working retries!

--- gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/streaming_retry.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/streaming_retry.rb
@@ -7,16 +7,13 @@ module Aws
       # @api private
       class RetryableBlockIO
         extend Forwardable
-        def_delegators :@block_io, :config, :delete_object, :head_object, :build_request
+        def_delegators :@block_io, :write, :read, :size
 
         def initialize(block_io)
           @block_io = block_io
-          super
         end
 
-        def truncate(integer)
-          puts "RetryableBlockIO: truncate: #{integer}"
-        end
+        def truncate(_integer); end
       end
 
       # TODO: docs
@@ -26,12 +23,13 @@ module Aws
         class Handler < Seahorse::Client::Handler
 
           def call(context)
-            if context.params.is_a?(Hash) && context.params[:response_target]
-              target = context.params.delete(:response_target)
-            else
-              target = context[:response_target]
+            target = context.params[:response_target] || context[:response_target]
+
+            # Streaming retry is only supported when response_target is a Proc
+            # AND when range is NOT set on the initial request
+            if target && target.is_a?(Proc) && !context.params[:range]
+              add_event_listeners(context, target)
             end
-            add_event_listeners(context, target) if target && target.is_a?(Proc)
             @handler.call(context)
           end
 
@@ -39,31 +37,30 @@ module Aws
 
           def add_event_listeners(context, target)
             handler = self
-            puts "S3: adding event handlers"
             context.http_response.on_headers(200..299) do
-              puts "S3:  on_headers"
               if context.http_response.body.is_a? Seahorse::Client::BlockIO
-                puts "S3: Replacing body"
                 context.http_response.body = RetryableBlockIO.new(context.http_response.body)
               end
             end
 
-            context.http_response.on_success(200..299) do
-              puts "S3: on_sucess"
-            end
+            context.http_response.on_error do |error|
+              bytes_expected = context.http_response.headers['content-length'].to_i
+              if context.http_response.body.is_a?(RetryableBlockIO) &&
+                  error.is_a?(Seahorse::Client::NetworkingError) &&
+                  error.original_error.is_a?(Seahorse::Client::NetHttp::Handler::TruncatedBodyError) &&
+                  bytes_expected > context.http_response.body.size
+                context.http_request.headers[:range] = "bytes=#{context.http_response.body.size}-"
 
-            context.http_response.on_error do
-              puts "S3: on_error"
-              puts "#context.params[:range]"
-              # Check the body type and the range parameter
-              # Maybe push the range into the blockIO?
+              else
+                context.http_response.body = StringIO.new # something to write the error to
+              end
             end
           end
 
         end
 
         handler(Handler,
-                step: :initialize, # TODO: what step??
+                step: :initialize,
                 operations: [:get_object]
         )
       end
