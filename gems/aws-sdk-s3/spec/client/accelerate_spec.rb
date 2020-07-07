a7c5cd5537fbf0fa271e55c9cbd4bca5a814970e
Matt Muller <mamuller@amazon.com>
2020-07-07 11:26:01 -0700

    Cleanup accelerate plugin

--- gems/aws-sdk-s3/spec/client/accelerate_spec.rb
+++ gems/aws-sdk-s3/spec/client/accelerate_spec.rb
@@ -6,23 +6,28 @@ require 'stringio'
 module Aws
   module S3
     describe Client do
-      let(:options) { { stub_responses: true, region: 'us-east-1' } }
+      let(:options) do
+        { stub_responses: true, region: 'us-east-1' }
+      end
 
       let(:client) { Client.new(options) }
 
       let(:accelerated_client) do
-        Client.new(options.merge(use_accelerate_endpoint: true))
+        Client.new(
+          options.merge(use_accelerate_endpoint: true)
+        )
       end
 
       let(:accel_dualstack_client) do
         Client.new(
           options.merge(
-            use_accelerate_endpoint: true, use_dualstack_endpoint: true
+            use_accelerate_endpoint: true,
+            use_dualstack_endpoint: true
           )
         )
       end
 
-      describe ':use_accelerate_endpoint' do
+      context ':use_accelerate_endpoint' do
         it 'defaults to false' do
           client = Client.new(options)
           expect(client.config.use_accelerate_endpoint).to be(false)
@@ -87,44 +92,44 @@ module Aws
           )
           expect(resp.context.http_request.headers['expect']).to be_nil
         end
-      end
 
-      describe ':use_accelerate_endpoint with :use_dualstack_endpoint' do
-        it 'properly uses the combined endpoint' do
-          resp = accel_dualstack_client.put_object(
-            bucket: 'bucket-name', key: 'key', use_accelerate_endpoint: true
-          )
-          expect(resp.context.http_request.endpoint.to_s).to eq(
-            'https://bucket-name.s3-accelerate.dualstack.amazonaws.com/key'
-          )
-        end
+        context ':use_dualstack_endpoint' do
+          it 'properly uses the combined endpoint' do
+            resp = accel_dualstack_client.put_object(
+              bucket: 'bucket-name', key: 'key', use_accelerate_endpoint: true
+            )
+            expect(resp.context.http_request.endpoint.to_s).to eq(
+              'https://bucket-name.s3-accelerate.dualstack.amazonaws.com/key'
+            )
+          end
 
-        it 'does not apply to #create_bucket' do
-          resp = accel_dualstack_client.create_bucket(bucket: 'bucket-name')
-          expect(resp.context.http_request.endpoint.to_s).to eq(
-            'https://bucket-name.s3.dualstack.us-east-1.amazonaws.com/'
-          )
-        end
+          it 'does not apply to #create_bucket' do
+            resp = accel_dualstack_client.create_bucket(bucket: 'bucket-name')
+            expect(resp.context.http_request.endpoint.to_s).to eq(
+              'https://bucket-name.s3.dualstack.us-east-1.amazonaws.com/'
+            )
+          end
 
-        it 'does not apply to #list_buckets' do
-          resp = accel_dualstack_client.list_buckets
-          expect(resp.context.http_request.endpoint.to_s).to eq(
-            'https://s3.dualstack.us-east-1.amazonaws.com/'
-          )
-        end
+          it 'does not apply to #list_buckets' do
+            resp = accel_dualstack_client.list_buckets
+            expect(resp.context.http_request.endpoint.to_s).to eq(
+              'https://s3.dualstack.us-east-1.amazonaws.com/'
+            )
+          end
 
-        it 'does not apply to #delete_bucket' do
-          resp = accel_dualstack_client.delete_bucket(bucket: 'bucket-name')
-          expect(resp.context.http_request.endpoint.to_s).to eq(
-            'https://bucket-name.s3.dualstack.us-east-1.amazonaws.com/'
-          )
-        end
+          it 'does not apply to #delete_bucket' do
+            resp = accel_dualstack_client.delete_bucket(bucket: 'bucket-name')
+            expect(resp.context.http_request.endpoint.to_s).to eq(
+              'https://bucket-name.s3.dualstack.us-east-1.amazonaws.com/'
+            )
+          end
 
-        it 'does not contain `expect` header in request' do
-          resp = accel_dualstack_client.put_object(
-            bucket: 'bucket-name', key: 'key', body: 'foo'
-          )
-          expect(resp.context.http_request.headers['expect']).to be_nil
+          it 'does not contain `expect` header in request' do
+            resp = accel_dualstack_client.put_object(
+              bucket: 'bucket-name', key: 'key', body: 'foo'
+            )
+            expect(resp.context.http_request.headers['expect']).to be_nil
+          end
         end
       end
     end
