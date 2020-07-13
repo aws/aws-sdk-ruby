a4d362b1f73af73f9a7020eb67edfcd593f0dd1f
Alex Woods <alexwoo@amazon.com>
2020-07-13 12:50:09 -0700

    Update example docs with new header parameter

--- gems/aws-sdk-s3/lib/aws-sdk-s3/client.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/client.rb
@@ -4589,8 +4589,10 @@ module Aws::S3
     #
     # @example Streaming data to a block
     #   # WARNING: yielding data to a block disables retries of networking errors
+    #   # However truncation of the body will be retried automatically using a range request
     #   File.open('/path/to/file', 'wb') do |file|
-    #     s3.get_object(bucket: 'bucket-name', key: 'object-key') do |chunk|
+    #     s3.get_object(bucket: 'bucket-name', key: 'object-key') do |chunk, headers|
+    #       # headers['content-length']
     #       file.write(chunk)
     #     end
     #   end
