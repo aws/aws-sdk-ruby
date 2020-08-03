566afd0aedf735a5e5e4160b8901c80882f9801f
AWS SDK for Ruby <aws-dr-rubygems@amazon.com>
2020-08-03 21:07:25 +0000

    Updated service API models for release.

--- aws-sdk-core/lib/aws-sdk-core/ssm.rb
+++ aws-sdk-core/lib/aws-sdk-core/ssm.rb
@@ -3,4 +3,5 @@ Aws.add_service(:SSM, {
   docs: "#{Aws::API_DIR}/ssm/2014-11-06/docs-2.json",
   examples: "#{Aws::API_DIR}/ssm/2014-11-06/examples-1.json",
   paginators: "#{Aws::API_DIR}/ssm/2014-11-06/paginators-1.json",
+  waiters: "#{Aws::API_DIR}/ssm/2014-11-06/waiters-2.json",
 })
