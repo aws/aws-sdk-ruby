5fac5ce7e67e90c5b91c49a20472999837a30c4e
Matt Muller <mamuller@amazon.com>
2020-08-21 10:34:53 -0700

    Update client docs

--- doc-src/templates/default/fulldoc/html/setup.rb
+++ doc-src/templates/default/fulldoc/html/setup.rb
@@ -1,13 +1,18 @@
 def class_list(root = Registry.root, tree = TreeContext.new)
+  children = run_verifier(root.children)
+  if root == Registry.root
+    children += @items.select {|o| o.namespace.is_a?(CodeObjects::Proxy) }
+  end
+  service_children = Registry.at('Aws').children.select { |c| c.group == 'service' }
+
   out = ''
   # non-service classes
   out << "<li class='#{tree.classes.join(' ')} nolink'>"
   out << "<div class='item' style='padding-left:#{tree.indent}'>"
   out << "<a class='toggle'></a> Non-Service Classes"
   out << "</div><ul>"
-  children = run_verifier(root.children)
   tree.nest do
-    out << class_list_children(children, tree:tree, skip_services:true)
+    out << class_list_children(children, tree, skip_services: true)
   end
   out << '</ul></li>'
 
@@ -16,21 +21,19 @@ def class_list(root = Registry.root, tree = TreeContext.new)
   out << "<div class='item' style='padding-left:#{tree.indent}'>"
   out << "<a class='toggle'></a> Service Gems"
   out << "</div><ul>"
-  children = Registry.at('Aws').children.select { |c| c.group == 'service' }
   tree.nest do
-    out << class_list_children(children.sort_by(&:name), tree:tree)
+    out << class_list_children(service_children, tree)
   end
   out << '</ul></li>'
   out
 end
 
-def class_list_children(children, options)
-  tree = options[:tree]
+def class_list_children(children, tree, options = {})
   skip_services = options[:skip_services]
-  out = ''
+  out = String.new("")
   children.compact.sort_by(&:path).each do |child|
     if child.is_a?(CodeObjects::NamespaceObject)
-      next if child.group == 'service' if skip_services
+      next if skip_services && child.group == 'service'
       name = child.namespace.is_a?(CodeObjects::Proxy) ? child.path : child.name
       grand_children = run_verifier(child.children)
       is_parent = grand_children.any? {|o| o.is_a?(CodeObjects::NamespaceObject) }
@@ -52,7 +55,7 @@ def class_list_children(children, options)
       out << "</div>"
       if is_parent
         tree.nest do
-          out << "<ul>#{class_list_children(grand_children, options)}</ul>"
+          out << "<ul>#{class_list_children(grand_children, tree, options)}</ul>"
         end
       end
       out << "</li>"
