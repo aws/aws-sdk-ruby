56e4a6f98590e05187f336223585f1cebf88121c
Alex Woods <alexwoo@amazon.com>
2020-09-14 14:39:26 -0700

    Add more detailed comment RE: SSO

--- build_tools/aws-sdk-code-generator/lib/aws-sdk-code-generator/views/service_module.rb
+++ build_tools/aws-sdk-code-generator/lib/aws-sdk-code-generator/views/service_module.rb
@@ -53,6 +53,8 @@ module AwsSdkCodeGenerator
 
       # @return [Boolean] - Return true if a check is needed before
       # requiring core to prevent circular dependencies.
+      # This is required to support backwards compatibility for SSO which was
+      # moved from the aws-sdk-sso gem into aws-sdk-core.
       def require_core_guard?
         name == 'SSO'
       end
