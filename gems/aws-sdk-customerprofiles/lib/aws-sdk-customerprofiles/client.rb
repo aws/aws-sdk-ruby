53215cecba72fab3b578bb9dfb9a4c948adc06c9
AWS SDK for Ruby <aws-dr-rubygems@amazon.com>
2020-12-02 03:50:42 +0000

    Updated API models and rebuilt service gems.

--- gems/aws-sdk-customerprofiles/lib/aws-sdk-customerprofiles/client.rb
+++ gems/aws-sdk-customerprofiles/lib/aws-sdk-customerprofiles/client.rb
@@ -1972,7 +1972,7 @@ module Aws::CustomerProfiles
         params: params,
         config: config)
       context[:gem_name] = 'aws-sdk-customerprofiles'
-      context[:gem_version] = '1.0.0'
+      context[:gem_version] = '1.1.0'
       Seahorse::Client::Request.new(handlers, context)
     end
 
