7a7d25fe4ce257cc2cc4d65acd183229543060c6
Matt Muller <mamuller@amazon.com>
2021-05-05 14:23:43 -0700

    Revert "Change to application/json"
    This reverts commit 5383588414cfc2ab62f2735e05e11bd220c2979a.

--- gems/aws-sdk-core/lib/aws-sdk-core/plugins/protocols/rest_json.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/plugins/protocols/rest_json.rb
@@ -6,8 +6,11 @@ module Aws
       class RestJson < Seahorse::Client::Plugin
 
         class ContentTypeHandler < Seahorse::Client::Handler
+          CONTENT_TYPE = 'application/x-amz-json-%s'
+
           def call(context)
-            context.http_request.headers['Content-Type'] = 'application/json'
+            content_type = CONTENT_TYPE % [context.config.api.metadata['jsonVersion']]
+            context.http_request.headers['Content-Type'] = content_type
             @handler.call(context)
           end
         end
