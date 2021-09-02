221bb778609de6ae0a2a3f2964a20ba227533633
Alex Woods <alexwoo@amazon.com>
2021-09-02 09:09:49 -0700

    Fix multiline error message

--- gems/aws-sigv4/lib/aws-sigv4/signer.rb
+++ gems/aws-sigv4/lib/aws-sigv4/signer.rb
@@ -131,9 +131,9 @@ module Aws
         end
 
         if options[:signing_algorithm] == :sigv4a
-          raise ArgumentError, 'You are attempting to sign a
-request with sigv4a which requires aws-crt and version 1.4.0.crt or later of the aws-sigv4 gem.
-Please install the gem or add it to your gemfile.'
+          raise ArgumentError, 'You are attempting to sign a' \
+' request with sigv4a which requires aws-crt and version 1.4.0.crt or later of the aws-sigv4 gem.'\
+' Please install the gem or add it to your gemfile.'
         end
       end
 
