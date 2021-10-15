28b176510f06b3f0a1a3803ec4cc6e7bf749251e
Matt Muller <53055821+mullermp@users.noreply.github.com>
2021-10-15 14:07:48 -0700

    Content Type fixes for Rest-Json (again) (#2595)

--- gems/aws-sdk-core/lib/seahorse/client/plugins/content_length.rb
+++ gems/aws-sdk-core/lib/seahorse/client/plugins/content_length.rb
@@ -7,16 +7,22 @@ module Seahorse
 
         # @api private
         class Handler < Client::Handler
+          # https://github.com/ruby/net-http/blob/master/lib/net/http/requests.rb
+          # Methods without body are forwards compatible, because content-length
+          # may be set for requests without body but is technically incorrect.
+          METHODS_WITHOUT_BODY = Set.new(
+            %w[GET HEAD DELETE OPTIONS TRACE COPY MOVE]
+          )
 
           def call(context)
-            # If it's an IO object and not a File / String / String IO
-            if context.http_request.body.respond_to?(:size)
-              length = context.http_request.body.size
-              context.http_request.headers['Content-Length'] = length
+            body = context.http_request.body
+            method = context.http_request.http_method
+            # We use Net::HTTP with body_stream which doesn't do this by default
+            if body.respond_to?(:size) && !METHODS_WITHOUT_BODY.include?(method)
+              context.http_request.headers['Content-Length'] = body.size
             end
             @handler.call(context)
           end
-
         end
 
         handler(Handler, step: :sign, priority: 0)
--- gems/aws-sdk-nimblestudio/CHANGELOG.md
+++ gems/aws-sdk-nimblestudio/CHANGELOG.md
@@ -1,6 +1,8 @@
 Unreleased Changes
 ------------------
 
+* Issue - Remove a plugin that sets the `Content-Type` header and instead depend on a new version of `aws-sdk-core` that does.
+
 1.5.0 (2021-09-01)
 ------------------
 
