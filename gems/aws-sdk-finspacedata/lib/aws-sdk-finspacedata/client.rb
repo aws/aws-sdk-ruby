4a7b3418c6cd34a510c66ce978ab622128c11833
Matt Muller <mamuller@amazon.com>
2021-10-15 13:53:08 -0700

    Keep finspace plugins since service hasn't fixed yet

--- gems/aws-sdk-finspacedata/lib/aws-sdk-finspacedata/client.rb
+++ gems/aws-sdk-finspacedata/lib/aws-sdk-finspacedata/client.rb
@@ -29,6 +29,7 @@ require 'aws-sdk-core/plugins/transfer_encoding.rb'
 require 'aws-sdk-core/plugins/http_checksum.rb'
 require 'aws-sdk-core/plugins/signature_v4.rb'
 require 'aws-sdk-core/plugins/protocols/rest_json.rb'
+require 'aws-sdk-finspacedata/plugins/content_type.rb'
 
 Aws::Plugins::GlobalConfiguration.add_identifier(:finspacedata)
 
@@ -75,6 +76,7 @@ module Aws::FinSpaceData
     add_plugin(Aws::Plugins::HttpChecksum)
     add_plugin(Aws::Plugins::SignatureV4)
     add_plugin(Aws::Plugins::Protocols::RestJson)
+    add_plugin(Aws::FinSpaceData::Plugins::ContentType)
 
     # @overload initialize(options)
     #   @param [Hash] options
