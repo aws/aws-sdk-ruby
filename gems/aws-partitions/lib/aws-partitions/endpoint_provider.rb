e522d93864284dcb979000b140d471e69dda21c4
Alex Woods <alexwoo@amazon.com>
2021-10-28 14:06:18 -0700

    Revert "[PROPOSED REFACTOR] Large cleanup of logic - use a selector_fn to be generic to variants vs normal resolution."
    This reverts commit bec64a9b7a40313fea4e3244dc6a87162f5ae7da.

--- gems/aws-partitions/lib/aws-partitions/endpoint_provider.rb
+++ gems/aws-partitions/lib/aws-partitions/endpoint_provider.rb
@@ -44,7 +44,7 @@ module Aws
       # @option variants [Boolean] :fips When true, resolve a FIPS endpoint.
       # @api private Use the static class methods instead.
       def resolve(region, service, sts_endpoint, variants)
-        'https://' + endpoint_for(region, service, variants, build_is_global_fn(sts_endpoint))
+        'https://' + endpoint_for(region, service, sts_endpoint, variants)
       end
 
       # @api private Use the static class methods instead.
@@ -58,7 +58,7 @@ module Aws
         # don't default to the service name
         # signers should prefer the api metadata's signingName
         # if no service is set in the credentialScope
-        credential_scope(region, service, build_is_global_fn)
+        credential_scope(region, service, build_is_global_fn('regional'))
           .fetch('service', nil)
       end
 
@@ -72,7 +72,8 @@ module Aws
       # @api private Use the static class methods instead.
       def dns_suffix_for(region, service, variants)
         if configured_variants?(variants)
-          build_merged_config(region, service, build_selector_fn(variants), build_is_global_fn)['dnsSuffix']
+          resolved_variants = resolve_variants(region, service)
+          get_variant(resolved_variants, variants)['dnsSuffix']
         else
           get_partition(region)['dnsSuffix']
         end
@@ -80,77 +81,125 @@ module Aws
 
       private
 
-      def build_selector_fn(config_variants)
-        if configured_variants?(config_variants)
-          tags = Set.new(config_variants.filter { |_k,v| v == true }.map { |k,_v| k.to_s })
-
-          lambda do |e|
-            variants = e.fetch('variants', [])
-            variants.find { |v| tags == Set.new(v['tags']) } || {}
-          end
-        else
-          lambda do |e|
-            e
-          end
-        end
-      end
-
-      # return true if any of the variants are set explicitly to true
       def configured_variants?(variants)
-        variants.values.any? { |v| v == true}
+        variants[:fips] == true || variants[:dualstack] == true
       end
 
-      def endpoint_for(region, service, variants, is_global_fn)
-        selector_fn = build_selector_fn(variants)
+      def resolve_variants(region, service)
+        partition = get_partition(region)
+        partition_variants = partition.fetch('defaults', {})
+                                      .fetch('variants', [])
 
-        merged_cfg = build_merged_config(region, service, selector_fn, is_global_fn)
+        service_variants = partition.fetch('services', {})
+                                    .fetch(service, {})
+                                    .fetch('defaults', {})
+                                    .fetch('variants', [])
 
-        hostname = merged_cfg['hostname']
-        dns_suffix = merged_cfg['dnsSuffix']
+        service_cfg = partition.fetch('services', {})
+                               .fetch(service, {})
+        endpoints = service_cfg.fetch('endpoints', {})
 
-        unless hostname && dns_suffix
-          raise ArgumentError, 'Unable to resolve'
+        is_global = !endpoints.key?(region) &&
+                    service_cfg['isRegionalized'] == false
+
+        # Check for global endpoint.
+        region = service_cfg.fetch('partitionEndpoint', region) if is_global
+
+        endpoint_variants = endpoints.fetch(region, {})
+                                     .fetch('variants', [])
+
+        variants = []
+
+        partition_variants.each do |variant|
+          matching_svc_variant = service_variants.find do |svc_variant|
+            Set.new(variant['tags']) == Set.new(svc_variant['tags'])
+          end || {}
+
+          matching_ep_variant = endpoint_variants.find do |ep_variant|
+            Set.new(variant['tags']) == Set.new(ep_variant['tags'])
+          end || {}
+
+          variants << variant.merge(matching_svc_variant)
+                             .merge(matching_ep_variant)
         end
 
-        if merged_cfg['deprecated']
-          warn("Using the #{region} region with #{service} is deprecated.")
-        end
-
-        hostname.sub('{region}', region)
-                .sub('{service}', service)
-                .sub('{dnsSuffix}', dns_suffix)
+        variants
       end
 
-      # builds a merged config from the endpoint/region, service and partition values
-      # selector_fn - given a config, selects the appropriate values from it (ie, correct variant or normal)
-      # is_global_fn - returns true if this region/service should be treated as global (non-regionalized)
-      def build_merged_config(region, service, selector_fn, is_global_fn)
-        partition_cfg = get_partition(region)
-        service_cfg = partition_cfg.fetch('services', {})
-                                   .fetch(service, {})
+      def get_variant(modeled_variants, config_variants)
+        tags = Set.new
+        tags << 'dualstack' if config_variants[:dualstack] == true
+        tags << 'fips' if config_variants[:fips] == true
+        modeled_variants.each do |modeled_variant|
+          if tags == Set.new(modeled_variant['tags'])
+            return modeled_variant
+          end
+        end
+        {}
+      end
 
-        endpoints_cfg = service_cfg.fetch('endpoints', {})
+      def validate_variants!(config_variants, resolved_variants)
+        if config_variants[:dualstack] == true && resolved_variants.empty?
+          raise ArgumentError,
+                'Dualstack is not supported for this region and partition'
+        end
+        if config_variants[:fips] == true && resolved_variants.empty?
+          raise ArgumentError,
+                'FIPS is not supported for this region and partition'
+        end
+      end
 
-        if is_global_fn.call(service, region, endpoints_cfg, service_cfg)
+      def endpoint_for(region, service, sts_endpoints, variants)
+        if configured_variants?(variants)
+          resolved_variants = resolve_variants(region, service)
+          validate_variants!(variants, resolved_variants)
+          variant = get_variant(resolved_variants, variants)
+          variant['hostname'].sub('{region}', region)
+                             .sub('{service}', service)
+                             .sub('{dnsSuffix}', variant['dnsSuffix'])
+        else
+          _endpoint_for(region, service, sts_endpoints)
+        end
+      end
+
+      def _endpoint_for(region, service, sts_regional_endpoints)
+        partition = get_partition(region)
+        service_cfg = partition.fetch('services', {}).fetch(service, {})
+
+        # Find the default endpoint
+        default_endpoint = service_cfg
+                   .fetch('defaults', {})
+                   .fetch('hostname', partition['defaults']['hostname'])
+
+        endpoints = service_cfg.fetch('endpoints', {})
+
+        # Check for sts legacy behavior
+        sts_legacy = service == 'sts' &&
+                     sts_regional_endpoints == 'legacy' &&
+                     STS_LEGACY_REGIONS.include?(region)
+
+        is_global = !endpoints.key?(region) &&
+                    service_cfg['isRegionalized'] == false
+
+        # Check for global endpoint.
+        if sts_legacy || is_global
           region = service_cfg.fetch('partitionEndpoint', region)
         end
 
-        partition_defaults = selector_fn.call(partition_cfg.fetch('defaults', {}))
-        service_defaults = selector_fn.call(service_cfg.fetch('defaults', {}))
-        endpoint_cfg = selector_fn.call(endpoints_cfg.fetch(region, {}))
+        # Check for service/region level endpoint.
+        endpoint = endpoints
+                   .fetch(region, {})
+                   .fetch('hostname', default_endpoint)
 
-        # dnsSuffix is a special case and can exist on the top level for non-variants
-        if (partition_dns_suffix = selector_fn.call(partition_cfg)['dnsSuffix'])
-          partition_defaults['dnsSuffix'] = partition_dns_suffix
-        end
-
-        # merge upwards, preferring values from endpoint > service > partition
-        partition_defaults.merge(service_defaults.merge(endpoint_cfg))
+        # Replace placeholders from the endpoints
+        endpoint.sub('{region}', region)
+                .sub('{service}', service)
+                .sub('{dnsSuffix}', partition['dnsSuffix'])
       end
 
       # returns a callable that takes a region
-      # and returns true if the service is global (ie, not regionalized)
-      def build_is_global_fn(sts_regional_endpoints='regional')
+      # and returns true if the service is global
+      def build_is_global_fn(sts_regional_endpoints)
         lambda do |service, region, endpoints, service_cfg|
           # Check for sts legacy behavior
           sts_legacy = service == 'sts' &&
@@ -170,7 +219,7 @@ module Aws
                                .fetch(service, {})
         endpoints = service_cfg.fetch('endpoints', {})
 
-        # Check for global endpoint
+        # Check for global endpoint.
         if is_global_fn.call(service, region, endpoints, service_cfg)
           region = service_cfg.fetch('partitionEndpoint', region)
         end
