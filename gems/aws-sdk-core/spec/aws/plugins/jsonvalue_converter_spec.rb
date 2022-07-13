981fdf88f44f68c387896d638e4b4b9d1cb95d7e
Alex Woods <alexwoo@amazon.com>
2022-07-13 12:04:36 -0700

    Spec formatting cleanups

--- gems/aws-sdk-core/spec/aws/plugins/jsonvalue_converter_spec.rb
+++ gems/aws-sdk-core/spec/aws/plugins/jsonvalue_converter_spec.rb
@@ -5,7 +5,6 @@ require_relative '../../spec_helper'
 module Aws
   module Plugins
     describe JsonvalueConverter do
-
       JsonValueClient = ApiHelper.sample_service(api: JSON.parse(<<~JSON
         {
           "version":"2.0",
@@ -88,27 +87,27 @@ module Aws
               }
             }
         }
-JSON
+      JSON
       )).const_get(:Client)
 
       let(:client) { JsonValueClient.new(stub_responses: true) }
-      let(:hash) { {'bar' => 'baz'} }
+      let(:hash) { { 'bar' => 'baz' } }
       let(:hash_no_json) do
-        hash = {'bar' => 'baz'}
-        hash.instance_eval('undef :to_json')
+        hash = { 'bar' => 'baz' }
+        hash.instance_eval('undef :to_json', __FILE__, __LINE__)
         hash
       end
 
       it 'converts ruby object to json string before validation' do
         ruby_types = {
-          hash: {'bar' => 'baz'},
-          array: ['bar', 'baz'],
-          num: 12345,
+          hash: { 'bar' => 'baz' },
+          array: %w[bar baz],
+          num: 12_345,
           str: 'foobarbaz',
           bool: true,
           time: Time.now
         }
-        ruby_types.each do |_k,v|
+        ruby_types.each do |_k, v|
           resp = client.foo(header_field: v)
           expect(resp.context.params[:header_field]).to eq(v.to_json)
         end
@@ -120,8 +119,8 @@ JSON
       end
 
       it 'serializes jsonvalue members set to nil' do
-        resp = client.foo(body: {body_field: nil})
-        expect(resp.context.params[:body]).to eq({body_field: "null"})
+        resp = client.foo(body: { body_field: nil })
+        expect(resp.context.params[:body]).to eq({ body_field: 'null' })
       end
 
       it 'serializes jsonvalue members of nested structures' do
@@ -130,7 +129,8 @@ JSON
             body_structure_field: {
               body_field: hash
             }
-          })
+          }
+        )
         expect(resp.context.params[:body][:body_structure_field][:body_field])
           .to eq(hash.to_json)
       end
@@ -148,32 +148,36 @@ JSON
       it 'serializes jsonvalue values of maps' do
         resp = client.foo(
           body: {
-            body_map_field: {key1: hash, key2: hash}
+            body_map_field: { key1: hash, key2: hash }
           }
         )
         expect(resp.context.params[:body][:body_map_field])
-          .to eq({"key1" => hash.to_json, "key2" => hash.to_json})
+          .to eq({ 'key1' => hash.to_json, 'key2' => hash.to_json })
       end
 
       it 'raises exception when an object is not JSON serializable' do
         expect do
           client.foo(header_field: hash_no_json)
         end.to raise_error(
-               ArgumentError,
-               "The value of params[header_field] is not JSON serializable.")
+          ArgumentError,
+          'The value of params[header_field] is not JSON serializable.'
+        )
       end
 
-      it 'raises exceptions when a nested structure member is not JSON serializable' do
+      it 'raises exceptions when structure member is not JSON serializable' do
         expect do
           client.foo(
             body: {
               body_structure_field: {
                 body_field: hash_no_json
               }
-            })
+            }
+          )
         end.to raise_error(
-                 ArgumentError,
-                 "The value of params[body][body_structure_field][body_field] is not JSON serializable.")
+          ArgumentError,
+          'The value of params[body][body_structure_field][body_field] '\
+          'is not JSON serializable.'
+        )
       end
 
       it 'raises exceptions when a list member is not JSON serializable' do
@@ -181,21 +185,27 @@ JSON
           client.foo(
             body: {
               body_list_field: [hash, hash_no_json]
-            })
+            }
+          )
         end.to raise_error(
-                 ArgumentError,
-                 "The value of params[body][body_list_field][1] is not JSON serializable.")
+          ArgumentError,
+          'The value of params[body][body_list_field][1] '\
+          'is not JSON serializable.'
+        )
       end
 
       it 'raises exceptions when a map value is not JSON serializable' do
         expect do
           client.foo(
             body: {
-              body_map_field: {key1: hash, key2: hash_no_json}
-            })
+              body_map_field: { key1: hash, key2: hash_no_json }
+            }
+          )
         end.to raise_error(
-                 ArgumentError,
-                 "The value of params[body][body_map_field][key2] is not JSON serializable.")
+          ArgumentError,
+          'The value of params[body][body_map_field][key2] '\
+          'is not JSON serializable.'
+        )
       end
     end
   end
