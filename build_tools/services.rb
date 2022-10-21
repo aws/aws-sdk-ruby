4ad2601652ba826909b5dd6f756db5b9db630376
Matt Muller <mamuller@amazon.com>
2022-10-21 16:14:33 -0400

    Bump versions

--- build_tools/services.rb
+++ build_tools/services.rb
@@ -10,7 +10,7 @@ module BuildTools
     MANIFEST_PATH = File.expand_path('../../services.json', __FILE__)
 
     # Minimum `aws-sdk-core` version for new gem builds
-    MINIMUM_CORE_VERSION = "3.163.0"
+    MINIMUM_CORE_VERSION = "3.165.0"
 
     EVENTSTREAM_PLUGIN = "Aws::Plugins::EventStreamConfiguration"
 
