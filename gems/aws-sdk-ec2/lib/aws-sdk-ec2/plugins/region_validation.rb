ea7447cff9656437d8492c2b0a8e2a10c8a90c8e
Vi <xxz3@users.noreply.github.com>
2022-11-21 09:53:00 +0100

    Add missing verb to error message

--- gems/aws-sdk-ec2/lib/aws-sdk-ec2/plugins/region_validation.rb
+++ gems/aws-sdk-ec2/lib/aws-sdk-ec2/plugins/region_validation.rb
@@ -9,7 +9,7 @@ module Aws
         def after_initialize(client)
           if region = client.config.region
             if matches = region.match(/^(\w+-\w+-\d+)[a-z]$/)
-              msg = ":region option must a region name, not an availability "\
+              msg = ":region option must be a region name, not an availability "\
                     "zone name; try `#{matches[1]}' instead of `#{matches[0]}'"
               raise ArgumentError, msg
             end
