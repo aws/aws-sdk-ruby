f6fdac54780a4ccf746c0f30c2a0ef27138f1233
mdomsch-seczetta <mdomsch@seczetta.com>
2023-02-06 16:54:16 -0500

    ECS Client list_container_instances() result documentation fix
    The ECS container instance ARNs returned by list_container_instances()
    include the cluster name in tail end of the ARN, after the string
    "container-instance" and before the container instance ID
    itself. Since you need just the container instance ID in calling
    methods like update_container_instance_state(), knowing the proper
    format for value returned here is important.

--- gems/aws-sdk-ecs/lib/aws-sdk-ecs/client.rb
+++ gems/aws-sdk-ecs/lib/aws-sdk-ecs/client.rb
@@ -4522,8 +4522,8 @@ module Aws::ECS
     #   resp.to_h outputs the following:
     #   {
     #     container_instance_arns: [
-    #       "arn:aws:ecs:us-east-1:<aws_account_id>:container-instance/f6bbb147-5370-4ace-8c73-c7181ded911f", 
-    #       "arn:aws:ecs:us-east-1:<aws_account_id>:container-instance/ffe3d344-77e2-476c-a4d0-bf560ad50acb", 
+    #       "arn:aws:ecs:us-east-1:<aws_account_id>:container-instance/default/f6bbb147-5370-4ace-8c73-c7181ded911f",
+    #       "arn:aws:ecs:us-east-1:<aws_account_id>:container-instance/default/ffe3d344-77e2-476c-a4d0-bf560ad50acb",
     #     ], 
     #   }
     #
