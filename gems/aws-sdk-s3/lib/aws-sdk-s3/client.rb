f174ab67c4ab3e55c9c8cd22425ce441edeaf641
Matt Muller <mamuller@amazon.com>
2023-04-04 14:44:15 -0400

    Unbump s3

--- gems/aws-sdk-s3/lib/aws-sdk-s3/client.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/client.rb
@@ -15436,7 +15436,7 @@ module Aws::S3
         params: params,
         config: config)
       context[:gem_name] = 'aws-sdk-s3'
-      context[:gem_version] = '1.120.1'
+      context[:gem_version] = '1.120.0'
       Seahorse::Client::Request.new(handlers, context)
     end
 
