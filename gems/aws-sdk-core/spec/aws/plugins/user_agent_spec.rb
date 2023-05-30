306617e3e646ba7a34ef9d7e51835b854ef64cac
Matt Muller <mamuller@amazon.com>
2023-05-30 15:26:21 -0400

    Use Aws::Json in user agent spec

--- gems/aws-sdk-core/spec/aws/plugins/user_agent_spec.rb
+++ gems/aws-sdk-core/spec/aws/plugins/user_agent_spec.rb
@@ -86,7 +86,7 @@ module Aws
       end
 
       context 'test runner' do
-        tests = JSON.load_file(
+        tests = Json.load_file(
           File.join(File.dirname(__FILE__), 'user_agent_tests.json')
         )
 
