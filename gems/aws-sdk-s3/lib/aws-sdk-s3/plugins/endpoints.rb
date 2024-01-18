7e99658870a3056a1677d835286ed468b28fca00
ksss <co000ri@gmail.com>
2024-01-18 10:08:54 +0900

    Set RBS type for undefined class

--- gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/endpoints.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/endpoints.rb
@@ -14,6 +14,7 @@ module Aws::S3
       option(
         :endpoint_provider,
         doc_type: 'Aws::S3::EndpointProvider',
+        rbs_type: 'untyped',
         docstring: 'The endpoint provider used to resolve endpoints. Any '\
                    'object that responds to `#resolve_endpoint(parameters)` '\
                    'where `parameters` is a Struct similar to '\
