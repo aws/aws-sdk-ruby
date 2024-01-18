8b2a7363caa6c8fc5445e215b6fa7ab4c5d5d805
Matt Muller <mamuller@amazon.com>
2024-01-18 17:25:01 -0500

    Undo more codegen and prepare version bump

--- gems/aws-sdk-core/lib/aws-sdk-ssooidc/plugins/endpoints.rb
+++ gems/aws-sdk-core/lib/aws-sdk-ssooidc/plugins/endpoints.rb
@@ -14,7 +14,6 @@ module Aws::SSOOIDC
       option(
         :endpoint_provider,
         doc_type: 'Aws::SSOOIDC::EndpointProvider',
-        rbs_type: 'untyped',
         docstring: 'The endpoint provider used to resolve endpoints. Any '\
                    'object that responds to `#resolve_endpoint(parameters)` '\
                    'where `parameters` is a Struct similar to '\
