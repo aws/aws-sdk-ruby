b1b3af66af60a12f14a68be20d1656a8137bdc2a
Juli Tera <terajul@amazon.com>
2024-02-29 15:57:22 -0800

    Add Content Type for rest-xml protocol

--- gems/aws-sdk-core/lib/aws-sdk-core/plugins/protocols/rest_xml.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/plugins/protocols/rest_xml.rb
@@ -5,9 +5,21 @@ module Aws
     module Protocols
       class RestXml < Seahorse::Client::Plugin
 
-        handler(Rest::Handler)
-        handler(Xml::ErrorHandler, step: :sign)
+        class ContentTypeHandler < Seahorse::Client::Handler
+          def call(context)
+            body = context.http_request.body
+            if !body.respond_to?(:size) ||
+               (body.respond_to?(:size) && body.size > 0)
+              context.http_request.headers['Content-Type'] ||=
+                'application/xml'
+            end
+            @handler.call(context)
+          end
+        end
 
+        handler(Rest::Handler)
+        handler(ContentTypeHandler, priority: 30)
+        handler(Xml::ErrorHandler, step: :sign)
       end
     end
   end
