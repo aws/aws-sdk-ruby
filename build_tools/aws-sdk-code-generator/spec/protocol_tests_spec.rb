f401aa8fbed5b7ea5d083078bcaf368e1f081dc8
Juli Tera <terajul@amazon.com>
2024-03-04 09:17:21 -0800

    Update ignore mechanism and its list

--- build_tools/aws-sdk-code-generator/spec/protocol_tests_spec.rb
+++ build_tools/aws-sdk-code-generator/spec/protocol_tests_spec.rb
@@ -12,7 +12,9 @@ ProtocolTestsHelper.fixtures.each do |protocol, files|
     describe 'input' do
       ProtocolTestsHelper.each_test_case(self, files['input']) do |group, suite, test_case, id, description|
         group.it(description) do
-          skip('Test Case is in the Ignore List') if ignore_list['input'].include? test_case['id']
+          # check if test case id is in the ignore list
+          ignore_result = ignore_list['input'].find { |i| i.values.flatten.include? test_case['id'] }
+          skip(ignore_result.keys.first) unless ignore_result.nil?
 
           if id.include?('IdempotencyToken')
             allow(SecureRandom).to receive(:uuid).and_return('00000000-0000-4000-8000-000000000000')
@@ -42,7 +44,9 @@ ProtocolTestsHelper.fixtures.each do |protocol, files|
     describe 'output' do
       ProtocolTestsHelper.each_test_case(self, files['output']) do |group, suite, test_case, id, description|
         group.it(description) do
-          skip('Test Case is in the Ignore List') if ignore_list['output'].include? test_case['id']
+          # check if test case id is in the ignore list
+          ignore_result = ignore_list['output'].find { |i| i.values.flatten.include? test_case['id'] }
+          skip(ignore_result.keys.first) unless ignore_result.nil?
 
           client = ProtocolTestsHelper.client_for(suite, test_case, "Output_#{id}")
           client.handle(step: :send) do |context|
