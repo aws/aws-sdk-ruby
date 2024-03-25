150fa36378927178b83d9018fc8a8190826e09ae
Juli Tera <terajul@amazon.com>
2024-03-25 15:38:19 -0700

    Add ContentType handler for rest protocol

--- gems/aws-sdk-core/lib/aws-sdk-core/plugins/protocols/rest_xml.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/plugins/protocols/rest_xml.rb
@@ -4,21 +4,8 @@ module Aws
   module Plugins
     module Protocols
       class RestXml < Seahorse::Client::Plugin
-
-        class ContentTypeHandler < Seahorse::Client::Handler
-          def call(context)
-            body = context.http_request.body
-            if !body.respond_to?(:size) ||
-               (body.respond_to?(:size) && body.size > 0)
-              context.http_request.headers['Content-Type'] ||=
-                'application/xml'
-            end
-            @handler.call(context)
-          end
-        end
-
         handler(Rest::Handler)
-        handler(ContentTypeHandler, priority: 30)
+        handler(Rest::ContentTypeHandler)
         handler(Xml::ErrorHandler, step: :sign)
       end
     end
---
+++ gems/aws-sdk-core/lib/aws-sdk-core/rest/request/content_type.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Aws
+  module Rest
+    class ContentTypeHandler < Seahorse::Client::Handler
+      def call(context)
+        body = context.http_request.body
+        if !body.respond_to?(:size) ||
+           (body.respond_to?(:size) && body.size > 0)
+          case (protocol = context.config.api.metadata['protocol'])
+          when 'rest-json'
+            context.http_request.headers['Content-Type'] ||=
+              'application/json'
+          when 'rest-xml'
+            context.http_request.headers['Content-Type'] ||=
+              'application/xml'
+          else raise "Unsupported protocol #{protocol}"
+          end
+        end
+        @handler.call(context)
+      end
+    end
+  end
+end
