a928e671e9d41c48163237f870b9a0db634f5e2e
Alex Woods <alexwoo@amazon.com>
2024-04-24 11:19:59 -0700

    Remove wait for ready and assume ready to start in event emitter

--- gems/aws-sdk-core/lib/aws-sdk-core/event_emitter.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/event_emitter.rb
@@ -6,7 +6,7 @@ module Aws
     def initialize
       @listeners = {}
       @validate_event = true
-      @status = :sleep
+      @status = :ready
       @signal_queue = Queue.new
     end
 
@@ -40,28 +40,10 @@ module Aws
         Aws::ParamValidator.validate!(
           @encoder.rules.shape.member(type), params)
       end
-      _ready_for_events?
       @stream.data(
         @encoder.encode(type, params),
         end_stream: type == :end_stream
       )
     end
-
-    private
-
-    def _ready_for_events?
-      return true if @status == :ready
-
-      # TODO: THIS IS A HACK
-      @signal_queue << :ready
-
-      # blocked until once initial 200 response is received
-      # signal will be available in @signal_queue
-      # and this check will no longer be blocked
-      @signal_queue.pop
-      @status = :ready
-      true
-    end
-
   end
 end
