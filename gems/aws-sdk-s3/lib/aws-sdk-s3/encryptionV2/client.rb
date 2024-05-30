5577191c0bb1dab84732891b2e2b2f564c496f43
Matt Muller <mamuller@amazon.com>
2024-05-29 22:57:51 -0400

    Fix encryption client test errors

--- gems/aws-sdk-s3/lib/aws-sdk-s3/encryptionV2/client.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/encryptionV2/client.rb
@@ -361,7 +361,7 @@ module Aws
             instruction_file_suffix: @instruction_file_suffix,
             kms_encryption_context: kms_encryption_context
           }
-          Aws::Plugins::UserAgent..metric('S3_CRYPTO_V2') do
+          Aws::Plugins::UserAgent.metric('S3_CRYPTO_V2') do
             req.send_request
           end
         end
@@ -416,7 +416,7 @@ module Aws
             kms_allow_decrypt_with_any_cmk: kms_any_cmk_mode,
             security_profile: security_profile
           }
-          Aws::Plugins::UserAgent..metric('S3_CRYPTO_V2') do
+          Aws::Plugins::UserAgent.metric('S3_CRYPTO_V2') do
             req.send_request(target: block)
           end
         end
