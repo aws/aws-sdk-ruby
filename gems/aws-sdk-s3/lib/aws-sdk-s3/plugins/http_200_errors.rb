8ed1bb90d0991f214d4a157bd6aaeceb52937ebd
Matt Muller <53055821+mullermp@users.noreply.github.com>
2024-06-07 16:56:20 -0400

    Handle S3 200 errors for all operations #2 (#3040)

--- gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/http_200_errors.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/http_200_errors.rb
@@ -15,21 +15,40 @@ module Aws
 
           def call(context)
             @handler.call(context).on(200) do |response|
-              if error = check_for_error(context)
-                context.http_response.status_code = 500
-                response.data = nil
-                response.error = error
-              end
+              return response if streaming_output?(context.operation.output)
+
+              error = check_for_error(context)
+              return response unless error
+
+              context.http_response.status_code = 500
+              response.data = nil
+              response.error = error
             end
+          end
+
+          private
+
+          def streaming_output?(output)
+            if (payload = output[:payload_member]) # checking ref and shape
+              payload['streaming'] || payload.shape['streaming'] ||
+                payload.eventstream
+            else
+              false
+            end
+          end
+
+          def members_in_body?(output)
+            output.shape.members.any? { |_, k| k.location.nil? }
           end
 
           def check_for_error(context)
             xml = context.http_response.body_contents
-            if xml.match(/<Error>/)
+            if xml.match(/\?>\s*<Error>/)
               error_code = xml.match(/<Code>(.+?)<\/Code>/)[1]
               error_message = xml.match(/<Message>(.+?)<\/Message>/)[1]
               S3::Errors.error_class(error_code).new(context, error_message)
-            elsif !xml.match(/<\w/) # Must have the start of an XML Tag
+            elsif members_in_body?(context.operation.output) && !xml.match(/<\w/)
+              # Must have a body member and have the start of an XML Tag
               # Other incomplete xml bodies will result in XML ParsingError
               Seahorse::Client::NetworkingError.new(
                 S3::Errors
@@ -40,15 +59,7 @@ module Aws
           end
         end
 
-        handler(
-          Handler,
-          step: :sign,
-          operations: [
-            :complete_multipart_upload,
-            :copy_object,
-            :upload_part_copy,
-          ]
-        )
+        handler(Handler, step: :sign)
       end
     end
   end
