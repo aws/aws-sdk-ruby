d3300b38e20ffa65ec9f230df7eaed559fadafe4
Matt Muller <mamuller@amazon.com>
2024-07-26 19:26:49 -0400

    Dirty fix to add different gem path

--- tasks/gems/core.rb
+++ tasks/gems/core.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Modify load path to include all of our gems from local repo
+Dir.glob("gems/*/lib") do |gem_path|
+  $LOAD_PATH.unshift(File.expand_path(gem_path))
+end
+
 module Benchmark
   module Gems
     class Core < Benchmark::Gem
