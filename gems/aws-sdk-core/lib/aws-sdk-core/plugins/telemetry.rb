e90ec7dce1250bfb626f1b03c429a426c56720fd
Juli Tera <terajul@amazon.com>
2024-08-12 12:23:04 -0700

    Add initial parent span

--- gems/aws-sdk-core/lib/aws-sdk-core/plugins/telemetry.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/plugins/telemetry.rb
@@ -4,30 +4,52 @@ module Aws
   module Plugins
     # TODO
     class Telemetry < Seahorse::Client::Plugin
-      # add config options
       option(
         :telemetry_provider,
         default: Aws::Telemetry::NoOpTelemetryProvider,
         doc_type: Aws::Telemetry::TelemetryProviderBase,
         rbs_type: 'untyped',
         docstring: <<~DOCS) do |cfg|
-          Allows you to provide a telemetry provider class. By default,
+          Allows you to provide a telemetry provider. By default,
           will use the NoOpTelemetryProvider.
         DOCS
         resolve_provider(cfg)
       end
 
-      # do we set default provider if none provided here
       # do we need to validate if there is something
       def self.resolve_provider(_cfg)
-        # resolve based on whether there's otel
-        # create an instance of NoOpTelemetryProvider
-        Telemetry::NoOpTelemetryProvider.new
+        Aws::Telemetry::NoOpTelemetryProvider.new
       end
 
-      # do we need handlers?
+      # this is the root parent handler
+      # sets up initial attributes to emit
       class Handler < Seahorse::Client::Handler
+        def call(context)
+          # serviceId may not be present in older versions
+          # do I need to retrieve legacy serviceId?
+          attributes = {
+            'rpc.service' => context.config.api.metadata['serviceId'],
+            'rpc.method' => context.operation.name,
+            'code.function' => context.operation_name.to_s,
+            'code.namespace' => 'AWS::Plugins::Telemetry'
+          }
+          context.tracer.in_span(
+            parent_span_name(context),
+            attributes: attributes,
+            kind: Aws::Telemetry::SpanKind::CLIENT
+          ) do
+            @handler.call(context)
+          end
+        end
+
+        private
+
+        def parent_span_name(context)
+          "#{context.config.api.metadata['serviceId']}.#{context.operation.name}".strip
+        end
       end
+
+      handler(Handler, step: :initialize, priority: 99)
     end
   end
 end
