df22114ac0613abf3e842a0942e7a5fb312d7aae
AWS SDK for Ruby <github-aws-sdk-ruby-automation@amazon.com>
2024-09-16 12:01:24 -0700

    Remove uneeded requires in cbor specs

--- gems/aws-sdk-core/spec/aws/cbor/cbor_engine_spec.rb
+++ gems/aws-sdk-core/spec/aws/cbor/cbor_engine_spec.rb
@@ -1,5 +1,4 @@
 require_relative '../../spec_helper'
-require 'aws-sdk-core/cbor/cbor_engine'
 
 module Aws
   module Cbor
--- gems/aws-sdk-core/spec/aws/cbor/decoder_spec.rb
+++ gems/aws-sdk-core/spec/aws/cbor/decoder_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative '../../spec_helper'
-require 'aws-sdk-core/cbor/decoder'
 
 module Aws
   module Cbor
