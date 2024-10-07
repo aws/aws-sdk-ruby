167dc31c1ebda4c2a6f5d96bbf68bad03a481041
Alex Woods <alexwoo@amazon.com>
2024-10-07 13:45:36 -0700

    Fix issue in new aws_partition method

--- gems/aws-partitions/lib/aws-partitions/metadata.rb
+++ gems/aws-partitions/lib/aws-partitions/metadata.rb
@@ -7,7 +7,7 @@ module Aws
       class << self
         def aws_partition(value)
           partition =
-            partitions.find { |p| p['regions']&.fetch(value) } ||
+            partitions.find { |p| p['regions']&.fetch(value, nil) } ||
               partitions.find { |p| value.match(p['regionRegex']) } ||
               partitions.find { |p| p['id'] == 'aws' }
 
