d213f1654528eefa10716b0daac1a5d8e550916f
Hannah Ramadan <hannahr315@gmail.com>
2024-10-29 16:09:16 -0700

    Docs: do not escape curly braces in example

--- gems/aws-sdk-kinesis/lib/aws-sdk-kinesis/client.rb
+++ gems/aws-sdk-kinesis/lib/aws-sdk-kinesis/client.rb
@@ -663,7 +663,7 @@ module Aws::Kinesis
     # Delete a policy for the specified data stream or consumer. Request
     # patterns can be one of the following:
     #
-    # * Data stream pattern: `arn:aws.*:kinesis:.*:\d\{12\}:.*stream/\S+`
+    # * Data stream pattern: `arn:aws.*:kinesis:.*:\d{12}:.*stream/\S+`
     #
     # * Consumer pattern:
     #   `^(arn):aws.*:kinesis:.*:\d\{12\}:.*stream\/[a-zA-Z0-9_.-]+\/consumer\/[a-zA-Z0-9_.-]+:[0-9]+`
