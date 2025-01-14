588925833af677d0dba5c5504dd30d41bab4745c
Richard Wang <wangrch@amazon.com>
2025-01-14 11:56:56 -0800

    Add skeleton for tests

--- perf-testing/test.rb
+++ perf-testing/test.rb
@@ -16,3 +16,66 @@ cbor_cloudwatch.describe_alarms
 Aws::SecretsManager::Client.remove_plugin(Aws::Plugins::Protocols::JsonRpc)
 json_ssm = Aws::SecretsManager::Client.new(plugins: [Aws::Plugins::Protocols::JsonRpc])
 cbor_ssm = Aws::SecretsManager::Client.new(plugins: [Aws::Plugins::Protocols::RpcV2])
+
+
+# TODO: add warm up iterations
+# TODO: add tests for each protocol type
+# TODO: add tests for each Ruby runtime version
+
+
+# Local Test
+(1...500).each do |i|
+  # echo (All types input)
+  # echo (Long list of strings input)
+  # echo (Complex object input)
+  # echo (List of complex object input)
+  # echo (Very large block input)
+end
+
+
+# SecretsManager Test
+(1...500).each do |i|
+  # create_secret
+  # create_secret(binary)
+end
+
+sizes = [64, 512, 4096, 8192, 45056]
+sizes.each do |size|
+  (1...500).each do |i|
+    # put_secret_value(input)
+    # put_secret_value_binary(input)
+  end
+  (1...500).each do |i|
+    # get_secret_value(input)
+    # get_secret_value_binary(input)
+  end
+end
+
+(1...500).each do |i|
+  # describe_secret(input)
+  # list_secrets(input)
+end
+
+(1...500).each do |i|
+  # delete_secret(input)
+  # delete_secret_binary(input)
+end
+
+
+# CloudWatch Test
+metric_counts = [16, 64, 256, 1000]
+metric_counts.each do |count|
+  (1...500).each do |i|
+    # put_metric_data(input)
+    sleep(2) if (i % 50).zero?
+  end
+  (1...500).each do |i|
+    # get_metric_data(input)
+    sleep(2) if (i % 50).zero?
+  end
+end
+
+(1...500).each do |i|
+  # list_metrics(input)
+  sleep(2) if (i % 50).zero?
+end
