d50fa922ab7985993e5f31810573fd970aea51cb
Richard Wang <wangrch@amazon.com>
2025-01-22 14:53:06 -0800

    Finish cloudwatch test with corrections and size

--- perf-testing/test-cloudwatch.rb
+++ perf-testing/test-cloudwatch.rb
@@ -4,14 +4,15 @@ $LOAD_PATH.unshift(File.expand_path('../gems/aws-sdk-cloudwatch/lib', __dir__))
 require 'aws-sdk-core'
 require 'aws-sdk-cloudwatch'
 require 'aws-sdk-core/plugins/protocols/rpc_v2'
+require 'securerandom'
 require_relative 'Stats'
 include Stats
 
 ITERATIONS = ARGV.first.to_i
 WARMUP = 10
 METRIC_COUNTS = [16, 64, 256, 1000]
-BASE_TIME = Time.now.to_i - 2 * 60 * 60
-SUITE_ID = (0...8).map { (48 + rand(10)).chr }.join
+BASE_TIME = Time.now - 2 * 60 * 60
+SUITE_ID = SecureRandom.uuid
 
 def generate_put_metric_data_request(metrics, base_time, suite_id)
   request = {}
@@ -23,8 +24,8 @@ def generate_put_metric_data_request(metrics, base_time, suite_id)
       metric_name: 'TestMetric',
       dimensions: [
         {
-          name: 'TestNamespace',
-          value: (suite_id.to_i - metrics).to_s
+          name: 'TestDimension',
+          value: "#{suite_id}-#{metrics}"
         }
       ],
       value: rand(0.0...1.0),
@@ -53,7 +54,7 @@ def generate_get_metric_data_request(metrics, base_time, suite_id)
         dimensions: [
           {
             name: 'TestDimension',
-            value: (suite_id.to_i - metrics).to_s
+            value: "#{suite_id}-#{metrics}"
           }
         ]
       },
@@ -64,39 +65,29 @@ def generate_get_metric_data_request(metrics, base_time, suite_id)
   request
 end
 
-def generate_list_metrics_request(iteration)
+def generate_list_metrics_request
   {
-    namespace: "TestNamespace#{iteration}"
+    namespace: "TestNamespace"
   }
 end
-
-thread = Thread.current
-thread[:query_serde_data] = []
-thread[:cbor_serde_data] = []
-thread[:query_total_data] = []
-thread[:cbor_total_data] = []
-thread[:warm] = false
-
-Aws::CloudWatch::Client.api.metadata['targetPrefix'] = 'GraniteServiceVersion20100801'
-Aws::CloudWatch::Client.remove_plugin(Aws::Plugins::Protocols::Query)
-query_cloudwatch = Aws::CloudWatch::Client.new(plugins: [Aws::Plugins::Protocols::Query], region: 'us-west-2')
-cbor_cloudwatch = Aws::CloudWatch::Client.new(plugins: [Aws::Plugins::Protocols::RpcV2], region: 'us-west-2')
-cbor_cloudwatch.config.api = cbor_cloudwatch.config.api.dup
-cbor_cloudwatch.config.api.metadata = cbor_cloudwatch.config.api.metadata.dup
-cbor_cloudwatch.config.api.metadata['protocol'] = 'smithy-rpc-v2-cbor'
-
-# metric_counts = [16, 64, 256, 1000]
-metric_counts = [1, 16]
-thread[:warm] = true
-
-data = File.open('perf-testing/test-output/cloudwatch/data.txt', 'w')
-raw = File.open('perf-testing/test-output/cloudwatch/raw.txt', 'w')
 
 def separate_serde_data(data)
   separated = Array.new(2) { [] }
   data.each do |i|
     separated[0] << i[0]
     separated[1] << i[1]
+  end
+  separated
+end
+
+def separate_byte_data(data)
+  separated = Array.new(2) { [] }
+  data.each_with_index do |i, idx|
+    if idx.even?
+      separated[0] << i
+    else
+      separated[1] << i
+    end
   end
   separated
 end
@@ -127,6 +118,10 @@ def output_raw(thread, outfile)
   pp thread[:query_total_data]
   puts 'Cbor Total Data Raw'
   pp thread[:cbor_total_data]
+  puts 'Request Size Data Raw (Alternating Query and CBOR)'
+  pp thread[:request_size_data]
+  puts 'Response Size Data Raw (Alternating Query and CBOR)'
+  pp thread[:response_size_data]
   $stdout = STDOUT
 end
 
@@ -135,80 +130,122 @@ def clear_thread_data(thread)
   thread[:cbor_serde_data] = []
   thread[:query_total_data] = []
   thread[:cbor_total_data] = []
+  thread[:request_size_data] = []
+  thread[:response_size_data] = []
 end
 
-def analyze(test_case, metrics, thread)
+def analyze(test_case, metrics, thread, data, raw)
   raw.puts(test_case)
   output_raw(thread, raw)
 
-  separated_query = separate_serde_data(thread[:query_serde_data])
-  write_test_output(test_case, 'Query', metrics, 'Serialization time (ms)', separated_query[0], data)
-  write_test_output(test_case, 'Query', metrics, 'Deserialization time (ms)', separated_query[1], data)
-  write_test_output(test_case, 'Query', metrics, 'Total request time (ms)', thread[:query_total_data], data)
+  separated_request_size = separate_byte_data(thread[:request_size_data])
+  # pp separated_request_size
+  separated_response_size = separate_byte_data(thread[:response_size_data])
+  # pp separated_response_size
 
-  separated_cbor = separate_serde_data(thread[:cbor_serde_data])
-  write_test_output(test_case, 'CBOR', metrics, 'Serialization time (ms)', separated_cbor[0], data)
-  write_test_output(test_case, 'CBOR', metrics, 'Deserialization time (ms)', separated_cbor[1], data)
+  separated_query_serde = separate_serde_data(thread[:query_serde_data])
+  write_test_output(test_case, 'Query', metrics, 'Serialization time (ms)', separated_query_serde[0], data)
+  write_test_output(test_case, 'Query', metrics, 'Deserialization time (ms)', separated_query_serde[1], data)
+  write_test_output(test_case, 'Query', metrics, 'Total request time (ms)', thread[:query_total_data], data)
+  write_test_output(test_case, 'Query', metrics, 'Request payload size (bytes)', separated_request_size[0], data)
+  write_test_output(test_case, 'Query', metrics, 'Response payload size (bytes)', separated_response_size[0], data)
+
+  separated_cbor_serde = separate_serde_data(thread[:cbor_serde_data])
+  write_test_output(test_case, 'CBOR', metrics, 'Serialization time (ms)', separated_cbor_serde[0], data)
+  write_test_output(test_case, 'CBOR', metrics, 'Deserialization time (ms)', separated_cbor_serde[1], data)
   write_test_output(test_case, 'CBOR', metrics, 'Total request time (ms)', thread[:cbor_total_data], data)
+  write_test_output(test_case, 'CBOR', metrics, 'Request payload size (bytes)', separated_request_size[1], data)
+  write_test_output(test_case, 'CBOR', metrics, 'Response payload size (bytes)', separated_response_size[1], data)
 
   clear_thread_data(thread)
 end
 
+thread = Thread.current
+thread[:query_serde_data] = []
+thread[:cbor_serde_data] = []
+thread[:query_total_data] = []
+thread[:cbor_total_data] = []
+thread[:request_size_data] = []
+thread[:response_size_data] = []
+thread[:warm] = false
+
+Aws::CloudWatch::Client.api.metadata['targetPrefix'] = 'GraniteServiceVersion20100801'
+Aws::CloudWatch::Client.remove_plugin(Aws::Plugins::Protocols::Query)
+query_cloudwatch = Aws::CloudWatch::Client.new(plugins: [Aws::Plugins::Protocols::Query], region: 'us-west-2')
+cbor_cloudwatch = Aws::CloudWatch::Client.new(plugins: [Aws::Plugins::Protocols::RpcV2], region: 'us-west-2')
+cbor_cloudwatch.config.api = cbor_cloudwatch.config.api.dup
+cbor_cloudwatch.config.api.metadata = cbor_cloudwatch.config.api.metadata.dup
+cbor_cloudwatch.config.api.metadata['protocol'] = 'smithy-rpc-v2-cbor'
+
+# metric_counts = [16, 64, 256, 1000]
+metric_counts = [1, 16]
+thread[:warm] = true
+
+data = File.open('perf-testing/test-output/cloudwatch/data.txt', 'w')
+raw = File.open('perf-testing/test-output/cloudwatch/raw.txt', 'w')
+
+
 metric_counts.each do |metrics|
   (0...ITERATIONS).each do |i|
-    puts 'Put Metric Data'
+    # puts 'Put Metric Data'
+    # puts 'Query'
     request = generate_put_metric_data_request(metrics, BASE_TIME, SUITE_ID)
     t_query_total_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     query_cloudwatch.put_metric_data(request)
     t_query_total_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    thread[:query_total_data] << format('%.3f', (t_query_total_end - t_query_total_start) * 1000.0)
+    thread[:query_total_data] << format('%.3f', (t_query_total_end - t_query_total_start) * 1000.0).to_f
 
+    # puts 'CBOR'
     t_cbor_total_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     cbor_cloudwatch.put_metric_data(request)
     t_cbor_total_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    thread[:cbor_total_data] << format('%.3f', (t_cbor_total_end - t_cbor_total_start) * 1000.0)
+    thread[:cbor_total_data] << format('%.3f', (t_cbor_total_end - t_cbor_total_start) * 1000.0).to_f
     sleep(2) if (i % 50).zero?
   end
-  analyze('Put metric data', metrics, thread)
+  analyze('Put metric data', metrics, thread, data, raw)
   (0...ITERATIONS).each do |i|
-    puts 'Get Metric Data'
+    # puts 'Get Metric Data'
+    # puts 'Query'
     request = generate_get_metric_data_request(metrics, BASE_TIME, SUITE_ID)
     t_query_total_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     query_resp = query_cloudwatch.get_metric_data(request)
     t_query_total_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    thread[:query_total_data] << format('%.3f', (t_query_total_end - t_query_total_start) * 1000.0)
+    thread[:query_total_data] << format('%.3f', (t_query_total_end - t_query_total_start) * 1000.0).to_f
 
+    # puts 'CBOR'
     t_cbor_total_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     cbor_resp = cbor_cloudwatch.get_metric_data(request)
     t_cbor_total_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    thread[:cbor_total_data] << format('%.3f', (t_cbor_total_end - t_cbor_total_start) * 1000.0)
-    puts 'Query Resp'
-    pp query_resp
-    puts 'Cbor Resp'
-    pp cbor_resp
+    thread[:cbor_total_data] << format('%.3f', (t_cbor_total_end - t_cbor_total_start) * 1000.0).to_f
+    # puts 'Query Resp'
+    # pp query_resp
+    # puts 'Cbor Resp'
+    # pp cbor_resp
     sleep(2) if (i % 50).zero?
   end
-  analyze('Get metric data', metrics, thread)
+  analyze('Get metric data', metrics, thread, data, raw)
 end
 (0...ITERATIONS).each do |i|
-  puts 'List Metrics'
-  request = generate_list_metrics_request(i)
+  # puts 'List Metrics'
+  # puts 'Query'
+  request = generate_list_metrics_request
   t_query_total_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
   query_resp = query_cloudwatch.list_metrics(request)
   t_query_total_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  thread[:query_total_data] << format('%.3f', (t_query_total_end - t_query_total_start) * 1000.0)
+  thread[:query_total_data] << format('%.3f', (t_query_total_end - t_query_total_start) * 1000.0).to_f
 
+  # puts 'CBOR'
   t_cbor_total_start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
   cbor_resp = cbor_cloudwatch.list_metrics(request)
   t_cbor_total_end = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  thread[:cbor_total_data] << format('%.3f', (t_cbor_total_end - t_cbor_total_start) * 1000.0)
-  puts 'Query Resp'
-  pp query_resp
-  puts 'Cbor Resp'
-  pp cbor_resp
+  thread[:cbor_total_data] << format('%.3f', (t_cbor_total_end - t_cbor_total_start) * 1000.0).to_f
+  # puts 'Query Resp'
+  # pp query_resp
+  # puts 'Cbor Resp'
+  # pp cbor_resp
   sleep(2) if (i % 50).zero?
 end
-analyze('Put metric data', 0, thread)
+analyze('Put metric data', 0, thread, data, raw)
 
 data.close
 raw.close
