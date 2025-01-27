9f7b617381b9647e37d6d4d25ac83cc9d245e6a0
Richard Wang <wangrch@amazon.com>
2025-01-27 15:03:32 -0800

    Fix script

--- perf-testing/run-perf-tests.sh
+++ perf-testing/run-perf-tests.sh
@@ -1,7 +1,13 @@
 #!/bin/zsh
 
-ruby ./perf-testing/test_local_only.rb "$1"
-ruby ./perf-testing/test_secretsmanager.rb "$1"
-ruby ./perf-testing/test_cloudwatch.rb "$1"
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-cat ./perf-testing/test-output/local-only/data.json ./perf-testing/test-output/secretsmanager/data.json ./perf-testing/test-output/cloudwatch/data.json > ./perf-testing/test-output/combined/combined.json
+ruby "$SCRIPT_DIR"/test_local_only.rb "$1"
+ruby "$SCRIPT_DIR"/test_secretsmanager.rb "$1"
+ruby "$SCRIPT_DIR"/test_cloudwatch.rb "$1"
+
+if [ ! -d "$SCRIPT_DIR"/test-output/combined ]; then
+  mkdir "$SCRIPT_DIR"/test-output/combined
+fi
+
+cat "$SCRIPT_DIR"/test-output/local-only/data.json "$SCRIPT_DIR"/test-output/secretsmanager/data.json "$SCRIPT_DIR"/test-output/cloudwatch/data.json > "$SCRIPT_DIR"/test-output/combined/combined.json
