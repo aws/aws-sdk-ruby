22bcac518e7104ddf3e32e59b804e08a2ffb82ab
Matt Muller <mamuller@amazon.com>
2025-01-29 12:26:08 -0500

    Add back correct retry delays

--- gems/aws-sdk-dynamodb/lib/aws-sdk-dynamodb/plugins/extended_retries.rb
+++ gems/aws-sdk-dynamodb/lib/aws-sdk-dynamodb/plugins/extended_retries.rb
@@ -4,6 +4,20 @@ module Aws
   module DynamoDB
     module Plugins
       class ExtendedRetries < Seahorse::Client::Plugin
+        DEFAULT_BACKOFF = lambda do |c|
+          return if c.retries < 1
+
+          delay = 2**(c.retries - 1) * c.config.retry_base_delay
+          if (c.config.retry_max_delay || 0) > 0
+            delay = [delay, c.config.retry_max_delay].min
+          end
+          jitter = c.config.retry_jitter
+          jitter = JITTERS[jitter] if jitter.is_a?(Symbol)
+          delay = jitter.call(delay) if jitter
+          Kernel.sleep(delay)
+        end
+
+
         option(
           :retry_limit,
           default: 10,
@@ -24,6 +38,15 @@ This option is only used in the `legacy` retry mode.
           docstring: <<-DOCS)
 The base delay in seconds used by the default backoff function. This option
 is only used in the `legacy` retry mode.
+        DOCS
+
+        option(
+          :retry_backoff,
+          default: DEFAULT_BACKOFF,
+          doc_type: Proc,
+          docstring: <<-DOCS)
+A proc or lambda used for backoff. Defaults to 2**retries * retry_base_delay.
+This option is only used in the `legacy` retry mode.
         DOCS
       end
     end
