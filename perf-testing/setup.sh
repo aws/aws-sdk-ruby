85f415c655b8b4a7a12963b8e9254b3568cf479a
Richard Wang <wangrch@amazon.com>
2025-02-04 15:29:29 -0800

    Update setup script

--- perf-testing/setup.sh
+++ perf-testing/setup.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-yum -y install \
-    zip unzip \
-    tar \
-    make \
-    gcc \
-    git \
-    libicu \
-    libxml2 libxml2-devel \
-    gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel \
-    rustc \
-    awscli wget
+sudo yum -y install \
+  zip unzip \
+  tar \
+  make \
+  gcc \
+  git \
+  libicu \
+  libxml2 libxml2-devel \
+  gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel libffi-devel \
+  rustc \
+  awscli wget
 
 curl -L https://cache.ruby-lang.org/pub/ruby/3.4/ruby-3.4.1.tar.gz | tar xz && \
- cd ruby-3.4.1 && \
- ./configure --enable-yjit && make && make install
+  cd ruby-3.4.1 && \
+  ./configure --enable-yjit && make && make install
 
 cd ..
 
