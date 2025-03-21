26c4ccaae208e03ef851f0e90d646c946b41d051
Matt Muller <mamuller@amazon.com>
2025-03-21 13:15:29 -0400

    Fix failing test

--- gems/aws-sdk-core/spec/seahorse/client/h2/connection_spec.rb
+++ gems/aws-sdk-core/spec/seahorse/client/h2/connection_spec.rb
@@ -7,7 +7,7 @@ module Seahorse
     module H2
 
       describe Connection do
-        let(:conn) { Connection.new }
+        let(:conn) { Connection.new(max_concurrent_streams: 1) }
 
         describe '#new_stream' do
 
