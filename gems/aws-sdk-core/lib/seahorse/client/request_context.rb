bf2fabb9b1bffc3dba02920b175385310b403340
Juli Tera <terajul@amazon.com>
2025-06-06 11:05:46 -0700

    Fix doc warning

--- gems/aws-sdk-core/lib/seahorse/client/request_context.rb
+++ gems/aws-sdk-core/lib/seahorse/client/request_context.rb
@@ -6,6 +6,7 @@ module Seahorse
   module Client
     class RequestContext
 
+      # @param [Hash] options
       # @option options [required,Symbol] :operation_name (nil)
       # @option options [required,Model::Operation] :operation (nil)
       # @option options [Model::Authorizer] :authorizer (nil)
@@ -16,7 +17,7 @@ module Seahorse
       # @option options [Http::Response] :http_response (Http::Response.new)
       # @option options [Integer] :retries (0)
       # @option options [Aws::Telemetry::TracerBase] :tracer (Aws::Telemetry::NoOpTracer.new)
-      # @options options [Hash] :metadata ({})
+      # @option options [Hash] :metadata ({})
       def initialize(options = {})
         @operation_name = options[:operation_name]
         @operation = options[:operation]
