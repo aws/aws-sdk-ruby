89d42bacee678f7d0c033b2d8e1a8ee574f3f485
Juli Tera <terajul@amazon.com>
2025-06-11 14:22:11 -0700

    Small update

--- gems/aws-sdk-core/lib/aws-sdk-core/credential_provider_chain.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/credential_provider_chain.rb
@@ -186,16 +186,6 @@ module Aws
       end
     end
 
-    def ec2_metadata_disabled?(profile_name)
-      if ENV['AWS_EC2_METADATA_DISABLED'] == 'true' ||
-         Aws.shared_config.config_enabled? &&
-         Aws.shared_config.disable_ec2_metadata(profile: profile_name) == 'true'
-        return true
-      end
-
-      false
-    end
-
     def instance_profile_credentials(options)
       profile_name = determine_profile_name(options)
       if ENV['AWS_CONTAINER_CREDENTIALS_RELATIVE_URI'] ||
@@ -206,6 +196,16 @@ module Aws
 
         InstanceProfileCredentials.new(options.merge(profile: profile_name))
       end
+    end
+
+    def ec2_metadata_disabled?(profile_name)
+      if ENV['AWS_EC2_METADATA_DISABLED'] == 'true' ||
+         Aws.shared_config.config_enabled? &&
+         Aws.shared_config.disable_ec2_metadata(profile: profile_name) == 'true'
+        return true
+      end
+
+      false
     end
 
     def assume_role_with_profile(options, profile_name)
