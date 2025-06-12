0f9217db80fae196f2461f61fd72f51e99790740
Juli Tera <terajul@amazon.com>
2025-06-12 14:23:06 -0700

    Some feedback updates

--- gems/aws-sdk-core/lib/aws-sdk-core/instance_profile_credentials.rb
+++ gems/aws-sdk-core/lib/aws-sdk-core/instance_profile_credentials.rb
@@ -7,13 +7,13 @@ module Aws
   # An auto-refreshing credential provider that loads credentials from
   # EC2 instances using IMDSv2.
   #
-  #     ec2_metadata = Aws::EC2Metadata.new # with customized opts
+  #     ec2_metadata = Aws::EC2Metadata.new(retries: 4)
   #     instance_creds = Aws::InstanceProfileCredentials.new(
-  #       client: ec2_metadata
+  #       ec2_metadata: ec2_metadata
   #     )
   #     ec2_client = Aws::EC2::Client.new(credentials: instance_creds)
   #
-  # If you omit the `:client` option, a new {Aws::EC2Metadata} will
+  # If you omit the `:ec2_metadata` option, a new {Aws::EC2Metadata} will
   # be created with options provided.
   class InstanceProfileCredentials
     include CredentialProvider
@@ -33,20 +33,21 @@ module Aws
       '/latest/meta-data/iam/security-credentials-extended/'
 
     # @param [Hash] options
-    # @option options [Aws::EC2Metadata] :client A custom EC2 metadata client to
-    #   use for loading credentials from IMDSv2. If not provided, a default
-    #   {Aws::EC2Metadata} client will be constructed with passed options.
+    # @option options [Aws::EC2Metadata] :ec2_metadata A custom EC2 metadata
+    #   client to use for loading credentials from IMDSv2. If not provided,
+    #   a default {Aws::EC2Metadata} client will be constructed with
+    #   passed options.
     # @option options [String] :ec2_instance_profile_name (nil) When set, this
     #   provider skips querying IMDS for the name of the active profile. When
-    #   `:ec2_instance_profile_name` are not configured directly, the following
+    #   `:ec2_instance_profile_name` is not configured directly, the following
     #   locations will be searched before defaulting to `nil`:
     #
     #   * `ENV['AWS_EC2_INSTANCE_PROFILE_NAME']`
     #   * `~/.aws/config`
     #
-    # @option options [Integer] :retries (3) Number of times to retry
-    #   when retrieving credentials. Defaults to `1` when resolving from
-    #   the default credential chain
+    # @option options [Integer] :retries (3) Number of times to retry for the
+    #   {Aws::EC2Metadata} when retrieving credentials. Defaults to `0` when
+    #   resolving from the default credential chain.
     # @option options [String] :endpoint ('http://169.254.169.254') The IMDS
     #   endpoint. This option has precedence over the `:endpoint_mode`.
     # @option options [String] :endpoint_mode ('IPv4') The endpoint mode for the
@@ -67,29 +68,32 @@ module Aws
     #   like `$stdout`.
     # @option options [Integer] :token_ttl Time-to-Live in seconds for EC2
     #   Metadata Token used for fetching Metadata Profile Credentials, defaults
-    #   to 21600 seconds
+    #   to `21600` seconds.
     # @option options [Callable] :before_refresh Proc called before
     #   credentials are refreshed. `before_refresh` is called
     #   with an instance of this object when AWS credentials are required
     #   and need to be refreshed.
     def initialize(options = {})
-      @client = options[:client] ||
-                EC2Metadata.new(resolve_client_opts(options))
+      @ec2_metadata =
+        options[:ec2_metadata] ||
+        EC2Metadata.new(resolve_opts(options))
+      options.delete(:ec2_metadata)
+      @profile_name = nil
       @ec2_instance_profile_name = resolve_ec2_instance_profile_name(options)
+
       @no_refresh_until = nil
       @async_refresh = false
       @metrics = ['CREDENTIALS_IMDS']
-
       @api_version = :unknown
-      @resolved_profile = nil
+      @metadata_path = METADATA_EXTENDED_PATH
       super
     end
 
     # @return [Integer] Number of times to retry when retrieving credentials
-    #   from the instance metadata service. Defaults to `1` when resolving from
+    #   from the instance metadata service. Defaults to `0` when resolving from
     #   the default credential chain.
     def retries
-      @client.retries
+      @ec2_metadata.retries
     end
 
     private
@@ -99,20 +103,15 @@ module Aws
     # :extended or :legacy after the first successful call
     attr_accessor :api_version
 
-    # tracks profile name returned from an IMDS call
-    attr_accessor :resolved_profile
-
     def empty_credentials?(creds)
-      return true if creds.nil?
-
-      !creds.set?
+      creds.nil? || !creds.set?
     end
 
     def fetch_credentials
-      profile_name = resolve_profile_name
+      resolve_profile_name
 
       begin
-        creds = @client.get(metadata_path + profile_name)
+        creds = @ec2_metadata.get(metadata_path + @profile_name)
         @api_version = :extended if @api_version == :unknown
         creds
       rescue EC2Metadata::MetadataNotFoundError
@@ -121,15 +120,15 @@ module Aws
           fetch_credentials
         elsif @ec2_instance_profile_name.nil?
           # cache profile may have been replaced
-          @resolved_profile = nil
+          @profile_name = nil
           fetch_credentials
         else
           raise InvalidProfile
         end
       end
+    rescue InvalidProfile
+      raise
     rescue StandardError => e
-      raise if e.is_a?(InvalidProfile)
-
       warn("Error retrieving instance profile credentials: #{e}")
       '{}'
     end
@@ -165,15 +164,21 @@ module Aws
       rand(300..360)
     end
 
-    def resolve_client_opts(options)
-      opts = options.dup
-      opts[:backoff] = opts[:delay] if opts[:delay]
-      # delete unnecessary configs for ec2 metadata client
-      %i[client delay ec2_instance_profile_name].each { |k| opts.delete(k) }
-      opts.merge(
+    def resolve_opts(options)
+      ec2_metadata_opts = options.merge(
         endpoint_mode: resolve_endpoint_mode(options),
         endpoint: resolve_endpoint(options)
       )
+
+      if ec2_metadata_opts[:delay]
+        ec2_metadata_opts[:backoff] = ec2_metadata_opts[:delay]
+        ec2_metadata_opts.delete(:delay)
+        warn('The `:delay` option is deprecated. Use `:backoff` instead.')
+      end
+
+      # delete unnecessary configs for ec2 metadata client
+      %i[client ec2_instance_profile_name].each { |k| opts.delete(k) }
+      ec2_metadata_opts
     end
 
     def resolve_ec2_instance_profile_name(options)
@@ -187,6 +192,11 @@ module Aws
     end
 
     def resolve_endpoint(options)
+      if (value = options[:ip_address])
+        warn('The `:ip_address` option is deprecated. Use `:endpoint` instead.')
+        return value
+      end
+
       options[:endpoint] ||
         ENV['AWS_EC2_METADATA_SERVICE_ENDPOINT'] ||
         Aws.shared_config.ec2_metadata_service_endpoint(
@@ -203,23 +213,18 @@ module Aws
     end
 
     def resolve_profile_name
-      if @ec2_instance_profile_name
-        @ec2_instance_profile_name
-      elsif @resolved_profile
-        @resolved_profile
-      else
-        begin
-          metadata = @client.get(metadata_path)
-          @resolved_profile = metadata.lines.first.strip
-          @api_version = :extended if @api_version == :unknown
-          @resolved_profile
-        rescue EC2Metadata::MetadataNotFoundError
-          raise unless @api_version == :unknown
+      return if @profile_name
 
-          # fall back to legacy api
-          @api_version = :legacy
-          resolve_profile_name
-        end
+      begin
+        metadata = @ec2_metadata.get(metadata_path)
+        @profile_name = metadata.lines.first.strip
+        @api_version = :extended if @api_version == :unknown
+      rescue EC2Metadata::MetadataNotFoundError
+        raise unless @api_version == :unknown
+
+        # fall back to legacy api
+        @api_version = :legacy
+        resolve_profile_name
       end
     end
 
