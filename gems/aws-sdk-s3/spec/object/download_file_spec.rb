d445c1a1e512ac9f049a486498cf83279478cd5e
Juli Tera <terajul@amazon.com>
2025-07-28 13:05:27 -0700

    Improve flakey spec

--- gems/aws-sdk-s3/spec/object/download_file_spec.rb
+++ gems/aws-sdk-s3/spec/object/download_file_spec.rb
@@ -281,7 +281,7 @@ module Aws
           end
 
           it 'deletes partially downloaded file when download fails' do
-            client.stub_responses(:get_object, [{ body: 'body', content_range: 'bytes 0-5242879/15728640' }, Timeout::Error])
+            client.stub_responses(:get_object, [{ body: 'body', content_range: 'bytes 0-5242879/15728640' }])
             expect { single_obj.download_file(path) }.to raise_error(Aws::S3::MultipartDownloadError)
             expect(File.exist?(path)).to be(false)
           end
