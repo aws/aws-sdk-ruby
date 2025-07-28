73662962d9686fc2e5394ffd61d85e152d142e30
Juli Tera <terajul@amazon.com>
2025-07-28 12:52:55 -0700

    Add new validations tests to downloader

--- gems/aws-sdk-s3/spec/object/download_file_spec.rb
+++ gems/aws-sdk-s3/spec/object/download_file_spec.rb
@@ -59,58 +59,15 @@ module Aws
           small_obj.download_file(path)
         end
 
-        it 'reports progress for single part objects' do
-          small_file_size = 1024
-          expect(client)
-            .to receive(:get_object)
-            .with(
-              { bucket: 'bucket', key: 'small', response_target: path, on_chunk_received: instance_of(Proc) }
-            ) do |args|
-            args[:on_chunk_received].call(small_file, small_file_size, small_file_size)
-          end
-
-          n_calls = 0
-          callback = proc do |bytes, part_sizes, total|
-            expect(bytes).to eq([small_file_size])
-            expect(part_sizes).to eq([small_file_size])
-            expect(total).to eq(small_file_size)
-            n_calls += 1
-          end
-
-          small_obj.download_file(path, progress_callback: callback)
-          expect(n_calls).to eq(1)
-        end
-
         it 'download larger files in parts' do
           expect(client)
             .to receive(:head_object)
             .with({ bucket: 'bucket', key: 'large', part_number: 1 })
             .exactly(1).times
 
-          client.stub_responses(:get_object, -> (_ctx) { { body: 'body', content_range: 'bytes 0-4/4' } })
+          client.stub_responses(:get_object, ->(_ctx) { { body: 'body', content_range: 'bytes 0-4/4' } })
 
           large_obj.download_file(path)
-        end
-
-        it 'reports progress for files downloaded in parts' do
-          expect(client).to receive(:get_object).exactly(4).times do |args|
-            args[:on_chunk_received].call(large_file, 4, 4)
-            client.stub_data(:get_object, body: StringIO.new('chunk'), content_range: 'bytes 0-4/4')
-          end
-
-          n_calls = 0
-          mutex = Mutex.new
-          callback = proc do |bytes, part_sizes, total|
-            mutex.synchronize do
-              expect(bytes.size).to eq(4)
-              expect(part_sizes.size).to eq(4)
-              expect(total).to eq(20 * one_meg)
-              n_calls += 1
-            end
-          end
-          large_obj.download_file(path, progress_callback: callback)
-
-          expect(n_calls).to eq(4)
         end
 
         it 'download larger files in ranges' do
@@ -119,7 +76,7 @@ module Aws
             .with({ bucket: 'bucket', key: 'single', part_number: 1 })
             .exactly(1).times
 
-          client.stub_responses(:get_object, -> (context) {
+          client.stub_responses(:get_object, lambda { |context|
             responses = {
               'bytes=0-5242879' => { body: 'body', content_range: 'bytes 0-5242879/15728640' },
               'bytes=10485760-15728639' => { body: 'body', content_range: 'bytes 10485760-15728639/15728640' },
@@ -138,56 +95,6 @@ module Aws
             .exactly(1).times
 
           small_obj.download_file(path, version_id: version_id)
-        end
-
-        it 'raises an error when checksum validation fails on single part' do
-          client.stub_responses(:get_object, { body: 'body', checksum_sha1: 'invalid' })
-
-          expect { small_obj.download_file(path) }.to raise_error(Aws::Errors::ChecksumError)
-        end
-
-        it 'raises an error when checksum validation fails on multipart' do
-          client.stub_responses(:get_object, { body: 'body', checksum_sha1: 'invalid' })
-
-          thread = double(value: nil)
-          expect(Thread).to receive(:new).and_yield.and_return(thread)
-
-          expect { large_obj.download_file(path) }.to raise_error(Aws::Errors::ChecksumError)
-        end
-
-        it 'does not download object when ETAG does not match during multipart get by ranges' do
-          allow(client)
-            .to receive(:head_object)
-            .with({ bucket: 'bucket', key: 'single', part_number: 1 })
-            .and_return(
-              client.stub_data(:head_object, content_length: 15 * one_meg, parts_count: nil, etag: 'test-etag')
-            )
-
-          client.stub_responses(:get_object, -> (ctx) {
-            expect(ctx.params[:if_match]).to eq('test-etag')
-            'PreconditionFailed'
-          })
-
-          thread = double(value: nil)
-          expect(Thread).to receive(:new).and_yield.and_return(thread)
-          expect { single_obj.download_file(path) }.to raise_error(Aws::S3::Errors::PreconditionFailed)
-        end
-
-        it 'does not download object when ETAG does not match during multipart get by parts' do
-          allow(client)
-            .to receive(:head_object)
-            .with({ bucket: 'bucket', key: 'large' })
-            .and_return(client.stub_data(:head_object, content_length: 20 * one_meg, etag: 'test-etag'))
-
-          client.stub_responses(:get_object, -> (ctx) {
-            expect(ctx.params[:if_match]).to eq('test-etag')
-            'PreconditionFailed'
-          })
-
-          thread = double(value: nil)
-          expect(Thread).to receive(:new).and_yield.and_return(thread)
-
-          expect { large_obj.download_file(path) }.to raise_error(Aws::S3::Errors::PreconditionFailed)
         end
 
         it 'calls on_checksum_validated on single part' do
@@ -235,38 +142,149 @@ module Aws
           small_obj.download_file(path, checksum_mode: 'DISABLED')
         end
 
-        it 'raises an error if an invalid mode is specified' do
-          expect { large_obj.download_file(path, mode: 'invalid_mode') }
-            .to raise_error(
-              ArgumentError,
-              'Invalid mode invalid_mode provided, mode should '\
-              'be :single_request, :get_range or :auto'
-            )
-        end
-
-        it 'raises an error if choose :get_range without :chunk_size' do
-          expect { large_obj.download_file(path, mode: 'get_range') }
-            .to raise_error(ArgumentError, 'In :get_range mode, :chunk_size must be provided')
-        end
-
-        it 'raises an error if :chunk_size is larger than file size' do
-          expect { large_obj.download_file(path, chunk_size: 50 * one_meg) }
-            .to raise_error(ArgumentError, ":chunk_size shouldn't exceed total file size.")
-        end
-
-        it 'raises an error if :on_checksum_validated is not callable' do
-          expect { large_obj.download_file(path, on_checksum_validated: 'string') }
-            .to raise_error(ArgumentError, 'on_checksum_validated must be callable')
-        end
-
         it 'downloads the file in range chunks' do
-          client.stub_responses(:get_object, -> (context) {
+          client.stub_responses(:get_object, lambda { |context|
             ranges = context.params[:range].split('=').last.split('-')
             expect(ranges[1].to_i - ranges[0].to_i + 1).to eq(one_meg)
             { content_range: "bytes #{ranges[0]}-#{ranges[1]}/#{20 * one_meg}" }
           })
 
           large_obj.download_file(path, chunk_size: one_meg)
+        end
+
+        context 'multipart progress' do
+          it 'reports progress for single part objects' do
+            small_file_size = 1024
+            expect(client)
+              .to receive(:get_object)
+              .with({ bucket: 'bucket', key: 'small', response_target: path, on_chunk_received: instance_of(Proc) }) do |args|
+              args[:on_chunk_received].call(small_file, small_file_size, small_file_size)
+            end
+
+            n_calls = 0
+            callback = proc do |bytes, part_sizes, total|
+              expect(bytes).to eq([small_file_size])
+              expect(part_sizes).to eq([small_file_size])
+              expect(total).to eq(small_file_size)
+              n_calls += 1
+            end
+
+            small_obj.download_file(path, progress_callback: callback)
+            expect(n_calls).to eq(1)
+          end
+          it 'reports progress for files downloaded in parts' do
+            expect(client).to receive(:get_object).exactly(4).times do |args|
+              args[:on_chunk_received].call(large_file, 4, 4)
+              client.stub_data(:get_object, body: StringIO.new('chunk'), content_range: 'bytes 0-4/4')
+            end
+
+            n_calls = 0
+            mutex = Mutex.new
+            callback = proc do |bytes, part_sizes, total|
+              mutex.synchronize do
+                expect(bytes.size).to eq(4)
+                expect(part_sizes.size).to eq(4)
+                expect(total).to eq(20 * one_meg)
+                n_calls += 1
+              end
+            end
+            large_obj.download_file(path, progress_callback: callback)
+
+            expect(n_calls).to eq(4)
+          end
+        end
+
+        context 'error handling' do
+          it 'raises an error when checksum validation fails on single part' do
+            client.stub_responses(:get_object, { body: 'body', checksum_sha1: 'invalid' })
+
+            expect { small_obj.download_file(path) }.to raise_error(Aws::Errors::ChecksumError)
+          end
+
+          it 'raises an error when checksum validation fails on multipart' do
+            client.stub_responses(:get_object, { body: 'body', checksum_sha1: 'invalid' })
+
+            thread = double(value: nil)
+            expect(Thread).to receive(:new).and_yield.and_return(thread)
+
+            expect { large_obj.download_file(path) }.to raise_error(Aws::Errors::ChecksumError)
+          end
+
+          it 'does not download object when ETAG does not match during multipart get by ranges' do
+            stub = client.stub_data(:head_object, content_length: 15 * one_meg, parts_count: nil, etag: 'test-etag')
+            allow(client)
+              .to receive(:head_object)
+              .with({ bucket: 'bucket', key: 'single', part_number: 1 })
+              .and_return(stub)
+
+            client.stub_responses(:get_object, lambda { |ctx|
+              expect(ctx.params[:if_match]).to eq('test-etag')
+              'PreconditionFailed'
+            })
+
+            thread = double(value: nil)
+            expect(Thread).to receive(:new).and_yield.and_return(thread)
+            expect { single_obj.download_file(path) }.to raise_error(Aws::S3::Errors::PreconditionFailed)
+          end
+
+          it 'does not download object when ETAG does not match during multipart get by parts' do
+            allow(client)
+              .to receive(:head_object)
+              .with({ bucket: 'bucket', key: 'large' })
+              .and_return(client.stub_data(:head_object, content_length: 20 * one_meg, etag: 'test-etag'))
+
+            client.stub_responses(:get_object, lambda { |ctx|
+              expect(ctx.params[:if_match]).to eq('test-etag')
+              'PreconditionFailed'
+            })
+
+            thread = double(value: nil)
+            expect(Thread).to receive(:new).and_yield.and_return(thread)
+
+            expect { large_obj.download_file(path) }.to raise_error(Aws::S3::Errors::PreconditionFailed)
+          end
+
+          it 'raises an error if an invalid mode is specified' do
+            expect { large_obj.download_file(path, mode: 'invalid_mode') }
+              .to raise_error(ArgumentError, /Invalid mode invalid_mode provided/)
+          end
+
+          it 'raises an error if choose :get_range without :chunk_size' do
+            expect { large_obj.download_file(path, mode: 'get_range') }
+              .to raise_error(ArgumentError, 'In :get_range mode, :chunk_size must be provided')
+          end
+
+          it 'raises an error if :chunk_size is larger than file size' do
+            expect { large_obj.download_file(path, chunk_size: 50 * one_meg) }
+              .to raise_error(ArgumentError, ":chunk_size shouldn't exceed total file size.")
+          end
+
+          it 'raises an error if :on_checksum_validated is not callable' do
+            expect { large_obj.download_file(path, on_checksum_validated: 'string') }
+              .to raise_error(ArgumentError, 'on_checksum_validated must be callable')
+          end
+
+          it 'raises MultipartDownloadError when expected parts count differs from completed requests' do
+            # Mock PartList to simulate missing parts
+            part_list = double('PartList')
+            allow(part_list).to receive(:count).and_return(4)
+            allow(part_list).to receive(:shift).and_return(nil)
+            allow(Aws::S3::FileDownloader::PartList).to receive(:new).and_return(part_list)
+
+            expect { large_obj.download_file(path) }.to raise_error(Aws::S3::MultipartDownloadError)
+          end
+
+          it 'raises error when range validation fails' do
+            client.stub_responses(:get_object, { body: 'body', content_range: 'bytes 0-4/4' })
+            expect { large_obj.download_file(path, mode: 'get_range', chunk_size: one_meg) }
+              .to raise_error(Aws::S3::MultipartDownloadError)
+          end
+
+          it 'deletes partially downloaded file when download fails' do
+            client.stub_responses(:get_object, [{ body: 'body', content_range: 'bytes 0-5242879/15728640' }, Timeout::Error])
+            expect { single_obj.download_file(path) }.to raise_error(Timeout::Error)
+            expect(File.exist?(path)).to be(false)
+          end
         end
       end
     end
