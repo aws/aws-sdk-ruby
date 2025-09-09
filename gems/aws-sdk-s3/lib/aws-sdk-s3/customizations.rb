7749ba5bbab9a8c111847e800672826f27917284
Juli Tera <terajul@amazon.com>
2025-09-09 07:44:39 -0700

    Add default executor

--- gems/aws-sdk-s3/lib/aws-sdk-s3/customizations.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/customizations.rb
@@ -6,6 +6,7 @@ module Aws
     autoload :BucketRegionCache, 'aws-sdk-s3/bucket_region_cache'
     autoload :Encryption, 'aws-sdk-s3/encryption'
     autoload :EncryptionV2, 'aws-sdk-s3/encryption_v2'
+    autoload :DefaultExecutor, 'aws-sdk-s3/default_executor'
     autoload :FilePart, 'aws-sdk-s3/file_part'
     autoload :FileUploader, 'aws-sdk-s3/file_uploader'
     autoload :FileDownloader, 'aws-sdk-s3/file_downloader'
---
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/default_executor.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+module Aws
+  module S3
+    # @api private
+    class DefaultExecutor
+      def initialize(options = {})
+        @queue = Queue.new
+        @max_threads = options[:max_threads] || 10
+        @pool = []
+        @running = true
+        monitor_pool
+      end
+
+      def post(*args, &block)
+        raise 'Executor is not running' unless @running
+
+        @queue << [args, block]
+      end
+
+      def shutdown
+        @running = false
+        @max_threads.times { @queue << :shutdown }
+        @pool.each(&:join)
+        @pool.clear
+        true
+      end
+
+      private
+
+      def monitor_pool
+        Thread.new do
+          while @running
+            @pool.select!(&:alive?)
+
+            @pool << spawn_worker if @queue.size > @pool.size && @pool.size < @max_threads
+            sleep(0.01)
+          end
+        end
+      end
+
+      def spawn_worker
+        Thread.new do
+          while (job = @queue.pop)
+            break if job == :shutdown
+
+            args, block = job
+            block.call(*args)
+          end
+        end
+      end
+    end
+  end
+end
