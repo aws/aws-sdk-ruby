d680917589f0c0c5b102d9e740f0c8e3092f31fd
Juli Tera <terajul@amazon.com>
2025-09-15 13:55:55 -0700

    Rubocop on specs + minor updates

--- gems/aws-sdk-cloudfront/spec/url_signer_spec.rb
+++ gems/aws-sdk-cloudfront/spec/url_signer_spec.rb
@@ -5,46 +5,51 @@ require_relative 'spec_helper'
 module Aws
   module CloudFront
     describe UrlSigner do
-      let(:rsa_key) { OpenSSL::PKey::RSA.new(1024).to_pem }
-      let(:options) { { key_pair_id: 'CF_KEYPAIR_ID' } }
-      let(:signer) { Aws::CloudFront::UrlSigner.new(options) }
-      let(:expires) { 1357034400 } # January 1, 2013 10:00 am UTC (Unix timestamp)
+      let(:signer) do
+        UrlSigner.new(
+          key_pair_id: 'CF_KEYPAIR_ID',
+          private_key_path: "#{File.dirname(__FILE__)}/rsa_dummy_key"
+        )
+      end
+      let(:expires) { 1_357_034_400 } # January 1, 2013 10:00 am UTC (Unix timestamp)
 
       describe '#initialize' do
         it 'accepts RSA private key' do
-          options[:private_key] = rsa_key
-          expect { signer }.to_not raise_error
+          expect do
+            UrlSigner.new(key_pair_id: 'CF_KEYPAIR_ID', private_key: OpenSSL::PKey::RSA.new(1024).to_pem)
+          end.to_not raise_error
         end
 
         it 'accepts ECDSA private key' do
-          options[:private_key_path] = "#{File.dirname(__FILE__)}/ecdsa_dummy_key"
-          expect { signer }.to_not raise_error
+          expect do
+            UrlSigner.new(key_pair_id: 'CF_KEYPAIR_ID', private_key_path: "#{File.dirname(__FILE__)}/ecdsa_dummy_key")
+          end.to_not raise_error
         end
 
         it 'accepts private key path' do
-          options[:private_key_path] = "#{File.dirname(__FILE__)}/rsa_dummy_key"
-          expect { signer }.to_not raise_error
+          expect do
+            UrlSigner.new(key_pair_id: 'CF_KEYPAIR_ID', private_key_path: "#{File.dirname(__FILE__)}/rsa_dummy_key")
+          end.to_not raise_error
         end
 
         it 'raises when private key or private key path is blank' do
-          expect { signer }.to raise_error(ArgumentError, /:private_key or :private_key_path should be provided/)
+          expect do
+            UrlSigner.new(key_pair_id: 'CF_KEYPAIR_ID')
+          end.to raise_error(ArgumentError, /:private_key or :private_key_path should be provided/)
         end
 
         it 'raises when key pair id is blank' do
-          options.delete(:key_pair_id)
-          options[:private_key] = rsa_key
-          expect { signer }.to raise_error(ArgumentError, /:key_pair_id must not be blank/)
+          expect do
+            UrlSigner.new(private_key: OpenSSL::PKey::RSA.new(1024).to_pem)
+          end.to raise_error(ArgumentError, /:key_pair_id must not be blank/)
         end
       end
 
       describe '#signed_url' do
-        let(:options) { { key_pair_id: 'CF_KEYPAIR_ID', private_key_path: "#{File.dirname(__FILE__)}/rsa_dummy_key" } }
-
         it 'raises error if url is invalid' do
-          expect {
-            signer.signed_url('what_ever_illegal/url')
-          }.to raise_error(ArgumentError)
+          expect { signer.signed_url('what_ever_illegal/url') }.to raise_error(ArgumentError)
         end
+
         it 'can generate signed urls with custom policy' do
           policy = {
             'Statement' => [
@@ -55,10 +60,7 @@ module Aws
               }
             ]
           }
-          url = signer.signed_url(
-            'http://abc.cloudfront.net/images/image.jpg',
-            :policy => policy.to_json
-          )
+          url = signer.signed_url('http://abc.cloudfront.net/images/image.jpg', policy: policy.to_json)
           expected_url = 'http://abc.cloudfront.net/images/image.jpg?'\
                          'Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiaW1hZ2VzL2l'\
                          'tYWdlLmpwZyIsIkNvbmRpdGlvbiI6eyJJcEFkZHJlc3MiOnsiQVdTO'\
@@ -68,26 +70,21 @@ module Aws
                          '8yQh8LYOxewItGJXFq0BxnKuSEKoEVYVTFEteFAGKXwhkbC7K~JfL8'\
                          '3aroPbRagjyG-V9Y5wMLccBAzMj5nHXxjvjlOu541VUR-RlR0KK106'\
                          'HT4-Hp1c~nyOmXs4R5mU_&Key-Pair-Id=CF_KEYPAIR_ID'
-            expect(url).to eq(expected_url)
+          expect(url).to eq(expected_url)
         end
+
         it 'can generate signed urls with canned policy' do
-          url = signer.signed_url(
-            'https://abc.cloudfront.net/images/image.jpg?color=red',
-            :expires => expires
-          )
+          url = signer.signed_url('https://abc.cloudfront.net/images/image.jpg?color=red', expires: expires)
           expected_url = 'https://abc.cloudfront.net/images/image.jpg?'\
                          'color=red&Expires=1357034400&Signature=GvrDx3aAG1u1sAQF'\
                          '68c~xD6LVORt36mRTvC2u5RwLjsvusXI0sJPxy3D0R8AQp4qFZlRehw'\
                          'h~mablw8DBNRFLQ81mazmbrUOhXbuepav5ZmCU-KgOmXtpMS49L7TLG'\
                          'USfwSksDx1qriAtB4mS4iJaNt2mfo0C5G-vlt9qMftkJg_'\
                          '&Key-Pair-Id=CF_KEYPAIR_ID'
-            expect(url).to eq(expected_url)
+          expect(url).to eq(expected_url)
         end
         it 'can generate signed rtmp urls' do
-          url = signer.signed_url(
-            'rtmp://example-distribution.cloudfront.net/videos/example.mp4',
-            :expires => expires
-          )
+          url = signer.signed_url('rtmp://example-distribution.cloudfront.net/videos/example.mp4', expires: expires)
           expected_url = 'videos/example.mp4?Expires=1357034400'\
                          '&Signature=TMXSGAw6x4fJsUfmBzJGbR2HYPduVFTPtqmoVQeZOEO'\
                          'Bs1NbQ6sARJoIMb9ot4CYyY95nNy8pzuuOkOm4DTsbda30a-8BrKx7'\
@@ -97,10 +94,7 @@ module Aws
         end
 
         it 'can handle urls with url-like paths' do
-          url = signer.signed_url(
-            'https://abc.cloudfront.net/images/from/s3://bucket/file.jpg',
-            :expires => expires
-          )
+          url = signer.signed_url('https://abc.cloudfront.net/images/from/s3://bucket/file.jpg', expires: expires)
           expected_url = 'https://abc.cloudfront.net/images/from/s3://bucket/file.jpg?'\
                          'Expires=1357034400&Signature=Xk0prcC1fjhyROJOIr~x0KUuc-wEOQ~'\
                          'v0zHwQ1GjPZH2rJPDIbjwNcvK8NIwyuQSk4PxmpaI1oVV8t62tuMPiQ8NuP2'\
