8a86909acb8c499ef3a362aa53e42d93e6b2335c
Juli Tera <terajul@amazon.com>
2025-09-23 11:30:21 -0700

    Add more context

--- gems/aws-sdk-s3/lib/aws-sdk-s3/file_downloader.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/file_downloader.rb
@@ -14,6 +14,10 @@ module Aws
 
       def initialize(options = {})
         @client = options[:client] || Client.new
+        # Downloads now use temporary files for String/Pathname destinations to prevent
+        # corruption on failure. However, customers who pass File/Tempfile paths expect
+        # the object to reflect changes during download. This flag restores direct writes
+        # for backward compatibility. Will be removed in next major version.
         @enable_direct_writes = options[:enable_direct_writes] || false
       end
 
