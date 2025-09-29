b9231e777ea5a63350edad400d8521e4d16f33b2
Juli Tera <terajul@amazon.com>
2025-09-29 09:08:25 -0700

    Feedback - update default executor

--- gems/aws-sdk-s3/lib/aws-sdk-s3/default_executor.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/default_executor.rb
@@ -4,37 +4,74 @@ module Aws
   module S3
     # @api private
     class DefaultExecutor
+      RUNNING = :running
+      SHUTTING_DOWN = :shutting_down
+      SHUTDOWN = :shutdown
+
       def initialize(options = {})
-        @queue = Queue.new
         @max_threads = options[:max_threads] || 10
+        @state = RUNNING
+        @queue = Queue.new
         @pool = []
-        @running = true
         @mutex = Mutex.new
       end
 
       def post(*args, &block)
-        raise 'Executor is not running' unless @running
+        raise 'Executor is not accepting new tasks' unless @state == RUNNING
 
         @queue << [args, block]
         ensure_worker_available
       end
 
-      def shutdown
-        @running = false
+      def kill
+        @mutex.synchronize do
+          @state = SHUTDOWN
+          @pool.each(&:kill)
+          @pool.clear
+          @queue.clear
+        end
+        true
+      end
+
+      def shutdown(timeout = nil)
+        @mutex.synchronize do
+          return true if @state == SHUTDOWN
+
+          @state = SHUTTING_DOWN
+        end
+
         @max_threads.times { @queue << :shutdown }
-        @pool.each(&:join)
+
+        if timeout
+          @pool.each { |thread| thread.join(timeout) }
+          @pool.select(&:alive?).each(&:kill)
+        else
+          @pool.each(&:join)
+        end
+
         @pool.clear
+        @state = SHUTDOWN
         true
       end
 
       def running?
-        @running
+        @state == RUNNING
+      end
+
+      def shutting_down?
+        @state == SHUTTING_DOWN
+      end
+
+      def shutdown?
+        @state == SHUTDOWN
       end
 
       private
 
       def ensure_worker_available
         @mutex.synchronize do
+          return unless @state == RUNNING
+
           @pool.select!(&:alive?)
           @pool << spawn_worker if @pool.size < @max_threads
         end
