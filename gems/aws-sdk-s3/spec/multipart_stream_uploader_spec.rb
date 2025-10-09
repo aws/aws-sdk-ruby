4003536958ddad68b1b674b71a0c94eefb7d086c
Juli Tera <terajul@amazon.com>
2025-10-09 10:37:49 -0700

    Update specs

--- gems/aws-sdk-s3/spec/multipart_stream_uploader_spec.rb
+++ gems/aws-sdk-s3/spec/multipart_stream_uploader_spec.rb
@@ -7,10 +7,15 @@ module Aws
   module S3
     describe MultipartStreamUploader do
       let(:client) { S3::Client.new(stub_responses: true) }
-      let(:subject) { MultipartStreamUploader.new(client: client) }
+      let(:executor) { S3::DefaultExecutor.new }
+      let(:subject) { MultipartStreamUploader.new(client: client, executor: executor) }
       let(:params) { { bucket: 'bucket', key: 'key' } }
       let(:one_mb) { '.' * 1024 * 1024 }
       let(:seventeen_mb) { one_mb * 17 }
+
+      after(:each) do
+        executor.shutdown
+      end
 
       describe '#initialize' do
         it 'constructs a default s3 client when none provided' do
@@ -50,7 +55,6 @@ module Aws
             }
           )
           expect(client).to receive(:complete_multipart_upload).with(expected_params).once
-
           subject.upload(params.merge(content_type: 'text/plain')) { |write_stream| write_stream << seventeen_mb }
         end
 
@@ -155,7 +159,7 @@ module Aws
         end
 
         context 'when tempfile is true' do
-          let(:subject) { MultipartStreamUploader.new(client: client, tempfile: true) }
+          let(:subject) { MultipartStreamUploader.new(client: client, tempfile: true, executor: S3::DefaultExecutor.new) }
 
           it 'uses multipart APIs' do
             client.stub_responses(:create_multipart_upload, upload_id: 'id')
