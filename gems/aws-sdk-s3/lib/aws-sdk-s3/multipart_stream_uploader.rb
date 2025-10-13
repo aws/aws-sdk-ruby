04a287fd50c23f542edf36926b9ebc023baa0a46
Juli Tera <terajul@amazon.com>
2025-10-13 09:52:18 -0700

    Feedback - update to use 10 threads only

--- gems/aws-sdk-s3/lib/aws-sdk-s3/multipart_stream_uploader.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/multipart_stream_uploader.rb
@@ -53,24 +53,22 @@ module Aws
 
       def upload_parts(upload_id, options, &block)
         completed_parts = Queue.new
+        done_signal = Queue.new
         errors = []
+        part_opts = upload_part_opts(options).merge(upload_id: upload_id)
 
         begin
           IO.pipe do |read_pipe, write_pipe|
-            upload_thread = Thread.new do
-              upload_with_executor(
-                read_pipe,
-                completed_parts,
-                errors,
-                upload_part_opts(options).merge(upload_id: upload_id)
-              )
+            @executor.post(read_pipe, completed_parts, errors, part_opts) do |r_pipe, parts, errs, opts|
+              upload_with_executor(r_pipe, parts, errs, opts)
+            ensure
+              done_signal << :done
             end
-
             block.call(write_pipe)
           ensure
             # Ensure the pipe is closed to avoid https://github.com/jruby/jruby/issues/6111
             write_pipe.close
-            upload_thread.join
+            done_signal.pop
           end
         rescue StandardError => e
           errors << e
