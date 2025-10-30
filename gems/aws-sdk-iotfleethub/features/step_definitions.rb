6ce8fff1ab2533594a045ce037298e195c064458
Richard Wang <wangrch@amazon.com>
2025-10-30 13:56:10 -0700

    [two-way-validation skip] Deprecate lots of services 2

--- gems/aws-sdk-iotfleethub/features/step_definitions.rb
+++
@@ -1,8 +0,0 @@
-Before("@iotfleethub") do
-  @service = Aws::IoTFleetHub::Resource.new
-  @client = @service.client
-end
-
-After("@iotfleethub") do
-  # shared cleanup logic
-end
--- gems/aws-sdk-lookoutmetrics/features/step_definitions.rb
+++
@@ -1,8 +0,0 @@
-Before("@lookoutmetrics") do
-  @service = Aws::LookoutMetrics::Resource.new
-  @client = @service.client
-end
-
-After("@lookoutmetrics") do
-  # shared cleanup logic
-end
