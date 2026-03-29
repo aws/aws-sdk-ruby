9c35b742fdd069defa6fcb317f75723e4b130b01
monkeyWzr <monkeywzr@gmail.com>
2026-03-29 22:00:42 +0900

    refactoring

--- gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/sse_cpk.rb
+++ gems/aws-sdk-s3/lib/aws-sdk-s3/plugins/sse_cpk.rb
@@ -41,8 +41,10 @@ This should only be disabled for local testing.
           end
 
           def require_https(context)
-            if context.config.require_https_for_sse_cpk &&
-                !(URI::HTTPS === context.config.endpoint)
+            # Skip the check if the user opted out of HTTPS enforcement
+            return unless context.config.require_https_for_sse_cpk
+
+            unless URI::HTTPS === context.config.endpoint
               msg = <<-MSG.strip.gsub("\n", ' ')
                 Attempting to send customer-provided-keys for S3
                 server-side-encryption over HTTP; Please configure a HTTPS
